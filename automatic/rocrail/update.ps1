eaad0c1e702e87474fadf42ee0398f1b71114e74
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-01 10:35:11 +0100

    RocRail: Fixed url parsing

--- automatic/rocrail/update.ps1
+++ automatic/rocrail/update.ps1
@@ -19,13 +19,17 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     #rocrail-14507-win32.exe
-    $re  = "rocrail-.+-win(32|64).exe"
-    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
+    $re32  = "rocrail-.+-win32.exe"
+    $re64  = "rocrail-.+-win64.exe"
+    $uri32 = $download_page.links | ? href -match $re32 | select -First 1 -expand href
+    $uri64 = $download_page.links | ? href -match $re64 | select -First 1 -expand href
 
-    $version = $url[0] -split '-' | select -Last 1 -Skip 1
+    $filename32 = $uri32.Substring($uri32.LastIndexOf("/") + 1)
+    $filename64 = $uri64.Substring($uri64.LastIndexOf("/") + 1)
+    $version = $filename64 -split '-' | select -Last 1 -Skip 1
     $version = '0.' + $version
-    $url32 = 'https://wiki.rocrail.net/rocrail-snapshot/history/' + $url[0]
-    $url64 = 'https://wiki.rocrail.net/rocrail-snapshot/history/' + $url[1]
+    $url32 = 'https://wiki.rocrail.net/rocrail-snapshot/history/' + $filename32
+    $url64 = 'https://wiki.rocrail.net/rocrail-snapshot/history/' + $filename64
 
     $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
