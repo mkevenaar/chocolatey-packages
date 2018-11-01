3eeb7ef86c3cd0ba8f4caf30b1556b5a89df4ba3
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-01 15:09:51 +0100

    * Added scarm * Updated packages to add sumary

---
+++ automatic/scarm/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://www.scarm.info/SCARMsetup_1_4_0.exe'
+$checksum     = '459dc4663c1ae2ac87bb2c28c7cb78e9c26fc3d77a805595e5ba7c54a2bbe85d'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType      = 'exe'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  softwareName  = 'SCARM*'
+  silentArgs    = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /FORCECLOSEAPPLICATIONS /NOICONS"
+  validExitCodes= @(0,3010)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/scarm/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$releases = 'https://www.scarm.info/index.php?page=download'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    #SCARMsetup_1_4_0.exe
+    $re  = "SCARMsetup_.+.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = (([regex]::Match($url,'SCARMsetup_(.+).exe')).Captures.Groups[1].value).replace('_','.')
+    $url = 'https:' + $url
+
+    return @{ 
+        URL32 = $url
+        Version = $version 
+    }
+}
+
+update
