3eea054c3790ec5b5d23cd6c089ed0ad1f4825fd
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-09 13:19:17 +0100

    New package [AU mysql.workbench]

---
+++ automatic/mysql.workbench/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-8.0.13-winx64.msi'
+$checksum     = '12cd112c94fcfeda9a895889ffe2dd879a2d096b5c7ac1ae284ab2ab910a9970'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType       = 'msi'
+  url           = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  softwareName  = 'MySQL Workbench*'
+  silentArgs    = "/passive /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/mysql.workbench/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'http://dev.mysql.com/downloads/tools/workbench/'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    
+    $version = ($download_page.ParsedHtml.getElementsByTagName('h1') | ? innerhtml -match "^MySQL Workbench").innerhtml -replace "^MySQL Workbench "
+    $version = $version.Trim()
+
+    $url = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-' + $version + '-winx64.msi'
+    $Latest = @{ URL32 = $url; Version = $version }
+    return $Latest
+}
+
+update
