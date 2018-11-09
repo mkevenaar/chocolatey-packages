5b46534a77d18d18bccfc9850d0253d3461c11ce
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-09 14:11:58 +0100

    Manual package QuickPar [skip ci]

---
+++ manual/QuickPar/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://download.quickpar.org.uk/download.php/QuickPar-0.9.1.0.exe'
+$checksum     = '074a9f11057821228bfcc9531b2921d482bb76bfd70a29fda53b36c71c12e8e1'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType       = 'exe'
+  url           = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  softwareName  = 'MySQL Workbench*'
+  silentArgs = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
