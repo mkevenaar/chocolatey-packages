0e9e582705372bfe2c5a81c6df77ced9b0aef6b1
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-21 16:14:09 +0100

    switched to json data

--- automatic/goland/update.ps1
+++ automatic/goland/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://www.jetbrains.com/go/download/download-thanks.html'
+$releases = 'https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release'
 
 function global:au_SearchReplace {
     @{
@@ -13,14 +13,9 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $ie = New-Object -comobject InternetExplorer.Application
-    $ie.Navigate2($releases)
-    $ie.Visible = $false
-    while($ie.ReadyState -lt 3) {start-sleep -m 100} 
-
-    $url = $ie.Document.body.getElementsByTagName("a") | Where-Object {$_.id -eq "download-link"} | select -First 1 -expand href
-
-    $version = ([regex]::Match($url,'goland-(.+).exe')).Captures.Groups[1].value
+    $json = Invoke-WebRequest $releases | ConvertFrom-Json
+    $url = $json.GO.downloads.windows.link
+    $version = $json.GO.version
 
     $Latest = @{ URL32 = $url; Version = $version }
     return $Latest
