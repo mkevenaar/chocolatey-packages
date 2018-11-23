b25e89e3e26b15c76d7db85f5baac9c79aabe64a
Maurice Kevenaar <maurice@kevenaar.name>
2018-11-23 16:20:43 +0000

    AU: 2 updated - goland rocrail [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/3563eeadd0748224ba58cf3949679ceaa5b89e11

--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-14588-win32.exe'
 $checksum32     = '4269360be93cf4737bc06e76bc162132131a852dfad3530504d5b775c4cb19e7'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-14588-win64.exe'
-$checksum64     = 'fc9183636cf402444c0340cd5152a58f8ab25d7b71e9ad03a4424c0ed9cc8c1d'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-14591-win64.exe'
+$checksum64     = 'd2b4a868383a6d6a84255bfa9535479aed6375d47199169d8ef8742081914ca0'
 $checksumType64 = 'sha256'
 
 
