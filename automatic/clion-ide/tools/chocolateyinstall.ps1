65171a5def20ba952ced7f37c5a9b776ad315ae3
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-03 06:38:06 +0100

    Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935 [AU clion-ide:2018.3]

--- automatic/clion-ide/tools/chocolateyinstall.ps1
+++ automatic/clion-ide/tools/chocolateyinstall.ps1
@@ -4,6 +4,10 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://download.jetbrains.com/cpp/CLion-2018.3.exe'
 $checksum     = '7c4a4388b34b315e445ba7c661db62a637b1af84bf3407535803ddf0b0b28f50'
 $checksumType = 'sha256'
+
+# Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
+$programfiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
+New-Item -ItemType Directory -Force -Path $programfiles\JetBrains
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
