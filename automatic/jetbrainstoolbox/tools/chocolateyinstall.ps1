6a666668129f7e2880d7b5d466fd86e9768eda4d
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-03 14:57:43 +0100

    New package jetbrainstoolbox [AU jetbrainstoolbox:1.12.4481]

---
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.12.4481.exe'
+$checksum     = 'dd6c5c3ef95c3b5d0cccb229f0ba36fc302febc9d6c9c0b9aebe4fca21c72ae7'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'JetBrains Toolbox*'
+  fileType      = 'exe'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  destination   = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs
