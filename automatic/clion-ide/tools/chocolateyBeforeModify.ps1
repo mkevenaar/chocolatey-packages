7f4e050740177ceeab314760dc3768f3f8666bec
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-08 16:59:50 +0100

    clion-ide: added beforemodify [skip ci]

---
+++ automatic/clion-ide/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  PackageName     = $env:ChocolateyPackageName
+  softwareName    = "JetBrains Clion*"
+  FileType        = 'exe'
+  Silent          = '/S'
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    Get-Process PyCharm* | % { $_.CloseMainWindow() }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
