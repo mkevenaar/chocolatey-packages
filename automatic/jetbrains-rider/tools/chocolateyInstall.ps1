632cca975e8c55276c1cce0684a19e3a662693c6
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-10 16:06:14 +0100

    (jetrains-rider) New package

---
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2018.2.3.exe'
+$checksum     = 'b94c23f2a3e4937bbfd5971222be37b3dc29b1a8f807e9416c8db25d69381f70'
+$checksumType = 'sha256'
+
+# Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
+$programfiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
+New-Item -ItemType Directory -Force -Path $programfiles\JetBrains
+ 
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Jetbrains Rider*'
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  validExitCodes = @(0)
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  destination   = $toolsDir
+}
+
+ Install-ChocolateyPackage @packageArgs	
