2ab22ed44a3aafcd49477a00a4f5e7481ced0dbc
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-14 21:47:55 +0000

    AU: 1 updated - movie-collector [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/bde28f41729d45025b96ba9e22e9f575af94e3e7

--- automatic/movie-collector/tools/chocolateyinstall.ps1
+++ automatic/movie-collector/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://installers.collectorz.com/movie-win/moviecollectorsetup1847.exe'
-$checksum     = '9dec421e9a2dedaa45acf17edf9266e90874bf96e0fa6a6e5e5071db74f89906'
+$url          = 'http://installers.collectorz.com/movie-win/moviecollectorsetup1902.exe'
+$checksum     = 'b8d2dcfa2cb8e74bee680f5c2dcb2c0cf0528f22391e69aed13d46b7953e1e96'
 $checksumType = 'sha256'
 
 $packageArgs = @{
