3b438e1aa4c3bf0828063f337fc3559ed160a4b3
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-20 11:24:58 +0100

    (speedfan) new package [AU speedfan]

---
+++ automatic/speedfan/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://www.almico.com/speedfan452.exe'
+$checksum     = 'e2ccb3c0d23f0d04ee8057f5ce3861eea952fb20694c1656c9805b1d4cd922ff'
+$checksumType = 'sha256'
+
+$options = @{
+  Headers = @{
+    Referer = 'http://www.almico.com/sfdownload.php';
+  }
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+  softwareName   = 'SpeedFan*'
+  silentArgs     = '/S'
+  options        = $options
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/speedfan/update.ps1
@@ -0,0 +1,47 @@
+import-module au
+
+$releases = 'http://www.almico.com/sfdownload.php'
+
+$headers = @{
+  Referer = 'http://www.almico.com/sfdownload.php';
+}
+    
+$options =
+@{
+  Headers = $headers
+}
+
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32 -Headers $headers
+    $Latest.ChecksumType32 = 'sha256'
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $re = 'speedfan(.+).exe'
+    $url = $download_page.Links | Where-Object href -match $re | Select-Object -First 1 -expand href
+    $url = 'http://www.almico.com/' + $url
+    
+    $version = ($download_page.Links | Where-Object href -match $re | Select-Object -First 1 -expand innerHTML) -replace "SpeedFan ", ""
+    
+
+    return @{ 
+        URL32 = $url
+        Version = $version
+        Options = $options
+    }
+}
+
+update -ChecksumFor none
