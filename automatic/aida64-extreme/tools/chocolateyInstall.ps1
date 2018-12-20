782b0de0f35e38c8b39ac87395996081ac1b8740
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-20 14:46:13 +0100

    (aida64-engineer) new package

---
+++ automatic/aida64-extreme/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://download.aida64.com/aida64extreme599.exe'
+$checksum     = 'c9b8d414b631995759702391798a7657c9db1187e4456c4d794e6196e3f0c334'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+  softwareName  = 'AIDA64 Extreme*'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0,3010)
+}
+
+Install-ChocolateyPackage @packageArgs
+
