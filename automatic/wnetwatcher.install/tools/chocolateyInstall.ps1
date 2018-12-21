77038a102b198ab4b547811d09b914cb5396f191
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-21 14:43:40 +0100

    (wnetwatcher) and (wnetwatcher.install) new packages

---
+++ automatic/wnetwatcher.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://www.nirsoft.net/utils/wnetwatcher_setup.exe'
+$checksum     = 'a26d4aab7230a55fcb02c24310425529973ee14a8c078de9b0398828c033c8bc'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+  softwareName   = 'Nirsoft wnetwatcher*'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ deprecated/wirelessnetview.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
