1b0898ba685eace23b064a3f3d986b848538b0c0
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-24 17:15:41 +0100

    (samsung-magician) New package

--- automatic/samsung-magician/tools/chocolateyInstall.ps1
+++ automatic/samsung-magician/tools/chocolateyInstall.ps1
@@ -17,14 +17,13 @@ $packageArgs = @{
   validExitCodes = @(0,3010)
 }
 
-$ahkExe = Get-ChildItem "$env:ChocolateyInstall\lib\autohotkey.portable" -Recurse -filter AutoHotKey.exe
-$ahkFile = Join-Path $toolsDir "chocolateyInstall.ahk"
-$ahkProc = Start-Process -FilePath $ahkExe `
-                         -ArgumentList $ahkFile `
-                         -PassThru
- 
-$ahkId = $ahkProc.Id
-Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
-Write-Debug "Process ID:`t$ahkId"
+# silent install requires AutoHotKey
+$ahkFile = Join-Path $toolsPath 'chocolateyInstall.ahk'
+$ahkEXE = Get-ChildItem "$env:ChocolateyInstall\lib\autohotkey.portable" -Recurse -filter autohotkey.exe
+$ahkProc = Start-Process -FilePath $ahkEXE.FullName -ArgumentList "$ahkFile" -PassThru
+Write-Debug "AutoHotKey start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$($ahkProc.Id)"
 
 Install-ChocolateyPackage @packageArgs
+
+if (Get-Process -id $ahkProc.Id -ErrorAction SilentlyContinue) {Stop-Process -id $ahkProc.Id}
