20139a03a077c6821813f7e433a1db9688fdf7a0
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-27 12:46:14 +0100

    (jetbrains-youtrack) New Package

--- automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
+++ automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.anyrail.com/downloads/AnyRail/6.21/AnyRail6.21.0.msi'
-$checksum     = 'e5df16f1cd475e53df8be6ef2140f1fee638fb1ab0e654f02011db59af6851c8'
+$url          = 'https://download.jetbrains.com/charisma/youtrack-2018.4.48293.msi'
+$checksum     = '91077051bcb6f4672c3f3539798dc7a5b7793861b384dd2042d8b309b3544e5e'
 $checksumType = 'sha256'
 
 $packageArgs = @{
@@ -12,7 +12,7 @@ $packageArgs = @{
   url            = $url
   checksum       = $checksum
   checksumType   = $checksumType
-  softwareName   = 'AnyRail6*'
+  softwareName  = 'JetBrains YouTrack*'
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
   validExitCodes= @(0,1641,3010)
 }
---
+++ automatic/jetbrains-youtrack/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$releases = 'https://data.services.jetbrains.com/products/releases?code=YTD&latest=true&type=release'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $json = Invoke-WebRequest $releases | ConvertFrom-Json
+    $url = $json.YTD.downloads.windows.link
+    $version = $json.YTD.version
+    $checksum = ((Invoke-RestMethod -Uri $json.YTD.downloads.windows.checksumLink -UseBasicParsing).Split(" "))[0]
+
+    $Latest = @{ URL32 = $url; Version = $version; Checksum32 = $checksum; ChecksumType32 = 'sha256' }
+    return $Latest
+}
+
+update -ChecksumFor none
