d2369a9a47025fc8019a76146c6ca3642420dc17
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-31 15:13:41 +0100

    (tomtom-mydrive-connect) new package

---
+++ automatic/tomtom-mydrive-connect/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://cdn.sa.services.tomtom.com/static/sa/Windows/InstallTomTomMyDriveConnect.exe'
+$checksum     = '67A3A6BB0DB14F014B3EDB0EC94B41965FE89EAE2ECF1F33CDE35741BFB95C3F'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType      = 'exe'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  softwareName  = 'TomTom MyDrive Connect*'
+  silentArgs    = "/S"
+  validExitCodes= @(0,3010)
+}
+
+Install-ChocolateyPackage @packageArgs
+
