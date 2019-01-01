8acd3fcb7e81ad0898781a0e2daf16f9bd3f4704
Maurice Kevenaar <maurice@kevenaar.name>
2019-01-01 11:19:30 +0100

    (devmanview) New package

---
+++ automatic/devmanview/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32          = 'http://www.nirsoft.net/utils/devmanview.zip'
+$checksum32     = '991a790b16a9d5830a7427ca3b534581b57c2d505fbafda466b94de5717cf2c4'
+$checksumType32 = 'sha256'
+$url64          = 'http://www.nirsoft.net/utils/devmanview-x64.zip'
+$checksum64     = '2608d1373069ecee11b635293b13419dbb7644435a0cc47cdb398c3dd0496809'
+$checksumType64 = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = $checksumType32
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
+}
+
+Install-ChocolateyZipPackage  @packageArgs
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "devmanview.exe.gui"
+Set-Content -Path $installFile `
+            -Value $null
---
+++ automatic/devmanview/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+$releases = 'http://www.nirsoft.net/utils/device_manager_view.html'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re = 'DevManView v(.+) -'
+    $version = ([regex]::Match($download_page.content,$re)).Captures.Groups[1].value
+    
+    $url = $download_page.Links | Where-Object href -match "devmanview(.+)?.zip" | Select-Object -First 2 -expand href
+    $url32 = 'http://www.nirsoft.net/utils/' + $url[0]
+    $url64 = 'http://www.nirsoft.net/utils/' + $url[1]
+
+    return @{ 
+        URL32 = $url32
+        URL64 = $url64
+        Version = $version 
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+    update
+}
