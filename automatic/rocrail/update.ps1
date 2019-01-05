8dcfa7ddc41bf38002e6f3a790f41e53a182ff14
Maurice Kevenaar <maurice@kevenaar.name>
2019-01-05 15:54:50 +0100

    (rocrail) order some variables in update

--- automatic/rocrail/update.ps1
+++ automatic/rocrail/update.ps1
@@ -5,11 +5,11 @@ $releases = 'https://wiki.rocrail.net/rocrail-snapshot/'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
             "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
             "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
      }
