bbce2e58cfb83ef071b1141ea5f450b51032700c
Maurice Kevenaar <maurice@kevenaar.name>
2019-01-06 13:50:22 +0100

    (spark) New package [AU spark]

---
+++ automatic/spark/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://www.igniterealtime.org/downloadServlet?filename=spark/spark_2_8_3.exe'
+$checksum     = '8488618c854ada8ae7142fab68d567254df8ec50dbe53ce96dbc04ec5d130e13'
+$checksumType = 'sha256'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+  softwareName   = 'Spark*'
+  silentArgs     = '-q'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/spark/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+$releases = 'https://www.igniterealtime.org/downloads/index.jsp'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re = 'spark(.+).exe'
+    $url = $download_page.Links | Where-Object href -match $re | Select-Object -First 1 -expand href | Foreach-Object { $_ -replace "/downloads/download-landing.jsp\?file=", "/downloadServlet?filename=" }
+    $url = 'https://www.igniterealtime.org' + $url 
+    
+    $versionre = "<h2>Spark (.+\d)</h2>"
+    $version = ([regex]::Match($download_page.content,$versionre)).Captures.Groups[1].value
+    
+
+    return @{ 
+        URL32 = $url
+        Version = $version
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+    update -ChecksumFor 32
+}
