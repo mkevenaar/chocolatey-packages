4596cab98a39d590fca3647f385b4077f40d77c0
Maurice Kevenaar <maurice@kevenaar.name>
2019-01-30 12:26:07 +0000

    AU: 2 updated - duplicatefilefinder jetbrains-rider [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/232733bba729c495e867e30df08980fb94b5c19f

--- automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
+++ automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
-$checksum     = '8b423087280a31564c7ec6477e751c8af83a2f0b385678849c8c9c3d04901123'
+$checksum     = '4675f46dc5b96015230cdb4857dec028ee81e3cc24150ad8cffa9bdaefd7a5b4'
 $checksumType = 'sha256'
 
 $packageArgs = @{
--- automatic/jetbrains-rider/tools/chocolateyInstall.ps1
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2018.3.1.exe'
-$checksum     = '5c39471f6912ed8ba9fd48cab594032a6e06b8c0f5039766c51b68a2d269e654'
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2018.3.2.exe'
+$checksum     = 'ecab5a91e8245f21a07ef5a9fba07b4c3d74a366ab3ffaf27ac38f3be7de1c58'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
