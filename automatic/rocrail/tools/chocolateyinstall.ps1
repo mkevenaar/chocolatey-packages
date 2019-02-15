845aa1580260f93135d67a7db087b20b4c751fa2
Maurice Kevenaar <maurice@kevenaar.name>
2019-02-15 18:28:44 +0000

    AU: 2 updated - firefox-dev rocrail [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/2da793768ab6a92fe9a5519dfe56fcd09a004207

--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-15037-win32.exe'
 $checksum32     = 'ca75250518420ae523db27f3d8aa96be59917acf3ba2e3b42e14d29e1626a3c6'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-15037-win64.exe'
-$checksum64     = '6f4e2229bdd77a55098516d2f2f6fe8f40e2a110f7a04137aa414c4bee6a4b48'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-15044-win64.exe'
+$checksum64     = 'baa5d26be4eef720a7eed1b90db253f1c1f3b2ad4e609e01e48b5f3ded1e750b'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
