7dccc0aa82505e7d6ecd1660d4e3f9159848761a
Maurice Kevenaar <maurice@kevenaar.name>
2019-02-20 06:28:54 +0000

    AU: 2 updated - windowsrepair windowsrepair.install [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/9d8826091a3494480b7bf6a98d4dccee530edd45

--- automatic/windowsrepair.install/tools/chocolateyInstall.ps1
+++ automatic/windowsrepair.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://www.tweaking.com/files/setups/tweaking.com_windows_repair_aio_setup.exe'
-$checksum     = '223f6f8fe9b9d1080853ddda9bab3553551212dd0954e8a04b0d96439b32adb4'
+$checksum     = 'aeafe14ad953769bfde4cb361489dc5d4a26b93d77a86fae2bede7c999627494'
 $checksumType = 'sha256'
 
 $packageArgs = @{
