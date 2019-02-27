6f25969437fcffe2edbf7ea1efccb93102256c00
Maurice Kevenaar <maurice@kevenaar.name>
2019-02-27 10:54:27 +0100

    (filezilla.server) Made ftpRoot configurable [AU:filezilla.server:0.9.60.2]

--- automatic/filezilla.server/tools/chocolateyInstall.ps1
+++ automatic/filezilla.server/tools/chocolateyInstall.ps1
@@ -10,16 +10,24 @@ $packageArgs = @{
   silentArgs    = "/S"
   validExitCodes = @(0)
 }
+
 $programfiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
 
-$fileZillaInstallDir = Join-Path $programfiles "FileZilla Server"
-$tempDir = "C:\temp\ftproot"
+$ftpRoot = "C:\temp\ftproot"
+$packageParameters = Get-PackageParameters
 
-write-host "Chocolatey is installing FileZilla Server to $fileZillaInstallDir. The port is 14147, the user name/pass is filezilla/filezilla. The local ftp root folder is $tempDir."
+if ( $packageParameters.ftproot ) {
+  Write-Host "ftproot Argument Found"
+  $ftpRoot = $packageParameters.ftproot
+}
+
+$fileZillaInstallDir = Join-Path $programfiles "FileZilla Server"
+
+write-host "Chocolatey is installing FileZilla Server to $fileZillaInstallDir. The port is 14147, the user name/pass is filezilla/filezilla. The local ftp root folder is $ftpRoot."
 write-host "Please wait..."
 Start-sleep 8
 
-if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+if (![System.IO.Directory]::Exists($ftpRoot)) {[System.IO.Directory]::CreateDirectory($ftpRoot)}
 if (![System.IO.Directory]::Exists($fileZillaInstallDir)) {[System.IO.Directory]::CreateDirectory($fileZillaInstallDir)}
 
 if (![System.IO.File]::Exists("$($fileZillaInstallDir)\FileZilla Server Interface.xml")) {
@@ -30,6 +38,12 @@ if (![System.IO.File]::Exists("$($fileZillaInstallDir)\FileZilla Server Interfac
 if (![System.IO.File]::Exists("$($fileZillaInstallDir)\FileZilla Server.xml")) {
   Write-Host "Copying FileZilla Server.xml to install directory"
   Copy-Item "$($toolsDir)\FileZilla Server.xml" "$fileZillaInstallDir" -Force
+}
+
+if ( $packageParameters.ftproot ) {
+  $filezillaConf = Get-Content "$($fileZillaInstallDir)\FileZilla Server.xml"
+  $filezillaConf = $filezillaConf -replace "<Permission Dir=`".+`">", "<Permission Dir=`"$ftpRoot`">"
+  Set-Content -Path "$($fileZillaInstallDir)\FileZilla Server.xml" -Value $filezillaConf -Encoding UTF8
 }
 
 Install-ChocolateyInstallPackage @packageArgs
