ddee98e3481dca3b4213da8592dbb8f516e741b6
Maurice Kevenaar <maurice@kevenaar.name>
2019-03-08 18:06:52 +0100

    (mariadb.install) embed package fixes GH-12 [AU mariadb mariadb.install]

--- automatic/mariadb.install/tools/chocolateyInstall.ps1
+++ automatic/mariadb.install/tools/chocolateyInstall.ps1
@@ -1,30 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.mariadb.org/f/mariadb-10.3.13/win32-packages/mariadb-10.3.13-win32.msi'
-$checksum32     = 'af23820183a379b45f6d9b9a2ba2b3dd8822fddf97b2ea8ee88936fc2ef466df'
-$checksumType32 = 'sha256'
-$url64          = 'https://downloads.mariadb.org/f/mariadb-10.3.13/winx64-packages/mariadb-10.3.13-winx64.msi'
-$checksum64     = '6b750c22f385057c91687fa33d139c7df95270164e1ff588980422c128fc122d'
-$checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = $checksumType32
-  url64bit      = $url64
-  checksum64    = $checksum64
-  checksumType64= $checksumType64
+  file           = "$toolsdir\mariadb-10.3.13-win32.msi"
+  file64         = "$toolsdir\mariadb-10.3.13-winx64.msi"
   softwareName   = 'MariaDB *'
   silentArgs     = "SERVICENAME=MySQL /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0,3010)
 }
 
 Write-Verbose "Downloading and installing program..."
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 Write-Verbose "Querying registry for install location..."
 $packageSearch = "MariaDB *"
@@ -45,3 +35,5 @@ if ($binPath) {
                 -Wait `
                 -NoNewWindow
 }
+
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
--- automatic/mariadb.install/update.ps1
+++ automatic/mariadb.install/update.ps1
@@ -1,19 +1,8 @@
-import-module au
+Import-Module au
 
 $releases = 'https://downloads.mariadb.org/mariadb/'
 
-function global:au_SearchReplace {
-    @{
-        '.\tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-        }
-     }
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $version_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -26,9 +15,31 @@ function global:au_GetLatest {
     $url32 = "https://downloads.mariadb.org/f/mariadb-" + $version + "/win32-packages/mariadb-" + $version + "-win32.msi"
     $url64 = "https://downloads.mariadb.org/f/mariadb-" + $version + "/winx64-packages/mariadb-" + $version + "-winx64.msi"
     
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
-    return $Latest
+    return @{ 
+        URL32 = $url32
+        URL64 = $url64
+        Version = $version 
+        FileType = 'msi'
+    }
 }
+
+function global:au_SearchReplace {
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
 if ($MyInvocation.InvocationName -ne '.') {
-    update
+  update -ChecksumFor None
 }
