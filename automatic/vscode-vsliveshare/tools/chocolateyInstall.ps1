7a4814c005c319ae5f496a2f3750c78e3d129585
Maurice Kevenaar <maurice@kevenaar.name>
2019-03-11 10:08:36 +0100

    (vscode-vsliveshare) New package

---
+++ automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@0.0'
---
+++ automatic/vscode-vsliveshare/tools/chocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+﻿ $ErrorActionPreference = 'Stop'
+
+ Uninstall-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare'
