f0d196ff0831f330f79a8900fc622b1c0e1e659f
Maurice Kevenaar <maurice@kevenaar.name>
2019-03-25 13:38:52 +0100

    (psi.install) some small changes to the install file

--- automatic/psi.install/tools/chocolateyInstall.ps1
+++ automatic/psi.install/tools/chocolateyInstall.ps1
@@ -3,12 +3,13 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = "$toolsDir\psi-1.3-win64-setup.exe"
-  softwareName   = 'Psi*'
+  softwareName  = 'Psi*'
   silentArgs    = "/S"
-  validExitCodes = @(0)
+  validExitCodes= @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
