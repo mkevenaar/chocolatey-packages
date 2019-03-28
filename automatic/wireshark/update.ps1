53eb88cb71c17bc7e7ebe4f2fc0885a57ac5b578
Maurice Kevenaar <maurice@kevenaar.name>
2019-03-28 14:40:14 +0100

    (wireshark) switched to embedded package [AU wireshark:3.0.0]

--- automatic/wireshark/update.ps1
+++ automatic/wireshark/update.ps1
@@ -1,20 +1,43 @@
-import-module au
+Import-Module au
 
 $version_page = 'https://www.wireshark.org/#download'
 $releases64 = 'https://www.wireshark.org/download/win64/all-versions/'
 $releases32 = 'https://www.wireshark.org/download/win32/all-versions/'
 
-function global:au_SearchReplace {
-    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-        }
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $version = GetVersion
+    
+    $r64  = '[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
+    $url64 = GetDownloadLink -downloadlinks $releases64 -pattern $r64 -version $version
+	
+    $r32 = '[Ww]ire[Ss]hark-win32-[\d\.]+\.exe$'   
+    $url32 = GetDownloadLink -downloadlinks $releases32 -pattern $r32 -version $version
+
+    return @{ 
+        URL32 = $url32
+        URL64 = $url64
+        Version = $version 
+        FileType = 'exe'
     }
+}
+
+function global:au_SearchReplace {
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$version_page>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
 }
 
 function GetVersion {
@@ -30,16 +53,6 @@ function GetDownloadLink([string] $downloadlinks, [string] $pattern, [string] $v
     $page.BaseResponse.ResponseUri.ToString() + $link
 }
 
-function global:au_GetLatest {
-    $version = GetVersion
-    
-    $r64  = '[Ww]ire[Ss]hark-win64-[\d\.]+\.exe$'
-    $url64 = GetDownloadLink -downloadlinks $releases64 -pattern $r64 -version $version
-	
-    $r32 = '[Ww]ire[Ss]hark-win32-[\d\.]+\.exe$'   
-    $url32 = GetDownloadLink -downloadlinks $releases32 -pattern $r32 -version $version
-
-    return @{ URL32 = $url32; URL64 = $url64; Version = $version }
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor None
 }
-
-update
