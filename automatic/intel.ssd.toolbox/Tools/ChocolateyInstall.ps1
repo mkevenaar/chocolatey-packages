0f96879d8ca86c305cc7fba3edb9521be21fab92
Maurice Kevenaar <maurice@kevenaar.name>
2019-04-04 16:53:28 +0200

    (intel.ssd.toolbox) New package

---
+++ automatic/intel.ssd.toolbox/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://downloadmirror.intel.com/28674/eng/Intel%20SSD%20Toolbox%20-%20v3.5.10.exe'
+$checksum     = '1EBD356C1A1783CFF8AB957EDC0517F80844ABCFCE9E5B89D845546B3F752C19'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  softwareName  = 'Intel SSD*'
+  silentArgs    = "/s"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/intel.ssd.toolbox/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$releases = 'https://downloadcenter.intel.com/Detail_Desc.aspx?DwnldID=18455'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.Links | Where-Object href -Match ".exe" | Select-Object -First 1 -ExpandProperty data-direct-path
+    $url = [uri]::EscapeUriString($url)
+
+    $versionregex = "v([0-9]+.[0-9]+.[0-9]+)"
+    $version = ([regex]::Match($url, $versionregex)).Captures.Groups[1].value
+
+    $releaseNotes = $download_page.Links | Where-Object href -Match ".pdf" | Select-Object -First 1 -ExpandProperty href
+
+    $Latest = @{ URL32 = $url; Version = $version; ReleaseNotes = $releaseNotes }
+    return $Latest
+}
+
+update -ChecksumFor 32
