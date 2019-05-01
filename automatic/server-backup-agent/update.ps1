56f43e87d0a7d26b22e084dad5fc3cdd1809f00c
Maurice Kevenaar <maurice@kevenaar.name>
2019-05-01 09:42:44 +0200

    (server-backup-agent) fixed MSI database versioning

--- automatic/server-backup-agent/update.ps1
+++ automatic/server-backup-agent/update.ps1
@@ -16,47 +16,43 @@ function global:au_SearchReplace {
 }
 
 function Get-MsiDatabaseVersion {
-  param (
-      [string] $fn
+  param(
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]$Path,
+ 
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet("ProductCode", "ProductVersion", "ProductName", "Manufacturer", "ProductLanguage", "FullVersion")]
+    [string]$Property
   )
   Process {
     try {
-      $FullPath = (Resolve-Path $fn).Path
-      $windowsInstaller = New-Object -com WindowsInstaller.Installer
-
-      $database = $windowsInstaller.GetType().InvokeMember(
-              "OpenDatabase", "InvokeMethod", $Null, 
-              $windowsInstaller, @($FullPath, 0)
-          )
-
-      $q = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
-      $View = $database.GetType().InvokeMember(
-              "OpenView", "InvokeMethod", $Null, $database, ($q)
-          )
-
-      $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null)
-
-      $record = $View.GetType().InvokeMember(
-              "Fetch", "InvokeMethod", $Null, $View, $Null
-          )
-
-      $productVersion = $record.GetType().InvokeMember(
-              "StringData", "GetProperty", $Null, $record, 1
-          )
-
-      $View.GetType().InvokeMember("Close", "InvokeMethod", $Null, $View, $Null)
-      $record = $Null
-      $View = $Null
-
-      return $productVersion.Trim()
-
-    } catch {
-      throw "Failed to get MSI file version the error was: {0}." -f $_
+        # Read property from MSI database
+        $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+        $MSIDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @($Path.FullName, 0))
+        $Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
+        $View = $MSIDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $MSIDatabase, ($Query))
+        $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
+        $Record = $View.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $View, $null)
+        $Value = $Record.GetType().InvokeMember("StringData", "GetProperty", $null, $Record, 1)
+ 
+        # Commit database and close view
+        $MSIDatabase.GetType().InvokeMember("Commit", "InvokeMethod", $null, $MSIDatabase, $null)
+        $View.GetType().InvokeMember("Close", "InvokeMethod", $null, $View, $null)           
+        $MSIDatabase = $null
+        $View = $null
+ 
+        # Return the value
+        return $Value
+    } 
+    catch {
+        Write-Warning -Message $_.Exception.Message ; break
     }
   }
   End {
     # Run garbage collection and release ComObject
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($windowsInstaller) | Out-Null
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WindowsInstaller) | Out-Null
     [System.GC]::Collect()
   }
 }
@@ -66,7 +62,7 @@ function GetResultInformation([string]$url32, [string]$url64) {
 
   Get-WebFile $url32 $dest | Out-Null
   $checksumType = 'sha256'
-  $version = (Get-MsiDatabaseVersion $dest).Trim()
+  $version = Get-MsiDatabaseVersion $dest ProductVersion
   $checksum32 = Get-FileHash $dest -Algorithm $checksumType | % Hash
   Remove-Item -force $dest -ErrorAction SilentlyContinue
 
