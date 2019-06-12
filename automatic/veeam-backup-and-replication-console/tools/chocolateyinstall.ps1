8475464b96fb07364ef45ea259cdf535182b627a
Maurice Kevenaar <maurice@kevenaar.name>
2019-06-12 12:59:52 +0200

    (veeam-backup-and-replication-console) New package

---
+++ automatic/veeam-backup-and-replication-console/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$isoPackageName = 'veeam-backup-and-replication-iso'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$filename = 'VeeamBackup&Replication_9.5.4.2753.Update4a.iso'
+$installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
+
+$fileLocation = 'Backup\Shell.x64.msi'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  isoFile        = $installPath
+  softwareName   = 'Veeam Backup & Replication Console*'
+  file           = $fileLocation
+  fileType       = 'msi'
+  silentArgs     = "ACCEPTEULA=YES ACCEPT_THIRDPARTY_LICENSES=1 /qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  validExitCodes = @(0,1641,3010)
+  destination    = $toolsDir
+}
+
+Install-ChocolateyIsoInstallPackage @packageArgs
