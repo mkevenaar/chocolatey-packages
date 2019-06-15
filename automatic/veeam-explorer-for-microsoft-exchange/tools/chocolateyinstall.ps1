830fbe68f8744d50cb97cfbabe69400fcf1927bb
Maurice Kevenaar <maurice@kevenaar.name>
2019-06-15 09:32:42 +0200

    (veeam-explorer-for-microsoft-exchange) new package

---
+++ automatic/veeam-explorer-for-microsoft-exchange/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$isoPackageName = 'veeam-backup-and-replication-iso'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$filename = 'VeeamBackup&Replication_9.5.4.2753.Update4a.iso'
+$installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
+
+$fileLocation = 'Explorers\VeeamExplorerForExchange.msi'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  isoFile        = $installPath
+  softwareName   = 'Veeam Explorer for Microsoft Exchange*'
+  file           = $fileLocation
+  fileType       = 'msi'
+  silentArgs     = "ACCEPT_EULA=1 ACCEPT_THIRDPARTY_LICENSES=1 /qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  validExitCodes = @(0,1641,3010)
+  destination    = $toolsDir
+}
+
+Install-ChocolateyIsoInstallPackage @packageArgs
