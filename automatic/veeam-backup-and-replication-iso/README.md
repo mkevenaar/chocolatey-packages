7fe3673a57a9e5847ef1cabad94709d4b06fe7d9
Maurice Kevenaar <maurice@kevenaar.name>
2019-06-16 20:33:40 +0200

    (veeam) Final things for management part

--- automatic/veeam-backup-and-replication-iso/README.md
+++ automatic/veeam-backup-and-replication-iso/README.md
@@ -6,6 +6,14 @@ Veeam® Backup & Replication™ is a backup solution developed for VMware vSpher
 
 This package is used as a dependency by other Chocolatey packages. To install any of the tools, please use one of the other packages.
 
+- [Veeam Backup & Replication Console](https://chocolatey.org/packages/veeam-backup-and-replication-console)
+- [Veeam Explorer for Microsoft Active Directory](https://chocolatey.org/packages/veeam-explorer-for-microsoft-active-directory)
+- [Veeam Explorer for Microsoft Exchange](https://chocolatey.org/packages/veeam-explorer-for-microsoft-exchange)
+- [Veeam Explorer for Microsoft SharePoint](https://chocolatey.org/packages/veeam-explorer-for-microsoft-sharepoint)
+- [Veeam Explorer for Microsoft SQL Server](https://chocolatey.org/packages/veeam-explorer-for-microsoft-sql-server)
+- [Veeam Explorer for Oracle](https://chocolatey.org/packages/veeam-explorer-for-oracle)
+- And more to come.
+
 **Please Note**: This is an automatically updated package. If you find it is
 out of date by more than a day or two, please contact the maintainer(s) and
 let them know the package is no longer updating correctly.
--- automatic/veeam-explorer-for-microsoft-active-directory/tools/chocolateyinstall.ps1
+++ automatic/veeam-explorer-for-microsoft-active-directory/tools/chocolateyinstall.ps1
@@ -8,6 +8,8 @@ $filename = 'VeeamBackup&Replication_9.5.4.2753.Update4a.iso'
 $installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
 
 $fileLocation = 'Explorers\VeeamExplorerForActiveDirectory.msi'
+$updateFileVersion = ($filename -replace 'VeeamBackup&Replication_' -replace '.iso').ToLower()
+$updateFileLocation = "Updates\veeam_backup_$($updateFileVersion)_setup.exe"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -21,3 +23,25 @@ $packageArgs = @{
 }
 
 Install-ChocolateyIsoInstallPackage @packageArgs
+
+# An update file is provided in the ISO. This should be executed after
+# the installation of the software.
+# This update file contains an update to all available software on the
+# ISO and should be executed After each installation.
+#
+# This should be in a seperate package, but you can't trigger / force a
+# dependency after the installation Therefore I need to trigger it this way.
+If ($filename -Match "Update") {
+  $updatePackageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    isoFile        = $installPath
+    softwareName   = $packageArgs['softwareName']
+    file           = $updateFileLocation
+    fileType       = 'exe'
+    silentArgs     = "VBR_AUTO_UPGRADE=1 /silent /noreboot /log `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+    validExitCodes = @(0,1641,3010)
+    destination    = $toolsDir
+  }
+
+  Install-ChocolateyIsoInstallPackage @updatePackageArgs
+}
