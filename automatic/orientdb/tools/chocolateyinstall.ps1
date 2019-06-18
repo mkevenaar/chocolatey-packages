853c89ae135cbf08e9ebfb10ad34a6f941b4fddc
Maurice Kevenaar <maurice@kevenaar.name>
2019-06-18 15:28:16 +0000

    AU: 3 updated - firefox-nightly jetbrainstoolbox orientdb [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/f4cdb713cefea6c44c0e3186cfc51ff98337896d

--- automatic/orientdb/tools/chocolateyinstall.ps1
+++ automatic/orientdb/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  fileFullPath   = "$toolsdir\orientdb-3.0.20.zip"
+  fileFullPath   = "$toolsdir\orientdb-3.0.21.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
