fa01c1d84a703655103d0822e5ab84ee15925313
Maurice Kevenaar <maurice@kevenaar.name>
2019-06-20 21:15:34 +0200

    (veeam-backup-and-replication-catalog) new package

---
+++ automatic/veeam-backup-and-replication-catalog/README.md
@@ -0,0 +1,18 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/mkevenaar/chocolatey-packages@dadb5921321fb56ca5a60149612bb0fba69daa84/icons/veeam-backup-and-replication-catalog.png" width="48" height="48"/> [veeam-backup-and-replication-catalog](https://chocolatey.org/packages/veeam-backup-and-replication-catalog)
+
+Veeam Backup Catalog is a feature that stands for VM guest OS file indexing. Veeam Backup Catalog comprises Veeam Guest Catalog services that run on the following servers in the backup infrastructure: Veeam backup server and Veeam Backup Enterprise Manager server.
+To have choco remember parameters on upgrade, be sure to set `choco feature enable -n=useRememberedArgumentsForUpgrades`.
+
+### Package Parameters
+The package accepts the following optional parameters:
+* `/catalogLocation` - Specifies a path to the catalog folder where index files must be stored. By default, Veeam Backup & Replication creates the VBRCatalog folder on a volume with the maximum amount of free space
+* `/username` - Specifies a user account under which the Veeam Guest Catalog Service will run. If you do not specify this parameter, the Veeam Guest Catalog Service will run under the Local System account.
+* `/password` - This parameter must be used if you have specified the /username parameter. Specifies a password for the account under which the Veeam Guest Catalog Service will run.
+* `/port` - Specifies a TCP port that will be used by the Veeam Guest Catalog Service. By default, port number 9393 is used.
+* `/create` - Create the requested user on this machine, this user will be added to the local Administrators group.
+
+Example: `choco install veeam-backup-and-replication-catalog --params "/port:9000 /catalogLocation:D:\VBRCatalog"`
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/veeam-backup-and-replication-catalog/tools/chocolateyinstall.ps1
@@ -0,0 +1,87 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$isoPackageName = 'veeam-backup-and-replication-iso'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
+$filename = 'VeeamBackup&Replication_9.5.4.2753.Update4a.iso'
+$installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
+
+$fileLocation = 'Catalog\VeeamBackupCatalog64.msi'
+$updateFileVersion = ($filename -replace 'VeeamBackup&Replication_' -replace '.iso').ToLower()
+$updateFileLocation = "Updates\veeam_backup_$($updateFileVersion)_setup.exe"
+
+$pp = Get-PackageParameters
+
+$silentArgs = ""
+
+if ($pp.catalogLocation) {
+  $silentArgs += " VM_CATALOGPATH=`"$($pp.catalogLocation)`""
+  if(!(Test-Path -Path $pp.catalogLocation )){
+    New-Item -Path $pp.catalogLocation -ItemType Directory
+  }
+}
+
+if ($pp.port) {
+  $silentArgs += " VBRC_SERVICE_PORT=$($pp.port)"
+}
+
+if ($pp.username) {
+  $computername = net user | Where-Object { $_ -match "\\\\(.+)" } | ForEach-Object { $Matches[1] }
+  $fulluser = $pp.username
+  if (-not $pp.username -contains "\") {
+    $fulluser = "$computername\$pp.username"
+  }
+  if(-not $pp.password) {
+    throw 'Password is required when setting a username...'
+  }
+  if ($pp.create) {
+    if ($pp.username -contains "\") {
+      throw "Only local users can be created"
+    }
+
+    # This is POSH3 or higher only.
+    if (Get-LocalUser -Name $pp.username -ErrorAction SilentlyContinue) {
+      Write-Warning "The local user already exists, not creating again"
+    } else {
+      New-LocalUser -Name $pp.username -Password $pp.password -AccountNeverExpires -UserMayNotChangePassword -Confirm:$false
+      Add-LocalGroupMember -Group "Administrators" -Member $pp.username -Confirm:$false
+    }
+  }
+  $silentArgs += " VBRC_SERVICE_USER=`"$($fulluser)`" VBRC_SERVICE_PASSWORD=`"$($pp.password)`""
+}
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  isoFile        = $installPath
+  softwareName  = 'Veeam Backup Catalog*'
+  file           = $fileLocation
+  fileType       = 'msi'
+  silentArgs     = "$($silentArgs) ACCEPTEULA=YES ACCEPT_THIRDPARTY_LICENSES=1 /qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  validExitCodes = @(0,1641,3010)
+  destination    = $toolsDir
+}
+
+Install-ChocolateyIsoInstallPackage @packageArgs
+
+# An update file is provided in the ISO. This should be executed after
+# the installation of the software.
+# This update file contains an update to all available software on the
+# ISO and should be executed After each installation.
+#
+# This should be in a seperate package, but you can't trigger / force a
+# dependency after the installation Therefore I need to trigger it this way.
+If ($filename -Match "Update") {
+  $updatePackageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    isoFile        = $installPath
+    softwareName   = $packageArgs['softwareName']
+    file           = $updateFileLocation
+    fileType       = 'exe'
+    silentArgs     = "VBR_AUTO_UPGRADE=1 /silent /noreboot /log `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+    validExitCodes = @(0,1641,3010)
+    destination    = $toolsDir
+  }
+
+  Install-ChocolateyIsoInstallPackage @updatePackageArgs
+}
