c370c90612c5d666f3f9dde945069a2537e03f11
Maurice Kevenaar <maurice@kevenaar.name>
2019-07-05 10:08:03 +0200

    (evernote) added OS detection

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -1,5 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$osInfo = Get-WmiObject Win32_OperatingSystem | Select-Object Version, ProductType, Caption, OperatingSystemSKU, BuildNumber
+
+Write-host "Detected:  $($osInfo.Caption)" -ForegroundColor Cyan
+
+$osInfo.Version = [version]$osInfo.Version
+if ($osInfo.Version -lt [version]'6.1') {
+   Throw 'Evernote requires Windows 7 or later.'
+}
+elseif ($osInfo.ProductType -ne 1) {
+   Throw 'Evernote cannot be installed on Windows Server.'
+}
+
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://cdn1.evernote.com/win6/public/Evernote_6.19.2.8555.exe'
 $checksum     = 'C38F178399CBD50D39B5941A1300D3737DA2224D77F349EB6DBC4912AAEF1115'
@@ -18,4 +30,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
