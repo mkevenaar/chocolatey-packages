bca7ad06485bb5adbe39f274f6aa6cd04de6f4fd
Maurice Kevenaar <maurice@kevenaar.name>
2019-07-10 18:27:31 +0000

    AU: 2 updated - jetbrains-rider vscode-vsliveshare [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/35db4a88e7f9e7279bfb7346373b312ace376cb9

--- automatic/jetbrains-rider/tools/chocolateyInstall.ps1
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2019.1.2.exe'
-$checksum     = '88edebdeebb9d54eb3b53bd044256ad4f40f34daa5dc00d8fc904db1335467c6'
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2019.1.3.exe'
+$checksum     = '060f43b3b663686fe0b58e93168c935ea990cc5725282e9e3b6d48b798bc8ac6'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
