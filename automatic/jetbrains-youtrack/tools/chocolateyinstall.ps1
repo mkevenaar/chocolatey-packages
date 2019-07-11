7d3f088e8998418ddbf15211da253bb5c6435a98
Maurice Kevenaar <maurice@kevenaar.name>
2019-07-11 00:25:59 +0000

    AU: 1 updated - jetbrains-youtrack [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/ea44d1754693cc90a894cb2df422a37d2ecd4440

--- automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
+++ automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/charisma/youtrack-2019.2.53938.msi'
-$checksum     = '6aacff345c0dd4fa54aecd2459cee692344e0add7eac7b220bb9f2b5c4924737'
+$url          = 'https://download.jetbrains.com/charisma/youtrack-2019.2.54033.msi'
+$checksum     = 'd16f33402eb9993f7e162aaee193462817356b5eb96cb1811f952dbfcdbcb0b4'
 $checksumType = 'sha256'
 
 $packageArgs = @{
