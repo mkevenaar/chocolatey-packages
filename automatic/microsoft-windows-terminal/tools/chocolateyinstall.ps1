2fd96986ec1d5d7a86f668412f479800bd9af893
Maurice Kevenaar <maurice@kevenaar.name>
2019-07-16 14:05:06 +0200

    (microsoft-windows-terminal) New Package

---
+++ automatic/microsoft-windows-terminal/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName       = "$toolsDir\Microsoft.WindowsTerminal_0.2.1831.0_8wekyb3d8bbwe.msixbundle"
+
+$WindowsVersion=[Environment]::OSVersion.Version
+if ($WindowsVersion.Major -ne "10") {
+  throw "This package requires Windows 10."
+}
+#The .msixbundle format is not supported on Windows 10 version 1709 and 1803. https://docs.microsoft.com/en-us/windows/msix/msix-1709-and-1803-support
+$IsCorrectBuild=[Environment]::OSVersion.Version.Build
+if ($IsCorrectBuild -lt "17763") {
+  throw "This package requires at least Windows 10 version 1809/OS build 17763.x."
+}
+
+Add-AppxPackage -Path $fileName
---
+++ automatic/microsoft-windows-terminal/update.ps1
@@ -0,0 +1,40 @@
+Import-Module au
+
+$releases = 'https://github.com/microsoft/terminal/releases'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    #Microsoft.WindowsTerminal_0.2.1831.0_8wekyb3d8bbwe.msixbundle
+    $re  = "Microsoft.WindowsTerminal_(.+)_.*.msixbundle"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ([regex]::Match($url,$re)).Captures.Groups[1].value
+    $url = 'https://github.com' + $url
+
+    return @{ 
+        URL32 = $url
+        Version = $version 
+        FileType = 'msixbundle'
+    }
+}
+
+function global:au_SearchReplace {
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]fileName\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+    }
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+    }
+  }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor None
+}
