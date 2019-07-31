2b44a5832f254561a854eca1a80b5639a39045c4
Maurice Kevenaar <maurice@kevenaar.name>
2019-07-31 00:27:32 +0000

    AU: 1 updated - zerotier-one [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/6bae94f1c7c1ed350dd03ce829c75662bb0ef64b

--- automatic/zerotier-one/tools/chocolateyinstall.ps1
+++ automatic/zerotier-one/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.zerotier.com/RELEASES/1.2.12/dist/ZeroTier%20One.msi'
-$checksum     = '05e9f984a53bb38c73df850c235933aa178343d412e616591ff058f731881f53'
+$url          = 'https://download.zerotier.com/RELEASES/1.4.0/dist/ZeroTier%20One.msi'
+$checksum     = '6a5111a13f5af9dafcf8be713cad008cc7e8c567fe44275f85a141048539ddb4'
 $checksumType = 'sha256'
 
 $packageArgs = @{
