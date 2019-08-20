3891769342daa20a0ac76898f53fbb73a8f6d737
Maurice Kevenaar <maurice@kevenaar.name>
2019-08-20 15:27:11 +0000

    AU: 3 updated - firefox-dev firefox-nightly orientdb [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/45dd1f1d5dfc09c6e2ad4c767faa66157027b9d3

--- automatic/orientdb/tools/chocolateyinstall.ps1
+++ automatic/orientdb/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  fileFullPath   = "$toolsdir\orientdb-3.0.22.zip"
+  fileFullPath   = "$toolsdir\orientdb-3.0.23.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
