1f4989580d8fc1211321a1af3b6b5601c9642acc
Maurice Kevenaar <maurice@kevenaar.name>
2019-08-23 12:26:51 +0000

    AU: 2 updated - lastpass vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/45f56c74ff0932e70cde2e0fb8e01df80bd02903

--- automatic/lastpass/tools/chocolateyInstall.ps1
+++ automatic/lastpass/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url32          = 'https://download.cloud.lastpass.com/windows_installer/lastpass.exe'
-$checksum32     = '10ae04d5facd3fb92beae3a40dcd60e63efed6154268b0a8733ce1961ecc96b7'
+$checksum32     = 'bf77ea7beb6f5065f363e64db541f889e1c4d57e3fa36d9b94dc2d89d03dc430'
 $checksumType32 = 'sha256'
 $url64          = 'https://download.cloud.lastpass.com/windows_installer/lastpass_x64.exe'
-$checksum64     = '4f66a5c28b58811a74ed828f536c3e2d4953c0da80fdbd8627575d22a7424f68'
+$checksum64     = '21bd641143743e6ddb4e313aa7d448d2cf27c9c3914c8630f5b023723405b238'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
