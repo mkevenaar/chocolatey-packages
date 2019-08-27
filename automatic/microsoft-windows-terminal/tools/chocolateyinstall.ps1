e8223b3810a8d23b94ec058a674a9daf59c4ea7a
Maurice Kevenaar <maurice@kevenaar.name>
2019-08-27 22:46:18 +0200

    Update chocolateyinstall.ps1

--- automatic/microsoft-windows-terminal/tools/chocolateyinstall.ps1
+++ automatic/microsoft-windows-terminal/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileName       = "$toolsDir\Microsoft.WindowsTerminal_0.3.2171.0_8wekyb3d8bbwe.msixbundle"
-$version        = '0.3.2171.0'
+$version        = "0.3.2171.0"
 
 $WindowsVersion=[Environment]::OSVersion.Version
 if ($WindowsVersion.Major -ne "10") {
