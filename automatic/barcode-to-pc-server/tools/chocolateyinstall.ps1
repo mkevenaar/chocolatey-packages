0e28418606cc5c1eb13761a1187337c868e9032e
Maurice Kevenaar <maurice@kevenaar.name>
2019-09-12 18:31:02 +0000

    AU: 1 updated - barcode-to-pc-server [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/5bc51cc8b5e89cd5f1c56ba19d2f80f3d87c661d

--- automatic/barcode-to-pc-server/tools/chocolateyinstall.ps1
+++ automatic/barcode-to-pc-server/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://github.com/fttx/barcode-to-pc-server/releases/download/v3.1.1/barcode-to-pc-server.v3.1.1.win.setup.exe'
-$checksum32     = '579b5e633af331fd0078301b651a8414db97aa0f8760c5ee67e4ec1d003df2d7'
+$url32          = 'https://github.com/fttx/barcode-to-pc-server/releases/download/v3.1.4/barcode-to-pc-server.v3.1.4.win.setup.exe'
+$checksum32     = '4333355acd5ef9287d0fd357fd9e7c87ba3a38034c199c5a20e1b7ea11b0043e'
 $checksumType32 = 'sha256'
 
 $packageArgs = @{
