30d125af1a0cbad4a70af36eff30537e71c8f458
Maurice Kevenaar <maurice@kevenaar.name>
2019-10-03 11:40:09 +0200

    (lastpass) fix checksum (agian)

--- automatic/lastpass/tools/chocolateyInstall.ps1
+++ automatic/lastpass/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url32          = 'https://download.cloud.lastpass.com/windows_installer/lastpass.exe'
-$checksum32     = 'b05d16efd037e084be674089c9f4a68ff5518d6e48573e51f26a3848705de989'
+$checksum32     = 'e4248bd0c2e82c08c79063686587b0451351f72186883fecb6796cad519404c6'
 $checksumType32 = 'sha256'
 $url64          = 'https://download.cloud.lastpass.com/windows_installer/lastpass_x64.exe'
-$checksum64     = '2401c795e7584c3a185e221cba77dbb422a82aa4ce97936534066f5ee9c79005'
+$checksum64     = 'b4ee78aff49c6213a97641179e5332b7d7125100f3d306f3ec5a18d0be247770'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
