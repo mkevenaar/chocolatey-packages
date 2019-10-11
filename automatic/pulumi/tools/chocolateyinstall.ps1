4c7dceec58be72a5d52a0b31c901e9beca6d3d1d
Maurice Kevenaar <maurice@kevenaar.name>
2019-10-11 03:26:02 +0000

    AU: 2 updated - firefox-nightly pulumi [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/6379019853ee30e6751461ad95d10a861538548e

--- automatic/pulumi/tools/chocolateyinstall.ps1
+++ automatic/pulumi/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file64         = "$toolsDir\pulumi-v1.3.0-windows-x64.zip"
+  file64         = "$toolsDir\pulumi-v1.3.1-windows-x64.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
