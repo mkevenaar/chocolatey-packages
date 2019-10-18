8ab3ea46db1819a07b79125d777b4641bff915b6
Maurice Kevenaar <maurice@kevenaar.name>
2019-10-18 15:27:46 +0000

    AU: 2 updated - duplicatefilefinder firefox-dev [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/a6ff02c5867da28b3da7a6f819e6f3383736ee52

--- automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
+++ automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
-$checksum     = '4fbd4392f52bac4528b5acf0ecffe49d0d3e3b44ef7e97fff42023bdbdbcae70'
+$checksum     = '7ad4c882b4261f048d214f74fca7e65f799b556087555d1e385eb25eb78b043a'
 $checksumType = 'sha256'
 
 $packageArgs = @{
