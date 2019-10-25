ba950c542597f87e3912da0dfb643ed193453d41
Maurice Kevenaar <maurice@kevenaar.name>
2019-10-25 14:41:53 +0000

    AU: 3 updated - wc3270 wc3270.install wc3270.portable [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/0d882e0ab835653d3acfb226e3cf83f96002ae15

--- automatic/wc3270.portable/tools/chocolateyinstall.ps1
+++ automatic/wc3270.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file          = "$toolsDir\helio-2.3-x32.zip"
-  file64        = "$toolsDir\helio-2.3-x64.zip"
+  file          = "$toolsDir\wc3270-3.6ga8-noinstall-32.zip"
+  file64        = "$toolsDir\wc3270-3.6ga8-noinstall-64.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
