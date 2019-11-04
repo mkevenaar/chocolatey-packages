eae95f2954f7c82bb37ee0ac73e904616962b4f7
Maurice Kevenaar <maurice@kevenaar.name>
2019-11-04 21:27:14 +0000

    AU: 1 updated - pulumi [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/4f2eb10e2fc77f88080d52075435a32ff8ae7d23

--- automatic/pulumi/tools/chocolateyinstall.ps1
+++ automatic/pulumi/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file64         = "$toolsDir\pulumi-v1.4.0-windows-x64.zip"
+  file64         = "$toolsDir\pulumi-v1.4.1-windows-x64.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
