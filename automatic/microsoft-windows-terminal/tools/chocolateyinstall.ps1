f404d45dc122ef57a203b4a753cf739166baf008
Maurice Kevenaar <maurice@kevenaar.name>
2019-11-26 17:32:31 +0000

    AU: 1 updated - microsoft-windows-terminal [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/197b1fb029db8f7542cbc8fe7974ca8b4c16cd90

--- automatic/microsoft-windows-terminal/tools/chocolateyinstall.ps1
+++ automatic/microsoft-windows-terminal/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName       = "$toolsDir\Microsoft.WindowsTerminal_0.6.2951.0_8wekyb3d8bbwe.msixbundle"
-$version        = "0.6.2951.0"
+$fileName       = "$toolsDir\Microsoft.WindowsTerminal_0.7.3291.0_8wekyb3d8bbwe.msixbundle"
+$version        = "0.7.3291.0"
 
 $WindowsVersion=[Environment]::OSVersion.Version
 if ($WindowsVersion.Major -ne "10") {
