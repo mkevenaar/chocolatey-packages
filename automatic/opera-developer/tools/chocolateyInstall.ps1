71be86c86a9b450402a758d0a0577ef90d97fef1
Maurice Kevenaar <maurice@kevenaar.name>
2019-11-27 15:26:45 +0000

    AU: 4 updated - firefox-nightly opera-developer rocrail vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/6b5880d9668b89642d7f46e87099aabc13c8ab7d

--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/66.0.3511.0/win/Opera_Developer_66.0.3511.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/66.0.3511.0/win/Opera_Developer_66.0.3511.0_Setup_x64.exe'
-  checksum       = '14c982fad5d8d42e392dff12248631e05c58ff4f903b18fa87242060aefb8c80'
-  checksum64     = '717b9d1377c98fe70822a5a540e3a82f242fb72afa7f35613ab39aad9eefe5f9'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/66.0.3515.2/win/Opera_Developer_66.0.3515.2_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/66.0.3515.2/win/Opera_Developer_66.0.3515.2_Setup_x64.exe'
+  checksum       = '40e1271228dd647700be3b8a57e431f63ae779840f2422f62fde6a25868dbac4'
+  checksum64     = '58ac05bcec94d798a1aa8e90926f801634dc06683d6ff292a425b1bce5c632be'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '66.0.3511.0'
+$version = '66.0.3515.2'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-221-win32.exe'
-$checksum32     = '726c163613d6602b2bf5cc24ca20d6989bc74d7994564baec62b75340686cc45'
+$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-227-win32.exe'
+$checksum32     = '67a2ff61f43d156e01fd1ab0e76de598f130692fece1317aa4469c4ec49b5776'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-221-win64.exe'
-$checksum64     = 'b478b31ca34af5427d419eb7add7d657485cfc521f18c582a553e2ff21eff41c'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-227-win64.exe'
+$checksum64     = '06d3f8b63a15f910f2a6906c03debdee6bc8785acb1862e93cc49e0cab1c0e76'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
