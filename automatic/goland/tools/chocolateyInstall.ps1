a3842f185c4a26db97986617301342f16cb560bc
Maurice Kevenaar <maurice@kevenaar.name>
2019-11-29 15:26:03 +0000

    AU: 2 updated - firefox-nightly goland [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/4a4a69bfa1a14ab2cc30e86d087886a353d733c6

--- automatic/goland/tools/chocolateyInstall.ps1
+++ automatic/goland/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/go/goland-2019.2.5.exe'
-$checksum     = '06080b74909ee1b54e349374f8d6dab63da92e72275d8b63e9d600f31d39e608'
+$url          = 'https://download.jetbrains.com/go/goland-2019.3.exe'
+$checksum     = '5a76ccd7d87b7b91c79d926ad43ceb26669d833c2ae7dacf51a3a5b4aea236e5'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
