ed085be985cd75eea4246321e3513af852eca051
Maurice Kevenaar <maurice@kevenaar.name>
2019-12-04 15:27:32 +0000

    AU: 2 updated - firefox-nightly server-backup-agent [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/f6baa0ce50ef1c633fc5b631b77eecf63656582f

--- automatic/server-backup-agent/tools/chocolateyinstall.ps1
+++ automatic/server-backup-agent/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url32          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x86.msi'
-$checksum32     = '5DCC7619EF12E22220114226382A91016F6B6210A3F56B4B6F18148A4120370C'
+$checksum32     = '307CA893374878051455B9E676E20BCB6D6E0C5551BCFC7D1F0844B712643632'
 $checksumType32 = 'sha256'
 $url64          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x64.msi'
-$checksum64     = '6cc38eabed3fffbd1fbc88a23d216ba1aab057a8d676b0463875a65c0c9b07ae'
+$checksum64     = 'f83a455d559ee50962ae5a0772f2a3a34cb4e821f1049da158dfab3e324d8a8f'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
