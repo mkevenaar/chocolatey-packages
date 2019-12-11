1d56d768d53ab7f0594d5b6f989af8d5246bc620
Maurice Kevenaar <maurice@kevenaar.name>
2019-12-11 09:28:15 +0000

    AU: 2 updated - vlc-nightly winrar [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/87d21d596ecde4bab4cc4d6b559f9a4b1b873103

--- automatic/winrar/tools/chocolateyInstall.ps1
+++ automatic/winrar/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $LCID = (Get-UICulture).LCID
-$url_version = '571'
+$url_version = '580'
 
 $checksumType32 = 'sha256'
 $checksumType64 = 'sha256'
