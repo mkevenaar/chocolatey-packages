71f4a6b080ea62940079c43921379292cbfbd641
Maurice Kevenaar <maurice@kevenaar.name>
2019-12-24 06:27:24 +0000

    AU: 1 updated - rocrail [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/9d5d8997890085fab8df94629d1d356988cfdfba

--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-317-win32.exe'
-$checksum32     = 'c78f3c5c2bed4f1da8f290a42b55a79a05a126fe91852bf8e396b1f05fd51ac8'
+$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-319-win32.exe'
+$checksum32     = '6d8c345c53db6be2d6e929006cc64eee7530fa300e158ff0185836e83f0f7693'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-317-win64.exe'
-$checksum64     = '6ad34e7ad388b174be01b2093ba6cfc86626383f7061d54b594422bc36bb2bbd'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-319-win64.exe'
+$checksum64     = '4ac7dff0969f67b36c4ac54531db6b3fad43b6e721625e6c2c629140d64d5f13'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
