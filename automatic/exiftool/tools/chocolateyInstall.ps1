d8cbf8e0eb6da22d66d8c91147c874ca7501bcb5
Maurice Kevenaar <maurice@kevenaar.name>
2020-01-02 14:03:10 +0000

    AU: 2 updated - exiftool winrar [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/ca5cb5981793a57e6c02f2008d6b43d6cdec5213

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.80.zip'
-$checksum     = '86af71e56f22d2b78cbc85c9210c7cd04e2de6f7d2f625a3366da53eca8c89cb'
+$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.81.zip'
+$checksum     = '8659321b52c49d18f52f571e9f6188eed279eb01956c16bc28097d0951d3b692'
 $checksumType = 'sha256'
 
 $packageArgs = @{
