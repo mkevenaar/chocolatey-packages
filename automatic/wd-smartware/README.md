803f9714370e899358b0dada5689b1872397770f
Maurice Kevenaar <maurice@kevenaar.name>
2020-01-13 20:32:35 +0100

    (wd-smartware) New package

---
+++ automatic/wd-smartware/README.md
@@ -0,0 +1,7 @@
+# <img src="https://cdn.jsdelivr.net/gh/mkevenaar/chocolatey-packages@85080cd8d1b6a862fb4ebf61ad63632819ffe416/icons/wd-smartware.png" width="48" height="48"/> [wd-smartware](https://chocolatey.org/packages/wd-smartware)
+
+WD SmartWare software is an easy-to-use backup application that enables users to automatically protect their data through continuous or scheduled backups. These backups can be performed by categories or the selection of the desired files.
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/wd-smartware/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://downloads.wdc.com/smartware/WD_SmartWare_Installer_2.4.21.1.zip'
+$checksum = '739bc334798aa6e1ce21b5a79a8e09b0af9b70dc8e6622ddf8b41195b5d6196e'
+$checksumType = 'sha256'
+$fileLocation = Join-Path $toolsDir "WD SmartWare Installer.exe"
+
+$downloadArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage @downloadArgs
+
+$installArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'WD SmartWare*'
+  file          = $fileLocation
+  fileType      = 'exe'
+  silentArgs    = "/s"
+  validExitCodes= @(0)
+  destination   = $toolsDir
+}
+
+Install-ChocolateyInstallPackage @installArgs
+
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
