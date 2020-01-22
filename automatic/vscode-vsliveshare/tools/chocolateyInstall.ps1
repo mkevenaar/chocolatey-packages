be83ecf1ac2257883667e2f5356839662e4a41a4
Maurice Kevenaar <maurice@kevenaar.name>
2020-01-22 00:27:03 +0000

    AU: 1 updated - vscode-vsliveshare [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/eedbab16ba92f34cfbeddb05df3e4c523a20d558

--- automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
+++ automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.1482'
+Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.1510'
