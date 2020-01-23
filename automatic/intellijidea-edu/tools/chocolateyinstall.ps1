1d0b37d2011743607fba46f0cd16781f49c06ae3
Maurice Kevenaar <maurice@kevenaar.name>
2020-01-23 18:28:15 +0000

    AU: 1 updated - intellijidea-edu [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/c76c63f3f7310e83e4a45719bb9df90fd712eaf7

--- automatic/intellijidea-edu/tools/chocolateyinstall.ps1
+++ automatic/intellijidea-edu/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.jetbrains.com/idea/ideaIE-2019.2.2.exe' 
-$checksum     = '9740965c5991cf82037faa351e28af1a5438b3775ebbb00dc1f1dbf054166012'
+$url = 'https://download.jetbrains.com/idea/ideaIE-2019.3.1.exe' 
+$checksum     = '419715adbf5e12a996cf93d6096e007225445ecc9033dadf2eeb61d32e895491'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
