99fffe211330c08032ff384af1a80e4c574a72dc
Maurice Kevenaar <maurice@kevenaar.name>
2020-02-04 12:43:44 +0000

    AU: 2 updated - movie-collector vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/4e4b52b8eeadc933c121ba3330c6591d8ace8c0f

--- automatic/movie-collector/tools/chocolateyinstall.ps1
+++ automatic/movie-collector/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://installers.collectorz.com/movie-win/moviecollectorsetup2001.exe'
-$checksum     = 'c64fbb5cd3fc61950979c11c897e612954e02db7af9725324e997fc47c70b21b'
+$url          = 'http://installers.collectorz.com/movie-win/moviecollectorsetup2011.exe'
+$checksum     = '09c41d7d821f6bf45d949a0c321ef845697785b22ee876ceb3ed9fbbbdf40488'
 $checksumType = 'sha256'
 
 $packageArgs = @{
