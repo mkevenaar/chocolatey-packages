9eeb18e523a37ce52df60dbda51d450e01491fb0
Maurice Kevenaar <maurice@kevenaar.name>
2020-02-07 18:43:48 +0000

    AU: 2 updated - pycharm pycharm-community [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/956b12473157e30a014f37d4b7faafa4edcc14c7

--- automatic/pycharm-community/tools/chocolateyInstall.ps1
+++ automatic/pycharm-community/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/python/pycharm-community-2019.3.2.exe'
-$checksum     = '5f32a286ca8a613fe2b908bdf83eb6553fc661132ad0b7abee4e2268c51f4487'
+$url          = 'https://download.jetbrains.com/python/pycharm-community-2019.3.3.exe'
+$checksum     = '96158e07ad24633a97cbfbdf522b0897cf81680c7f49b98b8e2f816fdfcffc0a'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
--- automatic/pycharm/tools/chocolateyinstall.ps1
+++ automatic/pycharm/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.jetbrains.com/python/pycharm-professional-2019.3.2.exe' 
-$checksum     = '23a15b62c1f5d7dca71f2e7f92ef3c7668ff099606320fdb323043a94593d713'
+$url = 'https://download.jetbrains.com/python/pycharm-professional-2019.3.3.exe' 
+$checksum     = 'e41028dbe1015c322dd9c4983472b224036488ae87ddc5531dcc8307d267b699'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
