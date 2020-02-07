86774e2422e3ee8ceb11d981fd23ace4121d2ff5
Maurice Kevenaar <maurice@kevenaar.name>
2020-02-07 12:43:40 +0000

    AU: 3 updated - duplicatefilefinder orientdb vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/722ec3699312b44f6de7ff38228e1751120961f7

--- automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
+++ automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
-$checksum     = 'fb1f751e0bb9fcf5567e4a981b63d39bfc9e514dca287a808211e0500fe9b206'
+$checksum     = 'e7625c9ed7201ad68961684f89c4ba7ba27410200692e3a48dfe952ca1ee7bdd'
 $checksumType = 'sha256'
 
 $packageArgs = @{
