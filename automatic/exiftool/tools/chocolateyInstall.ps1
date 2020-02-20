34e150ab872edded20edfa3088185c344a5547c1
Maurice Kevenaar <maurice@kevenaar.name>
2020-02-20 15:14:12 +0000

    AU: 2 updated - exiftool vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/bff7c60eb8b00f9351dee6937ef38a317704aaad

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.87.zip'
-$checksum     = 'aee46f0b7ee5c9ed30d4e4361f6c1c59f17debc53b3d67ced013e3aac24c67a5'
+$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.88.zip'
+$checksum     = '40d9250828dd2e0beb95c013bec83fee86e799e1cc7d86b8d611232ba07e58f2'
 $checksumType = 'sha256'
 
 $packageArgs = @{
