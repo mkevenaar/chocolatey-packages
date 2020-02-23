61fd9b51c8460309049d505339b53ffb69426ce3
Maurice Kevenaar <maurice@kevenaar.name>
2020-02-23 23:13:28 +0000

    AU: 3 updated - wc3270 wc3270.install wc3270.portable [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/3c0653ce5892881864ced26d5c6d3d8e461ddfa7

--- automatic/wc3270.portable/tools/chocolateyinstall.ps1
+++ automatic/wc3270.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file          = "$toolsDir\wc3270-3.6ga9-noinstall-32.zip"
-  file64        = "$toolsDir\wc3270-3.6ga9-noinstall-64.zip"
+  file          = "$toolsDir\wc3270-3.6ga10-noinstall-32.zip"
+  file64        = "$toolsDir\wc3270-3.6ga10-noinstall-64.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
