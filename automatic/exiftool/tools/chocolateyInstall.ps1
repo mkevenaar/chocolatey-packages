ffd52c4235e5bb0a0596092d62eaa17175a9ef01
Maurice Kevenaar <maurice@kevenaar.name>
2020-03-03 14:13:50 +0000

    AU: 2 updated - exiftool orientdb [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/989419c290421cd5ab239383a7a4c392176b9ac6

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.89.zip'
-$checksum     = 'c8d69e35da8283dbe3cae296df0b52e25e1905b83f95b84547236272e0d33365'
+$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.90.zip'
+$checksum     = 'd0a1b9b33824a7ca69ad3b1f179b744f7a771dfc4633748920d1998f78bb5178'
 $checksumType = 'sha256'
 
 $packageArgs = @{
--- automatic/orientdb/tools/chocolateyinstall.ps1
+++ automatic/orientdb/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  fileFullPath   = "$toolsdir\orientdb-3.0.28.zip"
+  fileFullPath   = "$toolsdir\orientdb-3.0.29.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
