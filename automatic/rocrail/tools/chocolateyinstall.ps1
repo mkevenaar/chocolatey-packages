2cbc8befe3c4e0c977021d191fd212dc981bc462
Maurice Kevenaar <maurice@kevenaar.name>
2020-03-05 06:13:16 +0000

    AU: 3 updated - iobit-malware-fighter rocrail vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/7c85f462e360b3eff3cccefea337d00587c1d420

--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-527-win32.exe'
-$checksum32     = '119de0129bc4440d711e25a41aa2f297d7cf3da1b6f089a9cfbc1fc9d667ffbb'
+$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-531-win32.exe'
+$checksum32     = '086631fa722e55b56fea653d80450ca2037077650668058097d91461819e95a1'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-527-win64.exe'
-$checksum64     = 'a58cb2e0a6f88d18634a3184a8ccb309f54cc9719924a66a7a86a2253d82551c'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-531-win64.exe'
+$checksum64     = 'bbf5b9240644b81b0a6f58be3dc4991b08f220d598970a4fc483c6a55b3b32cf'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
