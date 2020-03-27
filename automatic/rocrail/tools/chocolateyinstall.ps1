ffab066b3259c3d574e4f498985a6b1aa5939f78
Maurice Kevenaar <maurice@kevenaar.name>
2020-03-27 06:13:36 +0000

    AU: 2 updated - rocrail vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/b60f95b06659cdba55a1f4e815d29a7b7be364d2

--- automatic/rocrail/tools/chocolateyinstall.ps1
+++ automatic/rocrail/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-571-win32.exe'
-$checksum32     = '005ed5dce3320695d3bb37cab8552d43004fa4581a4e5bfb4a68c7fd8ad67d0c'
+$url32          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-574-win32.exe'
+$checksum32     = '74df4f288035ac2a3e0ec14c44d666ee55eb95e3600b29332e32fda0919e2b6a'
 $checksumType32 = 'sha256'
-$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-571-win64.exe'
-$checksum64     = 'cbb1a0fc79c6fdf009c22609032c8f5b906f8fd53c83bd2bed9fba4cc1edfbe2'
+$url64          = 'https://wiki.rocrail.net/rocrail-snapshot/history/rocrail-574-win64.exe'
+$checksum64     = '071e708d3aad57e8449936f7b238f4a8ef5758f98201fafb5220e00a604a1a21'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
