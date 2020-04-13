bd57da055655f78b97dc722ba47092774a3afa55
Maurice Kevenaar <maurice@kevenaar.name>
2020-04-13 10:59:41 +0000

    AU: 2 updated - anyrail6 firefox-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/44cc5f69a3e6d84be5fd692e669b60e8ad4c7cef

--- automatic/anyrail6/tools/chocolateyinstall.ps1
+++ automatic/anyrail6/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.anyrail.com/downloads/AnyRail/6.27/AnyRail6.27.0.msi'
-$checksum     = 'c0c9b1469b3cd8d8b6348f5b7c1443f765b0487676ba99ce742b97498afe0b8e'
+$url          = 'https://www.anyrail.com/downloads/AnyRail/6.28/AnyRail6.28.0.msi'
+$checksum     = 'dc9a0c8094b4bde84bfcaa1443aca1c98d6e21f33ee88225ce56a67f87313f5d'
 $checksumType = 'sha256'
 
 $packageArgs = @{
