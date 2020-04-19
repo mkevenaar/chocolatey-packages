d4992d149b2fa1ecf6f58bc84d51aca47aa34443
Maurice Kevenaar <maurice@kevenaar.name>
2020-04-19 11:38:57 +0200

    (wd-backup) new package

---
+++ automatic/wd-backup/README.md
@@ -0,0 +1,7 @@
+# <img src="https://cdn.jsdelivr.net/gh/mkevenaar/chocolatey-packages@a00d45ff3710fc93838077c18b700835e4a0ff0e/icons/wd-backup.png" width="48" height="48"/> [wd-backup](https://chocolatey.org/packages/wd-backup)
+
+WD Backup is an easy-to-use backup application. It automatically protects your files, photos and documents with on premise backups to a WD drive as primary storage. Off-premise backups to a cloud as secondary storage for disaster recovery purpose.
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/wd-backup/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://downloads.wdc.com/wdapp/WD_Backup_1.9.7375.5719.zip'
+$checksum = 'ecc9c4d8f790c2673e2e216ee5192cbe3bb6d3d36b016461aa7398e84d3eda6d'
+$checksumType = 'sha256'
+$fileLocation = Join-Path $toolsDir "WD Backup.exe"
+
+$downloadArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage @downloadArgs
+
+$installArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'WD Backup*'
+  file          = $fileLocation
+  fileType      = 'exe'
+  silentArgs    = "/s"
+  validExitCodes= @(0)
+  destination   = $toolsDir
+}
+
+Install-ChocolateyInstallPackage @installArgs
+
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
