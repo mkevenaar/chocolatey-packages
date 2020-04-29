30377515bb2251114d3128e7df899ded2a750821
Maurice Kevenaar <maurice@kevenaar.name>
2020-04-29 19:13:47 +0000

    AU: 2 updated - clion-ide powertoys [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/c0030e8910fc368dbf18b54a74c2564cddefb58b

--- automatic/clion-ide/tools/chocolateyinstall.ps1
+++ automatic/clion-ide/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/cpp/CLion-2020.1.exe'
-$checksum     = '89811fa993f977a36ae8dabe08a4357645ebb50685d40213fd0ade89dcafa0b0'
+$url          = 'https://download.jetbrains.com/cpp/CLion-2020.1.1.exe'
+$checksum     = '3e83f5eec7ed71fe075a439822b0caaeabf2f9e87c36919246ebae6d44506367'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
