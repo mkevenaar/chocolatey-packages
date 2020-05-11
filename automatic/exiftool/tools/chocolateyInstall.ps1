632079ba4c7f0d2827fc1dcd16d39e1af26545f2
Maurice Kevenaar <maurice@kevenaar.name>
2020-05-11 15:14:36 +0000

    AU: 2 updated - exiftool firefox-dev [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/cab48ee3f0e69a4b5a0c327a6134483176ee3ec4

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-11.96.zip'
-$checksum     = 'fbbfbbce9f859497527d514881a93a8808d3f94af5342c375692bd2a54b235e8'
+$url          = 'https://exiftool.org/exiftool-11.99.zip'
+$checksum     = '0980683f7c16a58a85f5769e462ae92bcabbe022a7aafafceb51d2343cc3c69e'
 $checksumType = 'sha256'
 
 $packageArgs = @{
