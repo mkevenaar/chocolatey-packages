21fd03d10a63c44dc3b25d4715a9f62d5c299544
Maurice Kevenaar <maurice@kevenaar.name>
2020-05-11 10:12:56 +0200

    (vlc-nightly) updated update script

--- automatic/vlc-nightly/update.ps1
+++ automatic/vlc-nightly/update.ps1
@@ -1,5 +1,5 @@
 import-module au
-$releases = 'https://nightlies.videolan.org'
+$releases = 'https://artifacts.videolan.org'
 
 function global:au_SearchReplace {
    @{
@@ -26,39 +26,46 @@ function CreateStream {
 
 function global:au_GetLatest {
   $date = Get-Date  -Format yyyyMM
-  #win64
-  $download_page64 = Invoke-WebRequest -Uri "$releases/build/win64/" -UseBasicParsing
-
-  $folder64Bits = $download_page64.links | Where-Object href -Match "vlc-.+$date" | Select-Object -first 1
-
-  $re    = '\.exe$'
 
   $streams = @{ }
 
-  $folder64Bits | Sort-Object | Foreach-Object {
-    # Get the version number from the current folder
-    [regex]$dotre = '\.'
-    $version = $_.href -split '-' | Select-Object -first 1 -Skip 1
-    $chocoDate = $_.href -split '-' | Select-Object -first 1 -Skip 2
+  $re    = '\.exe$'
 
-    if ( $dotre.matches($version).count -eq 2 ) {
-      $version = $version + "."
-    }
+  $folders = @(
+    "/vlc/nightly-win64/"
+    "/vlc-3.0/nightly-win64/"
+  )
 
-    $chocoVersion = $version + $chocoDate
+  foreach ($folder in $folders) {
+    $download_page64 = Invoke-WebRequest -Uri "$releases$folder" -UseBasicParsing
 
-    Write-Host $chocoVersion
+    $folder64Bits = $download_page64.links | Where-Object href -Match "$date" | Select-Object -first 1
 
-    # 64 bit
-    $64bitFolder = $_.href
-    $64bitFolderUrl = $releases + "/build/win64/" + $64bitFolder
-    $64bitFiles = Invoke-WebRequest -Uri $64bitFolderUrl -UseBasicParsing
-    $filename64 = $64bitFiles.links | Where-Object href -Match $re | Select-Object -ExpandProperty href
-    $url64bit = $releases + "/build/win64/" + $64bitFolder + $filename64
+    $folder64Bits | Sort-Object | Foreach-Object {
+      # Get the version number from the current folder
+      [regex]$dotre = '\.'
 
-    $streams.Add((Get-Version $version).ToString(2), (CreateStream $url64Bit $chocoVersion))
+      # 64 bit
+      $64bitFolder = $_.href
+      $64bitFolderUrl = $releases + $folder + $64bitFolder
+      $64bitFiles = Invoke-WebRequest -Uri $64bitFolderUrl -UseBasicParsing
+      $filename64 = $64bitFiles.links | Where-Object href -Match $re | Select-Object -ExpandProperty href
+      $url64bit = $releases + $folder + $64bitFolder + $filename64
 
-  } | Out-Null
+      $version = $filename64 -split '-' | Select-Object -first 1 -Skip 1
+      $chocoDate = $_.href -split '-' | Select-Object -first 1
+
+      if ( $dotre.matches($version).count -eq 2 ) {
+        $version = $version + "."
+      }
+
+      $chocoVersion = $version + $chocoDate
+
+      $streams.Add((Get-Version $version).ToString(2), (CreateStream $url64Bit $chocoVersion))
+
+    } | Out-Null
+
+  }
 
   return @{ Streams = $streams }
 }
