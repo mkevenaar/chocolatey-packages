05cb8c93eed7de3d6a8cdd48c03a66d8d2d9cb8a
Maurice Kevenaar <maurice@kevenaar.name>
2020-07-09 08:13:38 +0000

    AU: 1 updated - iobit-uninstaller [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/e06a1d0774969415a30747db9f540e49ff6249c1

--- automatic/iobit-uninstaller/tools/chocolateyInstall.ps1
+++ automatic/iobit-uninstaller/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://update.iobit.com/dl/iobituninstaller.exe'
-$checksum     = '107355A11DDD664FDBBB39BC25C9A13B6C02A1636EA28CC295A7145F7ED264A5'
+$checksum     = 'A040E4B0DF412E40652C252C5B67D219C184D36640B020C9ABA286ED8D1B4BD3'
 $checksumType = 'sha256'
 
 $packageArgs = @{
