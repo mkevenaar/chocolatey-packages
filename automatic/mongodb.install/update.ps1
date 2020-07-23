f41cdc7249500e1916989f8b96b80c4af4d62636
Maurice Kevenaar <maurice@kevenaar.name>
2020-07-23 09:17:03 +0200

    (MongoDB) change in filename format

--- automatic/mongodb.install/update.ps1
+++ automatic/mongodb.install/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
 
     $version = (($download_page.Links | Where-Object href -Match "releases/tag" | Where-Object href -NotMatch "rc" | Select-Object -First 1 -ExpandProperty href) -Split "/" | Select-Object -Last 1) -replace "^r"
 
-    $url = 'https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2012plus-' + $version + '-signed.msi'
+    $url = 'https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-' + $version + '-signed.msi'
 
     $url64   = $url
     return @{ URL64=$url64; Version = $version }
