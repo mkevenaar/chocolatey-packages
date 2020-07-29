cd67b96c71d8dc343ac2fb2fad906f18e0004717
Maurice Kevenaar <maurice@kevenaar.name>
2020-07-29 20:13:46 +0000

    AU: 1 updated - exiftool [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/adebf8a56ad8156af9f512521d8272033c7faf9d

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://exiftool.org/exiftool-12.02.zip'
-$checksum     = '683bd03ae607a9788447b2934142605848e5e037f9f36275c7a3fc587f9151ca'
+$url          = 'https://exiftool.org/exiftool-12.03.zip'
+$checksum     = 'c77854e541763edcd0d31d5d1f9f3fc03c6cbb5a78334cbe0374778a8144a5a7'
 $checksumType = 'sha256'
 
 $packageArgs = @{
