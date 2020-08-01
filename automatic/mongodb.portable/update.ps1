2b4a407ac5b2ea65ac74b81f6f0e94e191e695b2
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-01 14:00:51 +0200

    (mongodb) URL changed

--- automatic/mongodb.portable/update.ps1
+++ automatic/mongodb.portable/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
 
   $version = (($download_page.Links | Where-Object href -Match "releases/tag" | Where-Object href -NotMatch "rc" | Select-Object -First 1 -ExpandProperty href) -Split "/" | Select-Object -Last 1) -replace "^r"
 
-  $url = 'https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-' + $version + '.zip'
+  $url = 'https://fastdl.mongodb.org/win32/mongodb-windows-x86_64-' + $version + '.zip'
 
   $url64   = $url
   return @{ URL64=$url64; Version = $version }
