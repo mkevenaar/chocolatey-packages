eb1de6c6d4bb6a3b1efd346d0a3b79ccf1dc28e9
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-01 00:13:58 +0000

    AU: 2 updated - nsis nsis.portable [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/3e0473f949cc25aa5478ba5ce0d22b2e8411bd49

--- automatic/nsis.portable/tools/chocolateyInstall.ps1
+++ automatic/nsis.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   Destination    = $toolsDir
-  FileFullPath   = "$toolsDir\nsis-3.05.zip"
+  FileFullPath   = "$toolsDir\nsis-3.06.zip"
 }
 
 Get-ChocolateyUnzip  @packageArgs
