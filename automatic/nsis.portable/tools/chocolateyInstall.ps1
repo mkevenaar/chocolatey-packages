e7aabd5c2d35b2b490450e5032a47150966fb218
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-03 17:13:49 +0000

    AU: 4 updated - nsis nsis.install nsis.portable opera-developer [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/42b65bb32eb36ead7b4bd35967ed3f20c9e68ea0

--- automatic/nsis.portable/tools/chocolateyInstall.ps1
+++ automatic/nsis.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   Destination    = $toolsDir
-  FileFullPath   = "$toolsDir\nsis-3.06.zip"
+  FileFullPath   = "$toolsDir\nsis-3.06.1.zip"
 }
 
 Get-ChocolateyUnzip  @packageArgs
--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/71.0.3763.0/win/Opera_Developer_71.0.3763.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/71.0.3763.0/win/Opera_Developer_71.0.3763.0_Setup_x64.exe'
-  checksum       = '89beff9947ccc89d0ab6857179c24b66cbfa64da94991059ef39fd3e8fd16d3d'
-  checksum64     = '5db92278fe13ae89e692d013443b2f56242359cb8fcbd0f155412548f068d403'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/71.0.3770.0/win/Opera_Developer_71.0.3770.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/71.0.3770.0/win/Opera_Developer_71.0.3770.0_Setup_x64.exe'
+  checksum       = '7d20a12cacec4b19e0ce537de37066580704dc4d8524a94ceff820a27f71f9be'
+  checksum64     = '1de638ab386a8a32d08167a33014f151b4d2b0bce20ecf5e61f60ec8d0b55a95'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '71.0.3763.0'
+$version = '71.0.3770.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
