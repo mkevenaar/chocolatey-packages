edfa58cb4097fe478941632507a470e4dbd7988f
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-08-13 17:29:32 +0300

    (mongodb.install) update ADDLOCAL components

--- automatic/mongodb.install/tools/chocolateyInstall.ps1
+++ automatic/mongodb.install/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.0-signed.msi'
 $checksum64     = 'ab326721c480dfafe0d0c3c1489c25e3a411b86e830dd91151b5ae24b8c9d508'
 $checksumType64 = 'sha256'
-$silentArgs     = 'ADDLOCAL="Server,ServerService,Router,Client,MonitoringTools,ImportExportTools,MiscellaneousTools" /qn /norestart'
+$silentArgs     = 'ADDLOCAL="ServerService,Server,ProductFeature,Client,Router,MiscellaneousTools" /qn /norestart'
 $dataPath       = "$env:PROGRAMDATA\MongoDB\data\db"
 $logPath        = "$env:PROGRAMDATA\MongoDB\log"
 
