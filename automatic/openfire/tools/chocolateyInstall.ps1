5f86d4621ccd0b507bd456abd42eeb732d8a4a11
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-17 14:15:22 +0000

    AU: 1 updated - openfire [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/0a76fb5315f76a2be540b7967e6a596a4445dcc6

--- automatic/openfire/tools/chocolateyInstall.ps1
+++ automatic/openfire/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_5_2.exe'
-$checksum32     = '9d4b068b1a5e2a13a52dac24413256c34eb2cd5a5214dc762fb2612c294e59eb'
+$url32          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_5_3.exe'
+$checksum32     = 'be675d42dd62d98e05a6c15c1486e61cfc9cb981ede1523bb79f0eddabe37c15'
 $checksumType32 = 'sha256'
-$url64          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_5_2_x64.exe'
-$checksum64     = 'b0b4b3cedb9d32f06d424bd516ecb0168abcfdf001a0fd65c2893e0c96cc594a'
+$url64          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_5_3_x64.exe'
+$checksum64     = '88af47de8f359591d57c5c7a4c87ec6dd0cb9ff2bacb27e17756acf90b937ee6'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
