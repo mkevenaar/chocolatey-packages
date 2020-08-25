1b6f9c60f7123633e0575bc3ce1d03bea9917ec7
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-25 19:13:59 +0000

    AU: 1 updated - spark [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/945af00b12c7fc6d7cd130a1f1f886d2ba9eee40

--- automatic/spark/tools/chocolateyInstall.ps1
+++ automatic/spark/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.igniterealtime.org/downloadServlet?filename=spark/spark_2_9_0-with-jre.exe'
-$checksum     = '8f735dcef8a81ea55b4ac40423722b1d388dab44ea7867568390e9d75c0aa602'
+$url          = 'https://www.igniterealtime.org/downloadServlet?filename=spark/spark_2_9_1-with-jre.exe'
+$checksum     = '29e6e719610c5c04db133953c46319769f8994ae3878c7d4677d54a75fa5a247'
 $checksumType = 'sha256'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
