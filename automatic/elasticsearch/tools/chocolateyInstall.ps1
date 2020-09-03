37628cb4b280b14b5fe56d6982b898ed04ce809d
Maurice Kevenaar <maurice@kevenaar.name>
2020-09-03 16:16:31 +0000

    AU: 1 updated - elasticsearch [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/85aef0768f8829ddcb334bf36ced9d1a7436b34f

--- automatic/elasticsearch/tools/chocolateyInstall.ps1
+++ automatic/elasticsearch/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.0-windows-x86_64.zip'
-$checksum     = '016cb659816ccb10f31d8549102082bcc458059cf1151cd2e200c1e41ae3cb17'
+$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.1-windows-x86_64.zip'
+$checksum     = '659ca58e2a735ed230e5635c2a225d88714c9fcb4344ae825d5d2844656f9c68'
 $checksumType = 'sha256'
-$version      = "7.9.0"
+$version      = "7.9.1"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
