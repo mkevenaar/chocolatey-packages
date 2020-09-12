7836f92e9149b1a30e6500c3f3372f5105289028
Maurice Kevenaar <maurice@kevenaar.name>
2020-09-12 03:13:49 +0000

    AU: 4 updated - crystaldiskinfo crystaldiskinfo.install crystaldiskinfo.portable firefox-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/cd6db991bb36119f9427d7c540c93f04ed59b4b4

--- automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
+++ automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $linkName = "CrystalDiskInfo.lnk"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file          = "$toolsDir\CrystalDiskInfo8_8_5.zip"
+  file          = "$toolsDir\CrystalDiskInfo8_8_6.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
