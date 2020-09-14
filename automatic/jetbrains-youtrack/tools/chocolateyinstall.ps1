d74aac96b127607478683cd41324b4b94570a229
Maurice Kevenaar <maurice@kevenaar.name>
2020-09-14 15:16:15 +0000

    AU: 2 updated - firefox-nightly jetbrains-youtrack [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/6083ae54122769ae46787898bb372091e4f8367a

--- automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
+++ automatic/jetbrains-youtrack/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/charisma/youtrack-2020.3.8361.msi'
-$checksum     = 'a39b1ff290ccf5ee123aa6ddb06c689801c18eb95f9c6d9b5155e8ca8c7f5a5e'
+$url          = 'https://download.jetbrains.com/charisma/youtrack-2020.3.9516.msi'
+$checksum     = '2f445ac8fde95044103cdd1938044d62c3b22c81c05611572663beadb5657263'
 $checksumType = 'sha256'
 
 $packageArgs = @{
