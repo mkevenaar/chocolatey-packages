d6472b695ddadee9d4ba99942b7baf15d5607898
Maurice Kevenaar <maurice@kevenaar.name>
2020-10-19 09:14:26 +0000

    AU: 1 updated - mysql.workbench [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d

--- automatic/mysql.workbench/tools/chocolateyInstall.ps1
+++ automatic/mysql.workbench/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-8.0.21-winx64.msi'
-$checksum     = '71571f5be50c41485f8c2990af9a776859e9b1902edaec5c351f752fdbbedadc'
+$url        = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-8.0.22-winx64.msi'
+$checksum     = 'c86792f3b2abbc0131c88b5d0082507229287e4ab60de0f2ed9888dd24fb87a9'
 $checksumType = 'sha256'
 
 $packageArgs = @{
