0b06acaf5216a2bc244b99446957486c601b3645
Maurice Kevenaar <maurice@kevenaar.name>
2020-10-24 12:14:16 +0000

    AU: 2 updated - firefox-nightly spark [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d

--- automatic/spark/tools/chocolateyInstall.ps1
+++ automatic/spark/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.igniterealtime.org/downloadServlet?filename=spark/spark_2_9_2-with-jre.exe'
-$checksum     = 'a4e73860804e17646ab5a9fefef2656a35aea88d0c6353597e6cc73453e37883'
+$url          = 'https://www.igniterealtime.org/downloadServlet?filename=spark/spark_2_9_3-with-jre.exe'
+$checksum     = '33a90f1d45f5e50138755361270e7e3b4bcbd48671049d4f62d96b0abef7a3dd'
 $checksumType = 'sha256'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
