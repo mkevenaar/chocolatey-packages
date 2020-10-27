3670ffe78f9719fca90b3e6a6d25e75084a15239
Maurice Kevenaar <maurice@kevenaar.name>
2020-10-27 20:14:53 +0000

    AU: 1 updated - writage [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d

--- automatic/writage/tools/chocolateyinstall.ps1
+++ automatic/writage/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'http://www.writage.com/Writage-2.3.msi'
-$checksum32     = '3b0fa1144f37fa5ed6987ff0f84de06d119c8985c9fcc476e6de161369d9f9f9'
+$url32          = 'https://www.writage.com/Writage-2.4.msi'
+$checksum32     = 'cd38580a93971cbffa38674b5c9f4dc4616a318cbe322d81fafbdf77736fd547'
 $checksumType32 = 'sha256'
 
 $packageArgs = @{
