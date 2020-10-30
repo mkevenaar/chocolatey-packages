168843851ea19fd2ee76ce9df8c040c2c0e2a596
Maurice Kevenaar <maurice@kevenaar.name>
2020-10-30 09:18:44 +0000

    AU: 1 updated - samsung-magician [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d

--- automatic/samsung-magician/tools/chocolateyInstall.ps1
+++ automatic/samsung-magician/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://ssd.samsungsemi.com/ecomobile/ssd/update7.do?fname=/Samsung_Magician_Installer.zip'
+$url          = 'https://s3.ap-northeast-2.amazonaws.com/global.semi.static/SAMSUNG_SSD_MAGICIAN_201016/SW/5F99E3AA2FB6AC1BAC402B41D766B74675B9E5CD0C0055C39A2A1EB27C8E09EC/Samsung_Magician_Installer.zip'
 $checksum     = 'fe5671d1092d32662a0f4defc416a689e376621140c3c527cff6188b701b2f18'
 $checksumType = 'sha256'
 
