8725ea6b6c147baf28a4ed73768476ff6a859a49
Maurice Kevenaar <maurice@kevenaar.name>
2020-11-12 14:14:59 +0000

    AU: 2 updated - firefox-nightly JAWS [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d

--- automatic/JAWS/tools/chocolateyinstall.ps1
+++ automatic/JAWS/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'http://jaws2020.vfo.digital/2020.2008.24.400/63872E2D-666C-484F-94EB-F64B1F776A73/J2020.2008.24.400-Offline-x86.exe'
-$checksum32     = '5654b2fefffda0e1652e23dd87364bac50cbe14089060bc0dc1d8d7b35d75c23'
+$url32          = 'https://jaws2021.vfo.digital/2021.2011.16.400/21F14494-6E65-4803-AC0A-5E8C18AE016B/J2021.2011.16.400-Offline-x86.exe'
+$checksum32     = '3f2f8c6ec01f91beac67336cc78741ad41d3e9ed58afa26d6c8427801302897b'
 $checksumType32 = 'sha256'
-$url64          = 'http://jaws2020.vfo.digital/2020.2008.24.400/63872E2D-666C-484F-94EB-F64B1F776A73/J2020.2008.24.400-Offline-x64.exe'
-$checksum64     = '33be41686457400b6df8c67e7969163d78140e86a97fa62bb26818a1d032b4e0'
+$url64          = 'https://jaws2021.vfo.digital/2021.2011.16.400/21F14494-6E65-4803-AC0A-5E8C18AE016B/J2021.2011.16.400-Offline-x64.exe'
+$checksum64     = 'cba8623e04a948f6d4b498492e222210d6d1e83980f15e74eb4a0f1c1d9d7f2e'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
