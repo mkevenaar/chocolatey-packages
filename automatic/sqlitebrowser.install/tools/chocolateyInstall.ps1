d725970a865198aa8d80ccdd19d9e4d4cec51108
Marcin Wisnicki <mwisnicki@gmail.com>
2020-11-28 05:18:19 -0500

    Enable Start menu shortcuts
    Fix #28 aka sqlitebrowser/sqlitebrowser#2458

--- automatic/sqlitebrowser.install/tools/chocolateyInstall.ps1
+++ automatic/sqlitebrowser.install/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   file           = "$toolsdir\DB.Browser.for.SQLite-3.12.0-win32.msi"
   file64         = "$toolsdir\DB.Browser.for.SQLite-3.12.0-win64.msi"
   softwareName   = 'DB Browser for SQLite*'
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" SHORTCUT_SQLITE_PROGRAMMENU=1 SHORTCUT_SQLCIPHER_PROGRAMMENU=1"
   validExitCodes = @(0,3010)
 }
 
