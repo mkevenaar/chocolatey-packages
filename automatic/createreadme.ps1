772664211275fac37022b5e86c55a7f7ab3aa914
Maurice Kevenaar <maurice@kevenaar.name>
2021-01-02 17:11:51 +0100

    (maint) update createreadme.ps1

--- automatic/createreadme.ps1
+++ automatic/createreadme.ps1
@@ -1,10 +1,10 @@
-ls | ? PSIsContainer | ? { !(Test-Path $_\README.md) } | % {
-  [xml] $package = gc $_\*.nuspec -ea 0 -Encoding UTF8
+Get-ChildItem | Where-Object PSIsContainer | Where-Object { !(Test-Path $_\README.md) } | ForEach-Object {
+  [xml] $package = Get-Content $_\*.nuspec -ea -Encoding UTF8
   if (!$package) { return }
 
   $meta = $package.package.metadata
   $readme = ('# <img src="{1}" width="48" height="48"/> [{0}](https://chocolatey.org/packages/{0})' -f $meta.id, $meta.iconUrl), ''
-  $readme += $meta.description -split "`n" | % { $_.Trim() }
+  $readme += $meta.description -split "`n" | ForEach-Object { $_.Trim() }
   $readme -join "`n" | Out-File -Encoding UTF8 $_\README.md
   $meta.id
 }
