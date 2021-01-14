b25a60df76bee72ea350262cc73d05fcb0fcf011
Maurice Kevenaar <maurice@kevenaar.name>
2021-01-14 18:14:05 +0000

    AU: 1 updated - elasticsearch [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/f31acb9743273704ee0542237199a96e78726c36

--- automatic/elasticsearch/tools/chocolateyInstall.ps1
+++ automatic/elasticsearch/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.1-windows-x86_64.zip'
-$checksum     = 'dfc82782e1227c5bc064b80eb8527b5ca80df1e8440c7c3f91665897a1cc42c8'
+$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.2-windows-x86_64.zip'
+$checksum     = '04286f5128589b5402ad8e4c68dd0869869a38f508044e9772fc8106ac4f6960'
 $checksumType = 'sha256'
-$version      = "7.10.1"
+$version      = "7.10.2"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
