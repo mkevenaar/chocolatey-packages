e89456597a73ace067a1580d5ad4e1fe6f578bdf
Maurice Kevenaar <maurice@kevenaar.name>
2021-01-26 08:14:03 +0000

    AU: 1 updated - duplicatefilefinder [skip ci]

--- automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
+++ automatic/duplicatefilefinder/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe?clientId=1974020433.1609225866'
-$checksum     = '4f7b642b30987ab224990522a02df77f997b7c79e1fed59fccbcdcd8b90503eb'
+$url          = 'https://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe?clientId=2019604366.1611648672'
+$checksum     = '00870ca949c6d7b0bd9a23ef3d84282051facc0a13389cc2b4e301f8b4213fba'
 $checksumType = 'sha256'
 
 $packageArgs = @{
