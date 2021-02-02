8e4f493eaacfbeadb60f3e89f797d7518b8fabfe
Maurice Kevenaar <maurice@kevenaar.name>
2021-02-02 14:14:46 +0000

    AU: 1 updated - chromecacheview [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/96ec9706c25def99e912cf70f497362b970f3e1c

--- automatic/chromecacheview/tools/chocolateyInstall.ps1
+++ automatic/chromecacheview/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://www.nirsoft.net/utils/chromecacheview.zip'
-$checksum     = '7a62aedff0fa03342b1386b6122bbc5a83dddd0d21fec02f87dd1635a507fcff'
+$checksum     = '47a046c670a64b7de11a7287c683cdebba3fd7a0c4c14c8d321ffda801406b6b'
 $checksumType = 'sha256'
 
 $packageArgs = @{
