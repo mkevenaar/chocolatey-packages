fe995963aa5784ee51769a64dc4bf640b9dec7d4
Maurice Kevenaar <maurice@kevenaar.name>
2021-02-17 17:14:17 +0000

    AU: 5 updated - elasticsearch texstudio texstudio.install texstudio.portable zerotier-one [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/ccf59ca8a49b754ee60ac927db616a919640a25a

--- automatic/elasticsearch/tools/chocolateyInstall.ps1
+++ automatic/elasticsearch/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.11.0-windows-x86_64.zip'
-$checksum     = '6d126dfee6c141b187bd436ed173d06873442c08f6767efd402d672368275fc1'
+$url          = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.11.1-windows-x86_64.zip'
+$checksum     = '053ba37d2931d543db09a707160d6a26257de77adb7d4cae14169379394b0484'
 $checksumType = 'sha256'
-$version      = "7.11.0"
+$version      = "7.11.1"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
