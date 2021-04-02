19481aad6c9dc7900961bd411f1fc501917b2105
Maurice Kevenaar <maurice@kevenaar.name>
2021-04-02 09:23:24 +0200

    (dymo-label) upadated package

--- automatic/dymo-label/tools/chocolateyInstall.ps1
+++ automatic/dymo-label/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://download.dymo.com/dymo/Software/Win/DLS8Setup.8.7.3.exe'
-$checksum     = 'd7b55a5e1431d5b44241bee91d1841a5cd1990480dfcb73c9f80f57dc6b9c803'
+$url          = 'https://s3.amazonaws.com/download.dymo.com/dymo/Software/Win/DLS8Setup8.7.4.exe'
+$checksum     = '409d79f053ba0c803ce83b71e8245992cd63708fa27017f48a5063826f1bea4d'
 $checksumType = 'sha256'
 
 $packageArgs = @{
