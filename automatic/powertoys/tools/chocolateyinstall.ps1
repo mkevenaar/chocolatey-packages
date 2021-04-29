9bd9b63efd5c36f92a42db00706bba9d3a56dfcd
Maurice Kevenaar <maurice@kevenaar.name>
2021-04-29 19:14:39 +0000

    AU: 1 updated - powertoys [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/d6cde3cdb4d35c7111a72855af0e476522753658

--- automatic/powertoys/tools/chocolateyinstall.ps1
+++ automatic/powertoys/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileName       = "$toolsDir\PowerToysSetup-0.35.0-x64.exe"
-$version        = "0.35.0"
+$fileName       = "$toolsDir\PowerToysSetup-0.37.0-x64.exe"
+$version        = "0.37.0"
 
 $WindowsVersion=[Environment]::OSVersion.Version
 if ($WindowsVersion.Major -ne "10") {
