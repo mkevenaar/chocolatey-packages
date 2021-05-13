d7cb346e7c169695d69537b40ee87a3e2ba4728d
Maurice Kevenaar <maurice@kevenaar.name>
2021-05-13 10:14:02 +0000

    AU: 3 updated - texstudio texstudio.install texstudio.portable [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/a81a88d439ca61491e3e7ec06e43a6bd0e05738c

--- automatic/texstudio.portable/tools/chocolateyinstall.ps1
+++ automatic/texstudio.portable/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file           = "$toolsdir\texstudio-3.1.0-win-portable-qt5.zip"
+  file           = "$toolsdir\texstudio-3.1.2-win-portable-qt5.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
