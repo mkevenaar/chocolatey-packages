a12fa8fd00f096ed4da179561f93e31bbb46d718
Maurice Kevenaar <maurice@kevenaar.name>
2021-05-26 16:14:14 +0000

    AU: 3 updated - sqlitebrowser sqlitebrowser.install sqlitebrowser.portable [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/5c8afea9f23baee17e180db002105fd88811d55d

--- automatic/sqlitebrowser.portable/tools/chocolateyInstall.ps1
+++ automatic/sqlitebrowser.portable/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  file           = "$toolsdir\DB.Browser.for.SQLite-3.12.0-win32.zip"
-  file64         = "$toolsdir\DB.Browser.for.SQLite-3.12.0-win64.zip"
+  file           = "$toolsdir\DB.Browser.for.SQLite-3.12.2-win32.zip"
+  file64         = "$toolsdir\DB.Browser.for.SQLite-3.12.2-win64.zip"
 }
 
 Install-ChocolateyZipPackage  @packageArgs
