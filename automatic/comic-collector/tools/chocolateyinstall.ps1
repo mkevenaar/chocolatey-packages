6aa8943b8460406d9e2d22fb88af09fc324c0660
Maurice Kevenaar <maurice@kevenaar.name>
2021-06-08 12:14:01 +0000

    AU: 1 updated - comic-collector [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/0e7e5e25ecca30f0129f2a8e2e637353d31de830

--- automatic/comic-collector/tools/chocolateyinstall.ps1
+++ automatic/comic-collector/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://installers.collectorz.com/comic-win/comiccollectorsetup2111.exe'
-$checksum     = '5b6a3f4d9ae8b895a34428c0e0e1542702a545de2720a146e89a499dcb92967d'
+$url          = 'https://installers.collectorz.com/comic-win/comiccollectorsetup2121.exe'
+$checksum     = 'b09926a182d950d9806303c0b92052300df55ca51834b751eea562370bb1a5c8'
 $checksumType = 'sha256'
 
 $packageArgs = @{
