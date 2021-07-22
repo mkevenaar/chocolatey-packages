55ccf6da1f2a27603f39ea331cad8ab947f89c2c
Maurice Kevenaar <maurice@kevenaar.name>
2021-07-22 18:15:55 +0000

    AU: 3 updated - grafana grafana.install jetbrains-rider [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/a9edb34dd64e3aaefd4d76fb91f74c2abf947af8

--- automatic/jetbrains-rider/tools/chocolateyInstall.ps1
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2021.1.4.exe'
-$checksum     = '7bbacfc5e67f770bfa6a49aa1c7610402041644dad7fc3e9b3c1b863983e2681'
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2021.1.5.exe'
+$checksum     = '5bb6f2517582e2dade5b932a2918255b9d23fdd1c00f12cb9f3504fcfe57d56f'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
