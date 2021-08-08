a59df595867e1492b89565e3958c0d3379b41aaa
Maurice Kevenaar <maurice@kevenaar.name>
2021-08-08 11:14:05 +0000

    AU: 1 updated - ultraiso [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/86fb88caf7c4f0a86ec594048686170553432e3a

--- automatic/ultraiso/tools/chocolateyInstall.ps1
+++ automatic/ultraiso/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://www.ultraiso.com/uiso9_pe.exe'
-$checksum     = 'a49e945e5f6482b42c0e59e8187cddd4d736fcda0526768ca58a4411016d3e19'
+$checksum     = '26e3139f6ac209981c0ed5fd95d4e0c43f7dd82eeddaaf3e6585cbfcb0f95ca3'
 $checksumType = 'sha256'
 
 $packageArgs = @{
