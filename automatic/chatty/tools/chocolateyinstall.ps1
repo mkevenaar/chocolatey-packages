931f01cf79afa515ba1f5faf2120ec031f66621f
Maurice Kevenaar <maurice@kevenaar.name>
2021-08-12 05:14:27 +0000

    AU: 2 updated - chatty vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/a51f1fc3a8c55ae755b22d8d2b63f73cb27194d2

--- automatic/chatty/tools/chocolateyinstall.ps1
+++ automatic/chatty/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  file           = "$toolsdir\Chatty_0.15_win_standalone.zip"
+  file           = "$toolsdir\Chatty_0.16-b1_win_standalone.zip"
   validExitCodes = @(0)
 }
 
