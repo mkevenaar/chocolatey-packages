0ce148ba8a7e381e0b1edf0d2d2c72eac5bf22b5
Maurice Kevenaar <maurice@kevenaar.name>
2021-08-19 13:14:39 +0000

    AU: 3 updated - firefox-nightly opera-beta wifiinfoview [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/aa6e7a2ef530229a4a7ad5e545bc30eada7029a3

--- automatic/opera-beta/tools/chocolateyInstall.ps1
+++ automatic/opera-beta/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-beta/78.0.4093.103/win/Opera_beta_78.0.4093.103_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-beta/78.0.4093.103/win/Opera_beta_78.0.4093.103_Setup_x64.exe'
-  checksum       = 'ed7db949febe7cf837a0237e0593470338c3fc7dcd9fd016b6b576eaf5b97a90'
-  checksum64     = '80e4954a306de9dc3b48411d1848d4481c85c5b87068b03d170a630fb3cf0204'
+  url            = 'https://get.geo.opera.com/pub/opera-beta/79.0.4143.3/win/Opera_beta_79.0.4143.3_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-beta/79.0.4143.3/win/Opera_beta_79.0.4143.3_Setup_x64.exe'
+  checksum       = '8e0ad0475877a9e5e4789e2e334cc5510257f9825f5b924acad00f5e7a473e51'
+  checksum64     = '14ff9690880ad618c4628766fb5fe98204d66bb8b84e7f29258f69ba380c5374'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '78.0.4093.103'
+$version = '79.0.4143.3'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
--- automatic/wifiinfoview/tools/chocolateyInstall.ps1
+++ automatic/wifiinfoview/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://www.nirsoft.net/utils/wifiinfoview.zip'
-$checksum     = '03ddc8f440512d6fe2cecc36188537b13f801a3466f49614ac47728c85f976fe'
+$checksum     = '2569aec6f647e3d04baeed4b20949c071671bd85ca9ccbd3161a455073c6e134'
 $checksumType = 'sha256'
 
 $packageArgs = @{
