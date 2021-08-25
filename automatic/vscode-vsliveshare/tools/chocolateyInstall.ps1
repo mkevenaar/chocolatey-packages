1298f0af55ca3f1cb34b0f9e2519096b7377781a
Maurice Kevenaar <maurice@kevenaar.name>
2021-08-25 01:14:18 +0000

    AU: 2 updated - firefox-dev vscode-vsliveshare [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/74053f6fbf1f7cc243855e3ba04f54802df8583f

--- automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
+++ automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.4760'
+Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.4761'
