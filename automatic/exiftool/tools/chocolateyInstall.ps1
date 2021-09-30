cbcacf39f635cce264a67ec3898b23ed72510c02
Maurice Kevenaar <maurice@kevenaar.name>
2021-09-30 17:15:34 +0000

    AU: 2 updated - exiftool firefox-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/019ceae3bb5f15f54697674a88db23cdca0ad97e

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://exiftool.org/exiftool-12.31.zip'
-$checksum     = '301637198569f8b3ef1fed0b7fe6bcb170c28c69c0e0f8c4184a1fb7e079e186'
+$url          = 'https://exiftool.org/exiftool-12.32.zip'
+$checksum     = '3c4926e40e99df995308ec7bba68f296a57ee235a208014432431a5eb2ee05f1'
 $checksumType = 'sha256'
 
 $packageArgs = @{
