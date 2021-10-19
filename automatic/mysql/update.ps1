30d69aed27605b84b922be14cd7354d7b22b8a03
Maurice Kevenaar <maurice@kevenaar.name>
2021-10-19 15:33:09 +0200

    [AU mysql:8.0.27]

--- automatic/mysql/update.ps1
+++ automatic/mysql/update.ps1
@@ -5,9 +5,9 @@ $releases = 'https://dev.mysql.com/downloads/mysql/'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
  }
@@ -20,10 +20,29 @@ function global:au_GetLatest {
     $version = $versiondata.toString()
 
     $url = 'https://dev.mysql.com/get/Downloads/MySQL-' + $versiondata.toString(2) + '/mysql-' + $version + '-winx64.zip'
-    $Latest = @{ URL32 = $url; Version = $version }
+    $Latest = @{ URL64 = $url; Version = $version }
     return $Latest
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
     update -ChecksumFor 64
 }
+
+function global:au_AfterUpdate ($Package) {
+
+    if ($Package.RemoteVersion -ne $Package.NuspecVersion) {
+  
+        Get-RemoteFiles -NoSuffix
+  
+        $file = [IO.Path]::Combine("tools", $Latest.FileName32)
+  
+        Write-Output "Submitting file $file to VirusTotal"
+  
+        # Assumes vt-cli Chocolatey package is installed!
+        vt.exe scan file $file --apikey $env:VT_APIKEY
+  
+        Remove-Item $file -ErrorAction Ignore
+  
+        $Latest.Remove("FileName32")
+    }
+  }
