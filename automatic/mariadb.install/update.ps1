a1e53781fd830541abf258c485196d0d08c50f65
Maurice Kevenaar <maurice@kevenaar.name>
2021-10-26 10:22:21 +0200

    (mariadb) update checks to new REST API

--- automatic/mariadb.install/update.ps1
+++ automatic/mariadb.install/update.ps1
@@ -1,26 +1,108 @@
 Import-Module au
 
-$releases = 'https://downloads.mariadb.org/mariadb/'
+$releases = 'https://mariadb.org/download'
+$feed = 'https://downloads.mariadb.org/rest-api/mariadb/'
+$mirror = 'archive'
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function name4url($url) {
+  if ($FileNameBase) { return $FileNameBase }
+  $res = $url -split '/' | Select-Object -Last 1
+  $res -replace '\.[^.]+$'
+}
+
+function global:au_BeforeUpdate {
+  $ext = $Latest.FileType
+  $toolsPath = Resolve-Path tools
+  $NoSuffix = $true
+
+  $CurrentProgressPreference = $ProgressPreference
+  $ProgressPreference = 'SilentlyContinue'
+
+  Write-Host 'Purging' $ext
+  $purgePath = "$toolsPath{0}*.$ext" -f [IO.Path]::DirectorySeparatorChar
+  Remove-Item -Force $purgePath -ea ignore
+  try {
+
+    if ($Latest.Url32) {
+      $base_name = name4url $Latest.Url32
+      $file_name = "{0}{2}.{1}" -f $base_name, $ext, $(if ($NoSuffix) { '' } else {'_x32'})
+      $file_path = Join-Path $toolsPath $file_name
+
+      Write-Host "Downloading to $file_name -" $Latest.Url32
+      $url32 = $Latest.URL32  + "?mirror=${mirror}"
+      Invoke-WebRequest -Uri $url32 -OutFile $file_path
+      $Latest.FileName32 = $file_name
+    }
+
+    if ($Latest.Url64) {
+        $base_name = name4url $Latest.Url64
+        $file_name = "{0}{2}.{1}" -f $base_name, $ext, $(if ($NoSuffix) { '' } else {'_x64'})
+        $file_path = Join-Path $toolsPath $file_name
+
+        Write-Host "Downloading to $file_name -" $Latest.Url64
+        $url64 = $Latest.URL64  + "?mirror=${mirror}"
+        Invoke-WebRequest -Uri $url64 -OutFile $file_path
+        $Latest.FileName64 = $file_name
+    }
+  } catch {
+    throw $_
+  } finally {
+    $ProgressPreference = $CurrentProgressPreference
+  }
+}
 
 function global:au_GetLatest {
-    $version_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $CurrentProgressPreference = $ProgressPreference
+  $ProgressPreference = 'SilentlyContinue'
 
-    $re  = "^/mariadb/"
-    $version_url = $version_page.links | ? href -match $re | select -First 1 -expand href
+  $json = Invoke-WebRequest -Uri $feed -UseBasicParsing  | ConvertFrom-Json
 
-    $version = ([regex]::Match($version_url,'/mariadb/(.+)/')).Captures.Groups[1].value
+  $streams = @{ }
 
-    $url32 = "https://downloads.mariadb.org/f/mariadb-" + $version + "/win32-packages/mariadb-" + $version + "-win32.msi"
-    $url64 = "https://downloads.mariadb.org/f/mariadb-" + $version + "/winx64-packages/mariadb-" + $version + "-winx64.msi"
+  $major_releases = $json.major_releases
 
-    return @{
-        URL32 = $url32
-        URL64 = $url64
-        Version = $version
-        FileType = 'msi'
+  foreach ($major_release in $major_releases) {
+
+    $streamVersion = $major_release.release_id
+
+    $release_data_feed = "https://downloads.mariadb.org/rest-api/mariadb/${streamVersion}/"
+    $release_json = Invoke-WebRequest -Uri $release_data_feed -UseBasicParsing  | ConvertFrom-Json
+
+    $releaseversion = $release_json.releases.psobject.properties.name | Select-Object -First 1
+
+    if ($major_release.release_status -eq 'Alpha') {
+      $version = ($release_json.releases.psobject.properties.name | Select-Object -First 1) + "-" + $major_release.release_status
+      $releaseversion = Get-Version($version)
     }
+
+    $32bit = $release_json.releases.$releaseversion.files | Where-Object { ($_.os -eq "Windows") -and ($_.package_type -eq "MSI Package") -and ($_.cpu -eq "x86") }
+    $64bit = $release_json.releases.$releaseversion.files | Where-Object { ($_.os -eq "Windows") -and ($_.package_type -eq "MSI Package") -and ($_.cpu -eq "x86_64") }
+
+    $releaseNotes = $release_json.releases.$releaseversion.release_notes_url
+
+    if('' -eq $releaseNotes) {
+      $releasenotesversion = $releaseversion -replace "\."
+      $releaseNotes = "https://mariadb.com/kb/en/mariadb-${releasenotesversion}-release-notes/"
+    }
+
+    $Result = @{
+      Version        = $releaseversion
+      URL32          = $32bit.file_download_url
+      Checksum32     = $32bit.checksum.sha256sum
+      Checksum32Type = 'sha256'
+      URL64          = $64bit.file_download_url
+      Checksum64     = $64bit.checksum.sha256sum
+      Checksum64Type = 'sha256'
+      ReleaseNotes   = $releaseNotes
+      FileType       = 'msi'
+    }
+    if (-not $null -eq $32bit.file_name) {
+      $streams.Add($streamVersion, $Result)
+    }
+  }
+
+  $ProgressPreference = $CurrentProgressPreference
+  return @{ Streams = $streams }
 }
 
 function global:au_SearchReplace {
@@ -37,9 +119,13 @@ function global:au_SearchReplace {
       "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
       "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
     }
+    "$($Latest.PackageName).nuspec" = @{
+      "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+    }
+
   }
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
-  update -ChecksumFor None
+  update -ChecksumFor None -NoCheckUrl
 }
--- automatic/mariadb/update.ps1
+++ automatic/mariadb/update.ps1
@@ -7,11 +7,12 @@ function global:au_SearchReplace {
   @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
-        }
+            "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+          }
     }
  }
 
 # Left empty intentionally to override BeforeUpdate in mariadb.install
 function global:au_BeforeUpdate { }
 
-update -ChecksumFor none
+update -ChecksumFor None -NoCheckUrl
