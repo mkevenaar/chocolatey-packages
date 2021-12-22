816cee4342363221b1520d19ce75ee61af0f4ac3
Maurice Kevenaar <maurice@kevenaar.name>
2021-12-22 05:15:12 +0000

    AU: 3 updated - crystaldiskinfo.install crystaldiskinfo.portable vlc-nightly [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/82cf03c19121493d76f70c2980d5bbd49f95bca6

--- automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
+++ automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $linkName = "CrystalDiskInfo.lnk"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  file          = "$toolsDir\CrystalDiskInfo8_13_2.zip"
+  file          = "$toolsDir\CrystalDiskInfo8_13_3.zip"
 }
 
 Get-ChocolateyUnzip  @packageArgs
