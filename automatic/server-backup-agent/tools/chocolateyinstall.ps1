ac0760b2baa274973f9e5c9a8e0224d485dfce8f
Maurice Kevenaar <maurice@kevenaar.name>
2022-01-24 20:15:11 +0000

    AU: 1 updated - server-backup-agent [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/957be06af6ff7c4220452803d069857fe9ae0242

--- automatic/server-backup-agent/tools/chocolateyinstall.ps1
+++ automatic/server-backup-agent/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url32          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x86.msi'
-$checksum32     = '9252B0D94A1FEF22D68B5C201AE77C41A2AA4755B6609E40978E05EBAB8CC0D9'
+$checksum32     = 'B0D7CF675EB993C80A139B8D2F88B652E503F8DEEEABE6F0B5DBEA82216439BE'
 $checksumType32 = 'sha256'
 $url64          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x64.msi'
-$checksum64     = '0f91fe1ba81b2f93b4df9cca0672934dbee5044153be4d80ff017a31633a36b9'
+$checksum64     = '2fe93591a56610030a449d72ae65bc8b5680986118ab0e22feff099abf178e7c'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
