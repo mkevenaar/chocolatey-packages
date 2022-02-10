0e734c431678c07ef2925d585ab26cc9ea9d6775
Maurice Kevenaar <maurice@kevenaar.name>
2022-02-10 20:16:44 +0000

    AU: 2 updated - powertoys wireshark [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/d04742a564024f8a266e2bce32a14267c4833f94

--- automatic/wireshark/tools/chocolateyInstall.ps1
+++ automatic/wireshark/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file          = "$toolsDir\Wireshark-win32-3.6.1.exe"
-  file64        = "$toolsDir\Wireshark-win64-3.6.1.exe"
+  file          = "$toolsDir\Wireshark-win32-3.6.2.exe"
+  file64        = "$toolsDir\Wireshark-win64-3.6.2.exe"
   softwareName  = 'Wireshark*'
   silentArgs     = '/S /quicklaunchicon=no'
   validExitCodes = @(0)
