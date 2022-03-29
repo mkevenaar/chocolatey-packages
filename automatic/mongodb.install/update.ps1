f0704e79841d65190e713c4d78cdff324d76bda4
Maurice Kevenaar <maurice@kevenaar.name>
2022-03-29 13:34:11 +0200

    (mongodb) fix streams

--- automatic/mongodb.install/update.ps1
+++ automatic/mongodb.install/update.ps1
@@ -3,59 +3,63 @@ import-module au
 $feed = 'http://downloads.mongodb.org.s3.amazonaws.com/current.json'
 
 function global:au_SearchReplace {
-    @{
-        '.\tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-        }
-        "$($Latest.PackageName).nuspec" = @{
-            "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
-        }
+  @{
+    '.\tools\chocolateyInstall.ps1' = @{
+      "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
+    "$($Latest.PackageName).nuspec" = @{
+      "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+    }
+  }
 }
 
 function CreateStream {
-    param($url64bit, $version, $releaseNotes)
-    
-    $Result = @{
-        Version      = $version
-        URL64        = $url64bit
-        ReleaseNotes = $releaseNotes
-    }
-    
-    return $Result
+  param($url64bit, $version, $releaseNotes)
+
+  $Result = @{
+    Version      = $version
+    URL64        = $url64bit
+    ReleaseNotes = $releaseNotes
+  }
+
+  return $Result
 }
-      
+
 function global:au_GetLatest {
-    $json = Invoke-WebRequest -Uri $feed -UseBasicParsing  | ConvertFrom-Json
+  $json = Invoke-WebRequest -Uri $feed -UseBasicParsing  | ConvertFrom-Json
 
-    $streams = @{ }
+  $streams = @{ }
 
-    $versions = $json.versions
+  $versions = $json.versions
 
-    foreach ($version in $versions) {
+  foreach ($version in $versions) {
 
-        $releaseversion = Get-Version($version.version)
+    $releaseversion = Get-Version($version.version)
 
-        $url = $version.downloads | Where-Object { $_.target -eq "windows" -and $_.edition -eq "base" } | Select-Object -ExpandProperty msi
+    $url = $version.downloads | Where-Object { $_.target -eq "windows" -and $_.edition -eq "base" } | Select-Object -ExpandProperty msi
 
-        $releaseNotes = $version.notes
+    $releaseNotes = $version.notes
 
-        $streamVersion = $releaseversion.toString(2)
+    $streamVersion = $releaseversion.toString(2)
 
-        if ($version.release_candidate) {
-            $streamVersion += '-rc'
-        }
-        
-        if ($url) {
-            $streams.Add($streamVersion, (CreateStream $url $releaseversion $releaseNotes))
-        }
+    if ($version.release_candidate) {
+      $streamVersion += '-rc'
     }
 
-    return  @{ Streams = $streams }
+
+    if ($url) {
+      if ($streams.ContainsKey($streamVersion)) {
+        $streams.Remove($streamVersion);
+      }
+      $streams.Add($streamVersion, (CreateStream $url $releaseversion $releaseNotes))
+    }
+  }
+
+  return  @{ Streams = $streams }
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
-    update -ChecksumFor 64
+  update -ChecksumFor 64
 }
