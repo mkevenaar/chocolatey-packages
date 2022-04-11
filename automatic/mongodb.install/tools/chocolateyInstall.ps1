edd4143072b5d07b8edd4d9211830880073fce90
Maurice Kevenaar <maurice@kevenaar.name>
2022-04-11 15:15:35 +0000

    AU: 2 updated - mongodb mongodb.install [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/28ab7b6ef626d701b0399f67b164db98c6d032b8

--- automatic/mongodb.install/tools/chocolateyInstall.ps1
+++ automatic/mongodb.install/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-5.3.1-signed.msi'
-$checksum64     = 'fd8ca9ba821e8d834bffccdf9488d50ad113c18e634f6dc788acb53df9d4fde9'
+$url64          = 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-5.0.7-signed.msi'
+$checksum64     = '3d9eee63d56ff75176cf03308274a29c9e385704e21a26f5864cfe1357ca3cf7'
 $checksumType64 = 'sha256'
 $silentArgs     = 'ADDLOCAL="ServerService,Server,ProductFeature,Client,Router,MiscellaneousTools" /qn /norestart'
 $dataPath       = "$env:PROGRAMDATA\MongoDB\data\db"
