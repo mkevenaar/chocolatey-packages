5b5f10098334e1fc43e9dff6b164c622e176d690
Maurice Kevenaar <maurice@kevenaar.name>
2022-05-09 16:00:25 +0200

    (wd-security) fix package detection

--- automatic/wd-security/update.ps1
+++ automatic/wd-security/update.ps1
@@ -1,31 +1,31 @@
 import-module au
 
 $releases = 'https://support.wdc.com/downloads.aspx?p=172'
+$versionPage = 'https://support-en.wd.com/app/products/downloads/release-notes/note_id/29495'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.Links | Where-Object href -Match ".zip" | Select-Object -First 1 -ExpandProperty href
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $url = $download_page.Links | Where-Object href -Match ".zip" | Select-Object -First 1 -ExpandProperty href
 
-    $version = Get-Version $url
+  # $version = Get-Version $url
+  $version_page = Invoke-WebRequest -Uri $versionPage -UseBasicParsing
+  $re = '<p><strong>Version: (.+\d)\s'
+  $version = ([regex]::Match($version_page.content, $re)).Captures.Groups[1].value
 
-    $releaseNotes = $download_page.Links | Where-Object href -Match ".pdf" | Select-Object -First 1 -ExpandProperty href
+  $releaseNotes = $download_page.Links | Where-Object href -Match ".pdf" | Select-Object -First 1 -ExpandProperty href
 
-    $Latest = @{ URL32 = $url; Version = $version; ReleaseNotes = $releaseNotes }
-    return $Latest
+  $Latest = @{ URL32 = $url; Version = $version; ReleaseNotes = $releaseNotes }
+  return $Latest
 }
 
 update -ChecksumFor 32
