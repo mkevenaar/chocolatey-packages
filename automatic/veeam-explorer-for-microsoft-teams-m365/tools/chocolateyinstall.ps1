6b43078f669cd01675967b914bda958473656362
Maurice Kevenaar <maurice@kevenaar.name>
2022-06-04 20:37:55 +0200

    (VBM) Install patch and update to new version

--- automatic/veeam-explorer-for-microsoft-teams-m365/tools/chocolateyinstall.ps1
+++ automatic/veeam-explorer-for-microsoft-teams-m365/tools/chocolateyinstall.ps1
@@ -8,6 +8,7 @@ $filename = 'Veeam.Backup365_6.0.0.379_P20220413.iso'
 $installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
 
 $fileLocation = 'Explorers\VeeamExplorerForTeams.msi'
+$updateFileLocation = "Patches\VeeamExplorerForTeams.msp"
 
 [System.Collections.ArrayList]$silentArgs = @()
 
@@ -29,3 +30,22 @@ $packageArgs = @{
 
 Install-ChocolateyIsoInstallPackage @packageArgs
 
+# An update file is provided in the ISO. This should be executed after
+# the installation of the software.
+#
+# This should be in a seperate package, but you can't trigger / force a
+# dependency after the installation Therefore I need to trigger it this way.
+If ($filename -Match "_P") {
+  $updatePackageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    isoFile        = $installPath
+    softwareName   = $packageArgs['softwareName']
+    file           = $updateFileLocation
+    fileType       = 'msp'
+    silentArgs     = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+    validExitCodes = @(0,1638,1641,3010) #1638 was added to allow updating when an newer version is already installed.
+    destination    = $toolsDir
+  }
+
+  Install-ChocolateyIsoInstallPackage @updatePackageArgs
+}
