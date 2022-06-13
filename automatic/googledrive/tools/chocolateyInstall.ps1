0b188d78a533c3a280a41a771fb4f34c4ae2189f
Maurice Kevenaar <maurice@kevenaar.name>
2022-06-13 13:15:38 +0000

    AU: 1 updated - googledrive [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/ebfd0e3635ef493e4d6b4b6bb44008765318bca7

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
-$checksum     = 'FAADBF7D1915CFD95A5065B353507BACF62635C129E121398DC4CBA439F599A0'
+$checksum     = '07B47FC6D7563737321043C34FA07699C7E8217B3DB56CB7731BC912DA2EAE54'
 $checksumType = 'sha256'
 
 $packageArgs = @{
