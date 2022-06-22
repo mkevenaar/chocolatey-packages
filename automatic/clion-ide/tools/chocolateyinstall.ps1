50a83da7e2177ba0224ee59ade4c7630c8deea10
Maurice Kevenaar <maurice@kevenaar.name>
2022-06-22 22:15:33 +0000

    AU: 2 updated - chatty clion-ide [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/fcf0c0c6cf51a73769eb163c7ee5c602eed5876b

--- automatic/clion-ide/tools/chocolateyinstall.ps1
+++ automatic/clion-ide/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/cpp/CLion-2022.1.2.exe'
-$checksum     = '97cccfeea43fcf0d8bf13f8f52612ef3b97b1e2b8a6b6e69ee550e7135c2c979'
+$url          = 'https://download.jetbrains.com/cpp/CLion-2022.1.3.exe'
+$checksum     = '46a2df1f3c0b9fca6c34400eb979a3893bf00d875f0e2fcf74bf64b202ba747d'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
