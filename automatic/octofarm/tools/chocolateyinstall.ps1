7779e03c6c1cf414eb43c7b893cc5b49d6240814
Maurice Kevenaar <maurice@kevenaar.name>
2022-06-25 10:15:23 +0000

    AU: 1 updated - octofarm [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/30f8342325cd76fac5d858d474584ceace59dfc4

--- automatic/octofarm/tools/chocolateyinstall.ps1
+++ automatic/octofarm/tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installDir
   fileType       = 'exe'
-  file           = "$toolsdir\1.4.3.zip"
+  file           = "$toolsdir\1.5.0.zip"
   validExitCodes = @(0)
 }
 
