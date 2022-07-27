688936376302b66496b0e0b028c81b1a911d703a
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2022-07-27 13:27:13 +0200

    fix au

--- automatic/grafana-agent/update.ps1
+++ automatic/grafana-agent/update.ps1
@@ -13,20 +13,14 @@ function global:au_GetLatest {
   return @{
     URL32    = $url
     Version  = $version
-    FileType = 'zip'
+    FileType = 'exe'
   }
 }
 
 function global:au_SearchReplace {
   return @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
-    }
-    ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
-      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
     }
   }
 }
