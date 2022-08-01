da10738a55e9ad66275f8fa49e9328a6cd8f3998
Maurice Kevenaar <maurice@kevenaar.name>
2022-08-01 21:15:40 +0000

    AU: 2 updated - powertoys vscode-vsliveshare [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/96d0842b426f9a892f7fa695b1946e53c49e82a1

--- automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
+++ automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.5667'
+Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.5669'
