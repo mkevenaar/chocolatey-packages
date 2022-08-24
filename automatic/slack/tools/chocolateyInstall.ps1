28cce802848591c364080cfd1a929c64bc8ac827
Maurice Kevenaar <maurice@kevenaar.name>
2022-08-24 17:15:47 +0000

    AU: 1 updated - slack [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/3555fbd9506631f22060bf0c6227e17b18a4d41d

--- automatic/slack/tools/chocolateyInstall.ps1
+++ automatic/slack/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.slack-edge.com/releases/windows/4.27.154/prod/ia32/slack-standalone-4.27.154.0.msi'
-$checksum32     = 'fed9d743bd0dca07d320ae77b81d119e5149cd4cba26050b3a95a9dd3db07841'
+$url32          = 'https://downloads.slack-edge.com/releases/windows/4.28.171/prod/ia32/slack-standalone-4.28.171.0.msi'
+$checksum32     = '79f1b28f421d4948ccf3c9af7aa974c2a48d757e55df7ecaf24fb9fa3c14154d'
 $checksumType32 = 'sha256'
-$url64          = 'https://downloads.slack-edge.com/releases/windows/4.27.154/prod/x64/slack-standalone-4.27.154.0.msi'
-$checksum64     = '5fe92e297cc05a854064e6ebef7fdc6ba34f32854d4709bdba849d0386ac4fbf'
+$url64          = 'https://downloads.slack-edge.com/releases/windows/4.28.171/prod/x64/slack-standalone-4.28.171.0.msi'
+$checksum64     = '39dbc7fa2afb70ccc33517f465a4e585593c7cca37c4196d32989935e53eeb92'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
