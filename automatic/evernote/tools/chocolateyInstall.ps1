897967bd38b5e072cff5cdd5d2557daa30d60c47
Maurice Kevenaar <maurice@kevenaar.name>
2022-08-31 21:16:09 +0000

    AU: 2 updated - evernote vscode-vsliveshare [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/e768cf9f51e1bc3ce674ff2c319fd1ac22dab8f4

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -13,8 +13,8 @@ elseif ($osInfo.ProductType -ne 1) {
 }
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://cdn1.evernote.com/boron/win/builds/Evernote-10.43.7-win-ddl-ga-3598-a51c0cce94-setup.exe'
-$checksum     = '729ef2a67f53901815360c609e0a6eba2f68ebeb2cf801c0087762052d8999de'
+$url          = 'https://cdn1.evernote.com/boron/win/builds/Evernote-10.44.8-win-ddl-ga-3632-36e33ef8ac-setup.exe'
+$checksum     = '1f22277f3445e59bd20b1579492119615b5279d72eaf98ebc214610d9bd918bd'
 $checksumType = 'sha256'
 
 $packageArgs = @{
