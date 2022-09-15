10b634bf2d6235778f6ed3c9c75e902ff3561d9a
Maurice Kevenaar <maurice@kevenaar.name>
2022-09-15 21:15:38 +0000

    AU: 2 updated - pycharm pycharm-community [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/ca657892e3497e289dee01eb5011455994d627d3

--- automatic/pycharm-community/tools/chocolateyInstall.ps1
+++ automatic/pycharm-community/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/python/pycharm-community-2022.2.1.exe'
-$checksum     = '343f9f2e5f258877ad8404f56839dd24a14a9941d4e11685775855593b56839a'
+$url          = 'https://download.jetbrains.com/python/pycharm-community-2022.2.2.exe'
+$checksum     = '3e31a2c0c01404912870fbe738d7f9e9466144c6e6b55ac7d166600e21d2199f'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
