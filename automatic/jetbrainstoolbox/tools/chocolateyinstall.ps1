c48ae555520d26576f62083ba0165138774fc11a
Maurice Kevenaar <maurice@kevenaar.name>
2022-09-20 14:17:54 +0000

    AU: 2 updated - firefox-nightly jetbrainstoolbox [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/32bcd7f590c37cd5d944980eb3d52e1ebcc4ed58

--- automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.25.12999.exe'
-$checksum     = '3ef855c5b3eba1dc4030467fd02baf49ec62307e9c0a622d0a06e3ad10f03857'
+$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.26.0.13072.exe'
+$checksum     = 'b15c0c8f4315793914b996c49301ffd5eb79751afab29ff0c86194a923cbb3ee'
 $checksumType = 'sha256'
 
 $packageArgs = @{
