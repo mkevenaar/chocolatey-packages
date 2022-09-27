fad2c75a6c40efaa390fe67dcfbba2c73ad10ba5
Maurice Kevenaar <maurice@kevenaar.name>
2022-09-27 07:16:18 +0000

    AU: 4 updated - helio-workstation helio-workstation.install helio-workstation.portable vscode-ansible [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/5b9117ab86bfd84bb7dd897653605b3d0ca7e785

--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # clean up old extension
 Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
 
-Install-VsCodeExtension -extensionId 'redhat.ansible@0.14.74'
+Install-VsCodeExtension -extensionId 'redhat.ansible@0.14.75'
