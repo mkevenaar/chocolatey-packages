44ee9fa59a3cf7ce6723901909101122956d7b9e
Maurice Kevenaar <maurice@kevenaar.name>
2022-10-05 11:26:05 +0200

    (powertoys) switch to GH api

--- automatic/powertoys/update.ps1
+++ automatic/powertoys/update.ps1
@@ -1,37 +1,41 @@
-Import-Module au
+Import-Module AU
 
-$releases = 'https://github.com/microsoft/PowerToys/releases/latest'
-
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    $re  = "PowerToysSetup(.+)?-x64.exe"
-    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
-
-    $version = (($download_page.Links | Where-Object href -Match "releases/tag" | Select-Object -First 1 -ExpandProperty href) -Split "/" | Select-Object -Last 1) -Replace "v"
-    $url = 'https://github.com' + $url
-
-    return @{
-        URL64 = $url
-        Version = $version
-        RemoteVersion  = $version
-        FileType = 'exe'
-    }
-}
+$releases = 'https://api.github.com/repos/microsoft/PowerToys/releases/latest'
 
 function global:au_SearchReplace {
   return @{
-    ".\tools\chocolateyInstall.ps1" = @{
+    'tools\chocolateyInstall.ps1' = @{
       "(^[$]fileName\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
       "(^[$]version\s*=\s*)`".*`""             = "`${1}`"$($Latest.RemoteVersion)`""
     }
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"    = @{
       "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
       "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
       "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType64)"
       "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $header = @{
+    "Authorization" = "token $env:github_api_key"
+  }
+  $download_page = Invoke-RestMethod -Uri $releases -Headers $header
+
+  $asset = $download_page.assets | Where-Object -Property name -Like "PowerToysSetup*-x64.exe"
+
+  $version = $download_page.tag_name.Replace('v', '')
+  $version = Get-Version($version)
+  if ($asset) {
+    $url = $asset.browser_download_url
+    return @{
+      URL64         = $url
+      Version       = $version
+      RemoteVersion = $version
+      FileType      = 'exe'
     }
   }
 }
