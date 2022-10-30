79de05d63d8e145e31dc98d8b3a3c314699cc2d2
Maurice Kevenaar <maurice@kevenaar.name>
2022-10-30 16:41:44 +0100

    (JAWS) drop 32 bit support

--- automatic/JAWS/tools/chocolateyinstall.ps1
+++ automatic/JAWS/tools/chocolateyinstall.ps1
@@ -1,9 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://jaws2022.vfo.digital/2022.2207.25.400/2ACCEDFA-0210-472F-9953-AE54DF593427/J2022.2207.25.400-Offline-x86.exe'
-$checksum32     = '2b9021ef9f082b2ef547038ab45d7062f749e73128b8d32b834836bd09093fb3'
-$checksumType32 = 'sha256'
 $url64          = 'https://jaws2022.vfo.digital/2022.2207.25.400/2ACCEDFA-0210-472F-9953-AE54DF593427/J2022.2207.25.400-Offline-x64.exe'
 $checksum64     = 'd8848f7fe098ce75822dffa83ba7ee97d6743a6617f8363483e2b39910c4c81e'
 $checksumType64 = 'sha256'
@@ -12,13 +9,10 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = $checksumType32
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= $checksumType64
-  softwareName  = 'Freedom Scientific JAWS 2020*'
+  softwareName  = 'Freedom Scientific JAWS *'
   silentArgs    = "/Type Silent"
   validExitCodes= @(0)
 }
--- automatic/JAWS/update.ps1
+++ automatic/JAWS/update.ps1
@@ -3,9 +3,6 @@ import-module au
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
             "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -22,13 +19,12 @@ function global:au_GetLatest {
     $verisonsurl = 'https://support.freedomscientific.com/Downloads/OfflineInstallers/GetInstallers?product=JAWS&version='+$majorversion+'&language=mul&releaseType=Offline&_=' + $ED
     $verisonsjson = Invoke-WebRequest $verisonsurl | ConvertFrom-Json
 
-    $url32 = ($verisonsjson | Where-Object InstallerPlatform -Match "32" | Sort-Object -Property FileName -Descending | Select-Object -First 1 ).InstallerLocationHTTP
     $url64 = ($verisonsjson | Where-Object InstallerPlatform -Match "64" | Sort-Object -Property FileName -Descending | Select-Object -First 1 ).InstallerLocationHTTP
 
     $version = ($verisonsjson | Where-Object InstallerPlatform -Match "64" | Sort-Object -Property FileName -Descending | Select-Object -First 1 ).FileName
     $version = (Get-Version $version).Version
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $Latest = @{ URL64 = $url64; Version = $version }
 
     return $Latest
 }
