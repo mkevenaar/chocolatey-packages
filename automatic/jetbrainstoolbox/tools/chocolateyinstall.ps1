ac4a26e5f4044d8e4938a7c76927654a83cf1e7d
shawnfunke <null@shawnfunke.dev>
2022-11-01 07:06:05 +0100

    [jetbrainstoolbox] advertise product to current user
    This is to ensure that installations using the Chocolatey background
    service are advertised to the current user as well, the JetBrains
    Toolbox is installed for the user only

--- automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -6,15 +6,15 @@ $checksum     = '831cb3ced25598faf6e364748b9d4e3a19f582034c6b2190dace01b6d526572
 $checksumType = 'sha256'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'JetBrains Toolbox*'
-  fileType      = 'exe'
-  silentArgs   = '/S'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'JetBrains Toolbox*'
+  fileType       = 'exe'
+  silentArgs     = '/S /ju'
+  validExitCodes = @(0)
   url            = $url
   checksum       = $checksum
   checksumType   = $checksumType
-  destination   = $toolsDir
+  destination    = $toolsDir
 }
 
 Install-ChocolateyPackage @packageArgs
