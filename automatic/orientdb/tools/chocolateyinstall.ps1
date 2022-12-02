2e245e89c9b84d47b98dd4c22af0dc2bddfd5cf8
Maurice Kevenaar <maurice@kevenaar.name>
2022-12-02 14:19:37 +0000

    AU: 2 updated - etcher orientdb [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/35ce9ea66b0182ca16b9a5f632c74427aa11d4be

--- automatic/orientdb/tools/chocolateyinstall.ps1
+++ automatic/orientdb/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  fileFullPath   = "$toolsdir\orientdb-community-3.2.12.zip"
+  fileFullPath   = "$toolsdir\orientdb-community-3.2.13.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
