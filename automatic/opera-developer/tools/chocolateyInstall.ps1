44c20ddcf8b8292a81ac096d1460b32017ada8a5
Maurice Kevenaar <maurice@kevenaar.name>
2023-01-02 12:19:10 +0000

    AU: 2 updated - opera-developer vscode-ansible [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/266e26926bf08806a4b60e163581d77d30fa720a

--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/96.0.4640.0/win/Opera_Developer_96.0.4640.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/96.0.4640.0/win/Opera_Developer_96.0.4640.0_Setup_x64.exe'
-  checksum       = '03ac85bebb4c36272d1bc94ca6845bf8c1fb7096198cfa9f16a54aa272d8698d'
-  checksum64     = 'cf691aa193aceb66bbf2e53345af359e136d8ffeb05d0a8527ab1a1b3cbca996'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/96.0.4653.0/win/Opera_Developer_96.0.4653.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/96.0.4653.0/win/Opera_Developer_96.0.4653.0_Setup_x64.exe'
+  checksum       = '6ab1ef816a6eb4185d18660a9f33217b3d7eec9c3de7343ac284cd3223d029a1'
+  checksum64     = '16869e7cc6da012ee67558bb668530ca656f450a6a7fee2d8ddca795468a4cf2'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '96.0.4640.0'
+$version = '96.0.4653.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # clean up old extension
 Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
 
-Install-VsCodeExtension -extensionId 'redhat.ansible@1.1.32'
+Install-VsCodeExtension -extensionId 'redhat.ansible@1.1.34'
