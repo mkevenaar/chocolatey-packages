5abd4ed537add814fe93f1296fa66c2ae2144a7b
Maurice Kevenaar <maurice@kevenaar.name>
2023-01-23 11:19:15 +0000

    AU: 1 updated - anyrail6 [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/beb0d46258ede89c4c3020f4affb3b31d09d7923

--- automatic/anyrail6/tools/chocolateyinstall.ps1
+++ automatic/anyrail6/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.anyrail.com/downloads/AnyRail/6.50/AnyRail6.50.1.msi'
-$checksum     = 'b532822cc62e1c309956ff1e77d4aeffc113a18d16e73b00e46411f76de8447b'
+$url          = 'https://www.anyrail.com/downloads/AnyRail/6.50/AnyRail6.50.2.msi'
+$checksum     = '266165811181cf65956f3f6c63bf9757e271f581bfb004c00fcf675008810b77'
 $checksumType = 'sha256'
 
 $packageArgs = @{
