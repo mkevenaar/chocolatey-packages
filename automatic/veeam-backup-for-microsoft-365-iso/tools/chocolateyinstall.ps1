3e4e8d71fb2322d68857c3baffb3630d881ee8ab
Maurice Kevenaar <maurice@kevenaar.name>
2023-02-28 09:19:27 +0000

    AU: 9 updated - veeam-backup-for-microsoft-365 veeam-backup-for-microsoft-365-console veeam-backup-for-microsoft-365-iso veeam-backup-for-microsoft-365-management veeam-backup-for-microsoft-365-rest-api veeam-backup-for-microsoft-365-server veeam-explorer-for-microsoft-exchange-m365 veeam-explorer-for-microsoft-sharepoint-m365 veeam-explorer-for-microsoft-teams-m365 [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/696f0287d1a3e17b3f883ebe79c8e7adb39182c5

--- automatic/veeam-backup-for-microsoft-365-iso/tools/chocolateyinstall.ps1
+++ automatic/veeam-backup-for-microsoft-365-iso/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://download2.veeam.com/VBO/v6/VeeamBackupMicrosoft365_6.1.0.438_P20221215.iso'
-$checksum = '2357cda0228a97fddcdb16e7975d532ca0fe7c51adf33350c6dd6f34b1cb65a7'
+$url = 'https://download2.veeam.com/VBO/v7/VeeamBackupMicrosoft365_7.0.0.2911.iso'
+$checksum = '6173603ede34a15a719bf7343d0adad4e7e76a70926e38ca01bb332dd4549fc5'
 $checksumType = 'sha256'
 
-$filename = 'VeeamBackupMicrosoft365_6.1.0.438_P20221215.iso'
+$filename = 'VeeamBackupMicrosoft365_7.0.0.2911.iso'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
--- automatic/veeam-explorer-for-microsoft-sharepoint-m365/tools/chocolateyinstall.ps1
+++ automatic/veeam-explorer-for-microsoft-sharepoint-m365/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $isoPackageName = 'veeam-backup-for-microsoft-365-iso'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
-$filename = 'VeeamBackupMicrosoft365_6.1.0.438_P20221215.iso'
+$filename = 'VeeamBackupMicrosoft365_7.0.0.2911.iso'
 $installPath = Join-Path  (Join-Path $commonPath $isoPackageName) $filename
 
 $fileLocation = 'Explorers\VeeamExplorerForSharePoint.msi'
