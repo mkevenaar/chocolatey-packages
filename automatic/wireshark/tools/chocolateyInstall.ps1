2bacaad5cb139dee835e0784bff7852c8d62a92b
Maurice Kevenaar <maurice@kevenaar.name>
2023-03-02 23:19:10 +0000

    AU: 1 updated - wireshark [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/7d656da5f8f57f2d251d3cb54e6c9413eff77859

--- automatic/wireshark/tools/chocolateyInstall.ps1
+++ automatic/wireshark/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file64        = "$toolsDir\Wireshark-win64-4.0.3.exe"
+  file64        = "$toolsDir\Wireshark-win64-4.0.4.exe"
   softwareName  = 'Wireshark*'
   silentArgs     = '/S /quicklaunchicon=no'
   validExitCodes = @(0)
