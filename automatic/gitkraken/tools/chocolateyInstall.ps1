91e61259bb61f88b22e25559da9e05759fb6578f
Maurice Kevenaar <maurice@kevenaar.name>
2023-03-07 20:19:17 +0000

    AU: 2 updated - gitkraken powertoys [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/b02ee3eb642e138a364324315dfb2dfa6d4e9a4c

--- automatic/gitkraken/tools/chocolateyInstall.ps1
+++ automatic/gitkraken/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32          = 'https://release.gitkraken.com/win32/GitKrakenSetup.exe'
-$checksum32     = '1cc591b44f9f986f087bb63e59ff80e82f9128cc3e7266e8a053d0882bf9ab2d'
+$checksum32     = '6f5de085e662111913ef5c831b97e4673e9f3a9c3440fd523e5ea53caeb6bd19'
 $checksumType32 = 'sha256'
 $url64          = 'https://release.gitkraken.com/win64/GitKrakenSetup.exe'
-$checksum64     = 'a215e0ceaedc3a5a99e65ac2d8e82831447df7aad9dce5bb8a6b848d0eb0dd3f'
+$checksum64     = '358628fbc820c83e17260c4baa824ae839cd8020e8c61e1e3b867276d1ec5a56'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
