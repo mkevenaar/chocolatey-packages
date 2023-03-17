8789a4dbcae42865415cc58a7dfeef30a9a78e43
Maurice Kevenaar <maurice@kevenaar.name>
2023-03-17 11:41:03 +0000

    AU: 1 updated - meshmixer [skip ci] https://gist.github.com/675e3f672bda54270f8d7f5415f9374d/f3219a5d540a21845d6ab81ba9ae51cdb7ef2908

--- automatic/meshmixer/tools/chocolateyinstall.ps1
+++ automatic/meshmixer/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'http://www.meshmixer.com/downloads/Autodesk_Meshmixer_v3p5_Win64.exe'
+$url64          = 'https://meshmixer.com/downloads/Autodesk_Meshmixer_v3p5_Win64.exe'
 $checksum64     = '5d08cb6254e4d4280137d6d37ec689fc3f0c61dad08bbc978a451ac16b7f271c'
 $checksumType64 = 'sha256'
 
