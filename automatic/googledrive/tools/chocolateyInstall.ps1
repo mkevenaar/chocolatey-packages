417698bd5f5174160b973b042dcbd6713fa39751
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-03-18 23:45:52 -0400

    (googledrive) Improve handling of uninstalls, reinstalls, and package upgrades/downgrades

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -1,21 +1,35 @@
 ﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
-$checksum     = 'C58593C545A036FF2C2B03EA6DB2905CB3E02151F3E3A3539AA1007F8546A201'
-$checksumType = 'sha256'
+[version] $softwareVersion = '72.0.2.0'
+$installedVersion = Get-InstalledVersion
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
-  fileType       = 'exe'
-  url            = $url
-  checksum       = $checksum
-  checksumType   = $checksumType
-  softwareName  = 'Google Drive*'
-  silentArgs    = "--silent --desktop_shortcut"
-  validExitCodes= @(0,1641,3010)
+if ($installedVersion -eq $softwareVersion -and !$env:ChocolateyForce) {
+  Write-Output "Google Drive $version is already installed. Skipping download and installation."
 }
+else {
+  if ($softwareVersion -le $installedVersion) {
+    Write-Output "Current installed version (v$installedVersion) must be uninstalled first..."
+    Uninstall-CurrentVersion
+    throw 'Windows must be rebooted before installation can be completed!'
+  }
 
-Install-ChocolateyPackage @packageArgs
+  $url = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
+  $checksum = 'C58593C545A036FF2C2B03EA6DB2905CB3E02151F3E3A3539AA1007F8546A201'
+  $checksumType = 'sha256'
 
+  $packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    checksum       = $checksum
+    checksumType   = $checksumType
+    softwareName   = 'Google Drive*'
+    silentArgs     = "--silent --desktop_shortcut"
+    validExitCodes = @(0, 1641, 3010)
+  }
+
+  Install-ChocolateyPackage @packageArgs
+}
