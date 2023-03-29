4f9e29a40a0069b129a7a242cee097ee185343a7
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-03-28 21:53:45 -0400

    (googledrive) Reformat for editorconfig compliance

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
-$checksum     = '86783FF5907DE0A85D21738E0A20E502F4D27AC84DBFA3B1232A3EF27A297D26'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
+$checksum = '86783FF5907DE0A85D21738E0A20E502F4D27AC84DBFA3B1232A3EF27A297D26'
 $checksumType = 'sha256'
 
 $packageArgs = @{
@@ -12,9 +12,9 @@ $packageArgs = @{
   url            = $url
   checksum       = $checksum
   checksumType   = $checksumType
-  softwareName  = 'Google Drive*'
-  silentArgs    = "--silent --desktop_shortcut --skip_launch_new"
-  validExitCodes= @(0,1641,3010)
+  softwareName   = 'Google Drive*'
+  silentArgs     = "--silent --desktop_shortcut --skip_launch_new"
+  validExitCodes = @(0, 1641, 3010)
 }
 
 Install-ChocolateyPackage @packageArgs
