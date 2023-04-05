40f7414fe6a1cc1f764060c1d244ce2d8180fc28
Maurice Kevenaar <maurice@kevenaar.name>
2023-04-05 12:16:54 +0000

    AU: 2 updated - firefox-nightly jetbrains-rider [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/341cb3abf986bc03e38a8967e71949f71f06c2e5

--- automatic/jetbrains-rider/tools/chocolateyInstall.ps1
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2022.3.3.exe'
-$checksum     = 'd4b3fa0d17adccad5cb18da3809981c0bdc225912024e1a33d7f78b68e3e3795'
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2023.1.exe'
+$checksum     = 'eebcaf3d2f8fd374565ff98122356667d03159bde75cf5be5a210cf820a93dd6'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
