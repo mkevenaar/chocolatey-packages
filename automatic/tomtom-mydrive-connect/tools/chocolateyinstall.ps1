e58fb48d4613cbf3a6434ea3451c19325fb6c398
Maurice Kevenaar <maurice@kevenaar.name>
2023-04-14 17:17:31 +0000

    AU: 1 updated - tomtom-mydrive-connect [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/5114c49b71ff0c9f45c5d54889e75546eb3ddfb3

--- automatic/tomtom-mydrive-connect/tools/chocolateyinstall.ps1
+++ automatic/tomtom-mydrive-connect/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://cdn.sa.services.tomtom.com/static/sa/Windows/InstallTomTomMyDriveConnect.exe'
-$checksum     = '385A8BE499894184B8215BB9583C1D3EC49C79D9979180245078BBAF42098DCC'
+$checksum     = '93E2B2ECBEA0C9D0800BCD99AFAAFCF9823DCA2320125E2A0986B91FFCB0C766'
 $checksumType = 'sha256'
 
 $packageArgs = @{
