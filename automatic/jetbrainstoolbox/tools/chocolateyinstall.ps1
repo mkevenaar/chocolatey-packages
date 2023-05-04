2f32ef336e9fa6c5b1624438834f19e3fffdc565
Maurice Kevenaar <maurice@kevenaar.name>
2023-05-04 15:16:40 +0000

    AU: 1 updated - jetbrainstoolbox [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/790160d82462cbbb1b7ad33467a46a84f6c94eb6

--- automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.28.0.15158.exe'
-$checksum     = 'f8a689086e91e824c812b2b3d7bd8ddee795e9b8d7554bf6f16c9f7820f57372'
+$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.28.1.15219.exe'
+$checksum     = '3c7aec4a7d241ec2254c862d621b81c80d8b07239fd907f76b309a4f27180fe3'
 $checksumType = 'sha256'
 
 $packageArgs = @{
