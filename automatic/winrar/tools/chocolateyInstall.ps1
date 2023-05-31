a5ec4d46881776691b73089f777dfd39be2a9a89
Maurice Kevenaar <maurice@kevenaar.name>
2023-05-31 08:16:37 +0000

    AU: 1 updated - winrar [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/0bf09daeabac0d4baab5e452d5e303a488facfce

--- automatic/winrar/tools/chocolateyInstall.ps1
+++ automatic/winrar/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $LCID = (Get-UICulture).LCID
-$url_version = '621'
+$url_version = '622'
 
 $checksumType32 = 'sha256'
 $checksumType64 = 'sha256'
