e96583278aea8444057547bc4d97cb7889c602f5
Maurice Kevenaar <maurice@kevenaar.name>
2023-06-21 12:34:31 +0000

    AU: 1 updated - vscode-ansible [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/c10ef7a6cead3051115bd3e848b9efc04c418767

--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # clean up old extension
 Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
 
-Install-VsCodeExtension -extensionId 'redhat.ansible@2.3.74'
+Install-VsCodeExtension -extensionId 'redhat.ansible@2.4.78'
