31b529a4ea6fbecd7e2bd74d9b359ca8d7705cdb
Maurice Kevenaar <maurice@kevenaar.name>
2023-06-22 00:34:20 +0000

    AU: 1 updated - clion-ide [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/fdf0a8c283bf8802692912471538978d9abc55b5

--- automatic/clion-ide/tools/chocolateyinstall.ps1
+++ automatic/clion-ide/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/cpp/CLion-2023.1.3.exe'
-$checksum     = '49e89a4aedd5e26b50ae69eb3e67eae0b76e53921528db6a7c93e3c00d43a667'
+$url          = 'https://download.jetbrains.com/cpp/CLion-2023.1.4.exe'
+$checksum     = 'cc53253d1da392efb78e3bf036c3e16d02dc12cab40eae87584e34af858b5994'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
