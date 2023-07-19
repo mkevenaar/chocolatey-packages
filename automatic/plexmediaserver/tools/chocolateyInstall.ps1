0cd1285efda58c586dd87138a4a6b1868d8e18c6
Maurice Kevenaar <maurice@kevenaar.name>
2023-07-19 21:34:42 +0000

    AU: 1 updated - plexmediaserver [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/23904c6155d8622c33bc843ffdf4ae4d01f783d0

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.plex.tv/plex-media-server-new/1.32.4.7195-7c8f9d3b6/windows/PlexMediaServer-1.32.4.7195-7c8f9d3b6-x86.exe'
-$checksum32     = '85b67822f5adb73a4041b8f0c13d39c21bf9526c19f706f28020aaa315b5ad0e'
+$url32          = 'https://downloads.plex.tv/plex-media-server-new/1.32.5.7328-2632c9d3a/windows/PlexMediaServer-1.32.5.7328-2632c9d3a-x86.exe'
+$checksum32     = '63068bd2aa1364375703b1248bb1bcd6ebc0490bf97381994561e8e6c9bbf8e8'
 $checksumType32 = 'sha256'
-$url64          = 'https://downloads.plex.tv/plex-media-server-new/1.32.4.7195-7c8f9d3b6/windows/PlexMediaServer-1.32.4.7195-7c8f9d3b6-x86_64.exe'
-$checksum64     = '5b88d41d207bcf97b864f7ccf935ac46ce07cfa45edfd333845c6b9849405087'
+$url64          = 'https://downloads.plex.tv/plex-media-server-new/1.32.5.7328-2632c9d3a/windows/PlexMediaServer-1.32.5.7328-2632c9d3a-x86_64.exe'
+$checksum64     = '7d2da1cad6fce97797e507cc5ec0e4d5697cfde1dff05eff91a46e640372dbeb'
 $checksumType64 = 'sha256'
 
 Start-CheckandStop "Plex Media Server"
