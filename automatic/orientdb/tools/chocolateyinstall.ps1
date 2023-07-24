cfb8e07463f1b2cd7d67b4d158a1ad7f7c05ce73
Maurice Kevenaar <maurice@kevenaar.name>
2023-07-24 15:34:34 +0000

    AU: 1 updated - orientdb [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/4ced9fbfd4144e13997da3bb23d44cfa1be453ff

--- automatic/orientdb/tools/chocolateyinstall.ps1
+++ automatic/orientdb/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  fileFullPath   = "$toolsdir\orientdb-community-3.2.20.zip"
+  fileFullPath   = "$toolsdir\orientdb-community-3.2.21.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
