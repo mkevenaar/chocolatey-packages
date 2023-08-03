239b8d2c21ac027a434113d16d957037150f6c64
Maurice Kevenaar <maurice@kevenaar.name>
2023-08-03 23:34:18 +0000

    AU: 1 updated - crystaldiskinfo.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/5e356a0833b37d4488405a7baa590021b48f6345

--- automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
+++ automatic/crystaldiskinfo.portable/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $linkName = "CrystalDiskInfo.lnk"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = $toolsDir
-  file          = "$toolsDir\CrystalDiskInfo9_1_0.zip"
+  file          = "$toolsDir\CrystalDiskInfo9_1_1.zip"
 }
 
 Get-ChocolateyUnzip  @packageArgs
