004d9b3a0beb8979851169a5d437f946f2070c26
Maurice Kevenaar <maurice@kevenaar.name>
2023-08-14 20:34:40 +0000

    AU: 3 updated - mariadb mariadb.install mariadb.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/3ef8a5e6bb2d2bfb5cd481e55488b4af30e49449

--- automatic/mariadb.install/tools/chocolateyInstall.ps1
+++ automatic/mariadb.install/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  file64         = "$toolsdir\mariadb-11.2.0-winx64.msi"
+  file64         = "$toolsdir\mariadb-11.0.3-winx64.msi"
   softwareName   = 'MariaDB *'
   silentArgs     = "SERVICENAME=MySQL /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0,3010)
