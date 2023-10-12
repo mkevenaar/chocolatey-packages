9537d1b243c52dec9e11a8857596995cc2a167c3
Maurice Kevenaar <maurice@kevenaar.name>
2023-10-12 14:04:00 +0000

    AU: 9 updated - veeam-backup-for-microsoft-365 veeam-backup-for-microsoft-365-console veeam-backup-for-microsoft-365-iso veeam-backup-for-microsoft-365-management veeam-backup-for-microsoft-365-rest-api veeam-backup-for-microsoft-365-server veeam-explorer-for-microsoft-exchange-m365 veeam-explorer-for-microsoft-sharepoint-m365 veeam-explorer-for-microsoft-teams-m365 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/0fd4bf2fa25530ef053b0a261e1b710116c32722

--- automatic/veeam-backup-for-microsoft-365-iso/tools/chocolateyinstall.ps1
+++ automatic/veeam-backup-for-microsoft-365-iso/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://download2.veeam.com/VBO/v7/VeeamBackupMicrosoft365_7.0.0.3968_P20230704.iso'
-$checksum = '155c2e7999664704e406d652a362bd2a6be0f13a2bc03c15917815f5d4b1d306'
+$url = 'https://download2.veeam.com/VBO/v7/VeeamBackupMicrosoft365_7.0.0.4385_P20231005.iso'
+$checksum = 'b13fa6aa6ccf7124e23ff52dd771495bf77aaf3833490aafd03c7a946b6ee2ec'
 $checksumType = 'sha256'
 
-$filename = 'VeeamBackupMicrosoft365_7.0.0.3968_P20230704.iso'
+$filename = 'VeeamBackupMicrosoft365_7.0.0.4385_P20231005.iso'
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath $filename
 
