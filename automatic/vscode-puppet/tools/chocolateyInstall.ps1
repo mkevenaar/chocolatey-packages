5c164c30c4810928d22c52907e04d31d503c6167
Maurice Kevenaar <maurice@kevenaar.name>
2023-10-17 10:10:19 +0000

    AU: 1 updated - vscode-puppet [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/2cfea38ef676512cf141f9212b77f76aa616c7e1

--- automatic/vscode-puppet/tools/chocolateyInstall.ps1
+++ automatic/vscode-puppet/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'puppet.puppet-vscode@1.4.0'
+Install-VsCodeExtension -extensionId 'puppet.puppet-vscode@1.4.2'
 
 Write-Warning "If installed, you'll need to uninstall the old 'jpogran.puppet-vscode' puppet extension manually."
