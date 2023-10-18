a6dfdd358e39aac273c3fb0576aab8e6f118c5e2
Maurice Kevenaar <maurice@kevenaar.name>
2023-10-18 13:38:17 +0200

    (zettlr) fix check (#211)

--- automatic/zettlr/update.ps1
+++ automatic/zettlr/update.ps1
@@ -1,36 +1,40 @@
-Import-Module au
+Import-Module AU
 
-$releases = 'https://github.com/Zettlr/Zettlr/releases/latest'
-
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    #Zettlr-win32-x64-1.2.3.exe
-    $re  = "Zettlr-(.+\d)-x64.exe"
-    $url = $download_page.links | Where-Object href -match $re | Where-Object href -NotMatch "arm" | Select-Object -First 1 -expand href
-
-    $version = ([regex]::Match($url,$re)).Captures.Groups[1].value
-    $url = 'https://github.com' + $url
-
-    return @{
-        URL32 = $url
-        Version = $version
-        FileType = 'exe'
-    }
-}
+$releases = 'https://api.github.com/repos/Zettlr/Zettlr/releases/latest'
 
 function global:au_SearchReplace {
   return @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+    'tools\chocolateyInstall.ps1' = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
     }
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"    = @{
       "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
       "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
       "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
       "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+    }
+  }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $header = @{
+    "Authorization" = "token $env:github_api_key"
+  }
+  $download_page = Invoke-RestMethod -Uri $releases -Headers $header
+
+  #Zettlr-win32-x64-1.2.3.exe
+  $asset = $download_page.assets | Where-Object -Property name -Like "*-x64.exe"
+
+  $version = $download_page.tag_name.Replace('v', '')
+  $version = Get-Version($version)
+  if ($asset) {
+    $url = $asset.browser_download_url
+    return @{
+      URL32    = $url
+      Version  = $version
+      FileType = 'exe'
     }
   }
 }
