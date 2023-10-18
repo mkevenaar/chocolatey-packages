fed5076651727075fe22f97938aead856a715162
Maurice Kevenaar <maurice@kevenaar.name>
2023-10-18 15:17:12 +0000

    AU: 1 updated - opera-beta [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/4f7a559e66c893a7bd0bdd0fe912547f6bc7a635

--- automatic/opera-beta/tools/chocolateyInstall.ps1
+++ automatic/opera-beta/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-beta/104.0.4944.10/win/Opera_beta_104.0.4944.10_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-beta/104.0.4944.10/win/Opera_beta_104.0.4944.10_Setup_x64.exe'
-  checksum       = '1ccc99e3a02c7818bd7414fac2db342f28c54ad5f53b8f5985a9f5b4dbbf5ab6'
-  checksum64     = '42814b2edb1a30f12ac79c073a0ecb49aaee8ca197cced9042131c19f95cd2db'
+  url            = 'https://get.geo.opera.com/pub/opera-beta/104.0.4944.18/win/Opera_beta_104.0.4944.18_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-beta/104.0.4944.18/win/Opera_beta_104.0.4944.18_Setup_x64.exe'
+  checksum       = 'fc49aaa92e17b69f964511574de0a5b3741c4e2ad05bdfe4618d795a057da4ec'
+  checksum64     = '2df771303cc3619038a35cce78f9f52890f858842a4ea84d692c5e54d4f80309'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '104.0.4944.10'
+$version = '104.0.4944.18'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
