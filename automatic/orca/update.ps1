d0bbf5be2bb50218d19334c610220d34632bbb36
Maurice Kevenaar <maurice@kevenaar.name>
2023-11-17 13:28:45 +0100

    (orca) fix version check

--- automatic/orca/update.ps1
+++ automatic/orca/update.ps1
@@ -16,7 +16,7 @@ function global:au_GetLatest {
   $url32 = $download_page.links | Where-Object OuterHTML -match "Download the installer" | Select-Object -First 1 -expand href
   $url32 = Get-RedirectedUrl $url32
 
-  $re = 'Windows SDK \((.+\d)\)'
+  $re = 'Windows SDK \(([\d\.]+)\)'
   $version = ([regex]::Match($download_page.Content,$re)).Captures.Groups[1].value
 
   return @{
