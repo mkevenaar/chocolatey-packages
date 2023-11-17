c92046f034dea4a08a62f0af809df90bcb8a66c2
Maurice Kevenaar <maurice@kevenaar.name>
2023-11-17 13:18:20 +0100

    (mysql) fix update checks

--- automatic/mysql.workbench/update.ps1
+++ automatic/mysql.workbench/update.ps1
@@ -1,26 +1,28 @@
 import-module au
 
-$releases = 'http://dev.mysql.com/downloads/tools/workbench/'
+$releases = 'https://api.github.com/repos/mysql/mysql-workbench/tags'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
-     }
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    
-    $version = ($download_page.ParsedHtml.getElementsByTagName('h1') | ? innerhtml -match "^MySQL Workbench").innerhtml -replace "^MySQL Workbench "
-    $version = $version.Trim()
+  $header = @{
+    "Authorization" = "token $env:github_api_key"
+  }
+  $download_page = Invoke-RestMethod -Uri $releases -Headers $header
 
-    $url = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-' + $version + '-winx64.msi'
-    $Latest = @{ URL32 = $url; Version = $version }
-    return $Latest
+  $version = Get-Version($download_page[0].name)
+
+  $url = 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-' + $version + '-winx64.msi'
+  $Latest = @{ URL32 = $url; Version = $version }
+  return $Latest
 }
 
 update -ChecksumFor 32
--- automatic/mysql/update.ps1
+++ automatic/mysql/update.ps1
@@ -31,18 +31,18 @@ if ($MyInvocation.InvocationName -ne '.') {
 function global:au_AfterUpdate ($Package) {
 
     if ($Package.RemoteVersion -ne $Package.NuspecVersion) {
-  
+
         Get-RemoteFiles -NoSuffix
-  
+
         $file = [IO.Path]::Combine("tools", $Latest.FileName32)
-  
+
         Write-Output "Submitting file $file to VirusTotal"
-  
+
         # Assumes vt-cli Chocolatey package is installed!
         vt.exe scan file $file --apikey $env:VT_APIKEY
-  
+
         Remove-Item $file -ErrorAction Ignore
-  
+
         $Latest.Remove("FileName32")
     }
   }
