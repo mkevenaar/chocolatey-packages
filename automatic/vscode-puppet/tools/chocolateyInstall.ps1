43ce745667b1af93f04a40c8b3bde9da1dd32eeb
Maurice Kevenaar <maurice@kevenaar.name>
2023-11-27 17:16:58 +0000

    AU: 2 updated - firefox-dev vscode-puppet [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/19486b9d157a768503a26e371288551188ef26fe

--- automatic/vscode-puppet/tools/chocolateyInstall.ps1
+++ automatic/vscode-puppet/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'puppet.puppet-vscode@1.4.2'
+Install-VsCodeExtension -extensionId 'puppet.puppet-vscode@1.5.0'
 
 Write-Warning "If installed, you'll need to uninstall the old 'jpogran.puppet-vscode' puppet extension manually."
