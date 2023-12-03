91d1c442729c0aead45c3c36bcabc9d073e35e90
Maurice Kevenaar <maurice@kevenaar.name>
2023-12-03 21:16:51 +0000

    AU: 1 updated - googledrive [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/4bf36e1fa59d1eff996d37a5df9529ed7a74a882

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-[version] $softwareVersion = '84.0.10.0'
+[version] $softwareVersion = '84.0.11.0'
 $installedVersion = Get-InstalledVersion
 
 if ($installedVersion -eq $softwareVersion -and !$env:ChocolateyForce) {
@@ -16,7 +16,7 @@ else {
   }
 
   $url = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
-  $checksum = 'A41ABB86E1D3E42892BD3CBEF5C567FA910965A183E94F0E5D39177F4D635CE0'
+  $checksum = '47C2F89791B5B6FE4A9061617F5507A6FFADDBFD6D917C34869F0C9B8D5BD1E0'
   $checksumType = 'sha256'
 
   $packageArgs = @{
