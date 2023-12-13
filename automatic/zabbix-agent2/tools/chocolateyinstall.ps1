8de67051fa35f46cff2fb0bd7630941a56eab4d8
Maurice Kevenaar <maurice@kevenaar.name>
2023-12-13 15:16:55 +0000

    AU: 3 updated - zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/77d50f49a8e97927ad4995011308ec60a2df2d23

--- automatic/zabbix-agent2/tools/chocolateyinstall.ps1
+++ automatic/zabbix-agent2/tools/chocolateyinstall.ps1
@@ -39,8 +39,8 @@ if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'MSI'
-  file          = "$toolsDir\zabbix_agent2-6.4.10-windows-i386-openssl.msi"
-  file64        = "$toolsDir\zabbix_agent2-6.4.10-windows-amd64-openssl.msi"
+  file          = "$toolsDir\zabbix_agent2-6.0.25-windows-i386-openssl.msi"
+  file64        = "$toolsDir\zabbix_agent2-6.0.25-windows-amd64-openssl.msi"
   softwareName  = "Zabbix Agent *"
 
   SilentArgs     = $SilentArgs
