9578741a7857e40aaad64ab25b4cf4713213eb4b
Maurice Kevenaar <maurice@kevenaar.name>
2023-12-26 16:33:44 +0100

    (tcpvcon) migrated package

---
+++ automatic/tcpvcon/README.md
@@ -0,0 +1,17 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/mkevenaar/chocolatey-packages@063ff9b74c2db9c04d9fd1fb9a239adbea3c0d71/icons/tcpvcon.png" width="48" height="48"/> [tcpvcon](https://community.chocolatey.org/packages/tcpvcon)
+
+
+TCPVcon will show you detailed listings of all TCP and UDP endpoints on your system, including the local and remote addresses and state of TCP connections. On Windows Server 2008, Vista, and XP, TCPVcon also reports the name of the process that owns the endpoint. TCPVcon provides a more informative and conveniently presented subset of the Netstat program that ships with Windows.
+
+#### Command line usage
+
+Tcpvcon usage is similar to that of the built-in Windows netstat utility:
+
+__tcpvcon__ [__-a__] [__-c__] [__-n__] [[process name] [PID]]
+
+__-a__  Show all endpoints (default is to show established TCP connections)
+
+__-c__  Print output as CSV
+
+__-n__  Don't resolve addresses
+
---
+++ automatic/tcpvcon/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download.sysinternals.com/files/TCPView.zip'
+$checksum     = '4fc5ceba3e1b27ad95a24df35d094b454ec5f9478e12a8ca2b1b222705b9683b'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage  @packageArgs
+
+# TCPView and Tcpvcon files come in 1 zip file, removing TCPView files
+# Currently tcpview.chm tcpview.exe tcpview64.exe tcpview64a.exe
+Remove-Item -Path $toolsDir\tcpview*.*
+
+# I cannot revert this registery key, it's being shared with the tcpview package.
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'TCPView'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  Write-Warning "Failed setting registry value."
+}
