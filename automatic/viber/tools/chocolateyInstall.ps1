9dd44802ce111307972cb70e67b0331601095033
Maurice Kevenaar <maurice@kevenaar.name>
2024-01-24 14:16:44 +0000

    AU: 1 updated - viber [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/9c70a3c6ce930a73ef5c0f7a6c8d67eb7435e4e0

--- automatic/viber/tools/chocolateyInstall.ps1
+++ automatic/viber/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://download.cdn.viber.com/desktop/windows/ViberSetup.exe'
-$checksum     = 'C2700437DD7F204AB11BE27DEB37E726F0498859407CB4B161FC2E0DC03F2D97'
+$checksum     = '1E6221293C1647289C1AB15EB5DB9767377791B2829E4729F9CC708F2929AECF'
 $checksumType = 'sha256'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
