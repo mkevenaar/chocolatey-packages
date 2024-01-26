8cc6e5bcdb95c504fa381330c747f371e70fc4d5
Maurice Kevenaar <maurice@kevenaar.name>
2024-01-26 11:17:27 +0000

    AU: 1 updated - goland [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/9c2fc3696558063b520d91e346d7ef25cac5f698

--- automatic/goland/tools/chocolateyInstall.ps1
+++ automatic/goland/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/go/goland-2023.3.2.exe'
-$checksum     = '539d35737da07504b6cdd1f3d5c86548639b91c7613ec0b924d04daf261360f3'
+$url          = 'https://download.jetbrains.com/go/goland-2023.3.3.exe'
+$checksum     = '5f0825a8adf7f21b34abea260b8a9833a18f7d2c189796c4fe3358c8e8dabffa'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
