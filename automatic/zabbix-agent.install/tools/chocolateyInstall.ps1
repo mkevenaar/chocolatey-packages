b5924ecde7c3576d54b1a45f6b9416de67a45b95
Maurice Kevenaar <maurice@kevenaar.name>
2024-01-29 16:17:09 +0000

    AU: 2 updated - zabbix-agent zabbix-agent.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/992314eb55a6d8e4eda245cda9a2e761caeec66d

--- automatic/zabbix-agent.install/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent.install/tools/chocolateyInstall.ps1
@@ -35,8 +35,8 @@ if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'MSI'
-  file          = "$toolsDir\zabbix_agent-6.0.26-windows-i386-openssl.msi"
-  file64        = "$toolsDir\zabbix_agent-6.0.26-windows-amd64-openssl.msi"
+  file          = "$toolsDir\zabbix_agent-6.4.11-windows-i386-openssl.msi"
+  file64        = "$toolsDir\zabbix_agent-6.4.11-windows-amd64-openssl.msi"
   softwareName  = "Zabbix Agent *"
 
   SilentArgs     = $SilentArgs
--- automatic/zabbix-agent/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$version = '6.0.26'
+$version = '6.4.11'
 $title = 'Zabbix Agent'
 
 $configDir = Join-Path $env:PROGRAMDATA 'zabbix'
@@ -19,8 +19,8 @@ $service = Get-WmiObject -Class Win32_Service -Filter "Name=`'$title`'"
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   unzipLocation = $tempDir
-  file          = "$toolsDir\zabbix_agent-6.0.26-windows-i386-openssl.zip"
-  file64        = "$toolsDir\zabbix_agent-6.0.26-windows-amd64-openssl.zip"
+  file          = "$toolsDir\zabbix_agent-6.4.11-windows-i386-openssl.zip"
+  file64        = "$toolsDir\zabbix_agent-6.4.11-windows-amd64-openssl.zip"
 }
 
 try {
