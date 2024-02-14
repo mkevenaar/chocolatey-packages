4b0be2793e70b1f7d4a536737012477a71c07348
Maurice Kevenaar <maurice@kevenaar.name>
2024-02-14 10:46:42 +0100

    (grafana) switch to stream

--- automatic/grafana/update.ps1
+++ automatic/grafana/update.ps1
@@ -1,13 +1,13 @@
 Import-Module AU
 
-$releases = 'https://api.github.com/repos/grafana/grafana/releases/latest'
+$releases = "https://api.github.com/repos/grafana/grafana/releases"
 
 function global:au_SearchReplace {
   return @{
-    'tools\chocolateyInstall.ps1' = @{
+    ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
     }
-    ".\legal\VERIFICATION.txt"    = @{
+    ".\legal\VERIFICATION.txt"   = @{
       "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
       "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
       "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
@@ -16,27 +16,46 @@ function global:au_SearchReplace {
   }
 }
 
+function CreateStream {
+  param($url32bit, $version)
+
+  $Result = @{
+    Version  = $version
+    URL32    = $url32bit
+    FileType = 'zip'
+  }
+
+  return $Result
+}
+
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
   $header = @{
     "Authorization" = "token $env:github_api_key"
   }
-  $download_page = Invoke-RestMethod -Uri $releases -Headers $header
+  $json = Invoke-RestMethod -Uri $releases -Headers $header
 
+  $streams = @{ }
 
-  $version = $download_page.tag_name.Replace('v', '')
-  $version = Get-Version($version)
+  foreach ($release in $json) {
+    $version = $release.tag_name.Replace('v', '')
+    $version = Get-Version($version)
 
-  $url = "https://dl.grafana.com/oss/release/grafana-$version.windows-amd64.zip"
+    $url = "https://dl.grafana.com/oss/release/grafana-$version.windows-amd64.zip"
 
-  return @{
-    URL32    = $url
-    Version  = $version
-    FileType = 'zip'
+    $streamVersion = $version.toString(2)
+    if ($release.prerelease) {
+      $streamVersion += '-rc'
+    }
+    if (!$streams.ContainsKey("$streamVersion")) {
+      $streams.Add($streamVersion, (CreateStream $url $version))
+    }
+
   }
+  return  @{ Streams = $streams }
 }
 
-if ($MyInvocation.InvocationName -ne '.') {
+if ($MyInvocation.InvocationName -ne ".") {
   update -ChecksumFor None
 }
