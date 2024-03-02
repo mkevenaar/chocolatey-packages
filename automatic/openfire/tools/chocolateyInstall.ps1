d8c4b264743f29a29056ca20a7b2a65e651af05d
Maurice Kevenaar <maurice@kevenaar.name>
2024-03-02 20:17:21 +0000

    AU: 4 updated - openfire wc3270 wc3270.install wc3270.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/031ab654e934b8e2d9d9e8d645f3aed761f0c9bf

--- automatic/openfire/tools/chocolateyInstall.ps1
+++ automatic/openfire/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_8_0.exe'
-$checksum32     = 'daba71eec8eca9978e22add1198123c045218df95ae02c7d96567870a92a9c75'
+$url32          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_8_1.exe'
+$checksum32     = '141f6eaf374dfb7c4cca345e1b598fed5ce3af9c70062a8cc0d9571e15c29c7d'
 $checksumType32 = 'sha256'
-$url64          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_8_0_x64.exe'
-$checksum64     = 'f0469bb13e38264ae69cb55006a88fd0572dd5b3c41fe1021d1c778336242bcb'
+$url64          = 'https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4_8_1_x64.exe'
+$checksum64     = 'bec5b03ed56146fec2f84593c7e7b269ee5c32b3a0d5f9e175bd41f28a853abe'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
