b8e11dd4aa12e087a2183e98989a8e8a28caf4ac
Maurice Kevenaar <maurice@kevenaar.name>
2024-03-19 16:20:07 +0000

    AU: 2 updated - opera-developer server-backup-agent [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/5d61fcad409a83ee501a3ac7fe03eb75e62e4b87

--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/110.0.5104.0/win/Opera_Developer_110.0.5104.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/110.0.5104.0/win/Opera_Developer_110.0.5104.0_Setup_x64.exe'
-  checksum       = '64ec4ad2e0535b93a2bc9833d0658fedcc35b82b3e50222ade36538ff699c2ba'
-  checksum64     = '5a7079fcd9355899517df6375236cd647a207781ccbbd460e9bd2207066e952d'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/110.0.5111.0/win/Opera_Developer_110.0.5111.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/110.0.5111.0/win/Opera_Developer_110.0.5111.0_Setup_x64.exe'
+  checksum       = '95ff91019fd5dc8ab9c53be0c77ff3bb00c827b2a9d508cb9692aefab8b75bee'
+  checksum64     = 'b8faec12f99b0bd062cfc8bc16c414dfcec44f0342077fa32d1a911a80e0a215'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '110.0.5104.0'
+$version = '110.0.5111.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
--- automatic/server-backup-agent/tools/chocolateyinstall.ps1
+++ automatic/server-backup-agent/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url32          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x86.msi'
-$checksum32     = '8D447D633EFD2F6783115BE39F000165B1E9A3F2F70B324BB2540A114552DB41'
+$checksum32     = '65E9C8F23B726E2C5C62D3A173A3A76743C9D6DC828A7D3C31700C227E0B508E'
 $checksumType32 = 'sha256'
 $url64          = 'http://repo.r1soft.com/trials/ServerBackup-Windows-Agent-x64.msi'
-$checksum64     = 'e3e905ba4a4c96a5592af3f34dd39135ed6675504350574e66d4a9f110de16a3'
+$checksum64     = '26ea72ea7a6129d5a9110a0f3816782f58a342c948fc6961364b60000677b633'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
