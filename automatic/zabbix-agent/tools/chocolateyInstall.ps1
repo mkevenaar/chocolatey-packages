0808043e979d3b137d72032991ec66f1c54bd82c
Maurice Kevenaar <maurice@kevenaar.name>
2024-03-26 10:28:29 +0000

    AU: 3 updated - zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/1ba1e8e2ec9900a84a1b7c643af8f9a5ca7e81bb

--- automatic/zabbix-agent/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$version = '5.0.42'
+$version = '6.4.13'
 $title = 'Zabbix Agent'
 
 $configDir = Join-Path $env:PROGRAMDATA 'zabbix'
@@ -19,8 +19,8 @@ $service = Get-WmiObject -Class Win32_Service -Filter "Name=`'$title`'"
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   unzipLocation = $tempDir
-  file          = "$toolsDir\zabbix_agent-5.0.42-windows-i386-openssl.zip"
-  file64        = "$toolsDir\zabbix_agent-5.0.42-windows-amd64-openssl.zip"
+  file          = "$toolsDir\zabbix_agent-6.4.13-windows-i386-openssl.zip"
+  file64        = "$toolsDir\zabbix_agent-6.4.13-windows-amd64-openssl.zip"
 }
 
 try {
