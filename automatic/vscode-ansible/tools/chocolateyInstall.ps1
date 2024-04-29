23c87902311cf3e6b648becd04af495cc0b2f866
Maurice Kevenaar <maurice@kevenaar.name>
2024-04-29 18:23:11 +0000

    AU: 2 updated - firefox-dev vscode-ansible [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/c2038f3a43e56b52a29ad5ba0f73618a7793632f

--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # clean up old extension
 Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
 
-Install-VsCodeExtension -extensionId 'redhat.ansible@24.4.1526292'
+Install-VsCodeExtension -extensionId 'redhat.ansible@24.4.2141360'
