efad9ca8c048b732d0db46822e71be00619c5dd5
Maurice Kevenaar <maurice@kevenaar.name>
2024-05-05 10:21:03 +0000

    AU: 3 updated - lidarr radarr readarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/181113a8e47e53b63e172cab2c5314aec1a2d344

--- automatic/radarr/tools/chocolateyInstall.ps1
+++ automatic/radarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'radarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Radarr.develop.5.5.1.8747.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Radarr.develop.5.5.1.8747.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Radarr.develop.5.5.2.8781.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Radarr.develop.5.5.2.8781.windows-core-x64-installer.exe"
   softwareName  = 'Radarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/readarr/tools/chocolateyInstall.ps1
+++ automatic/readarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'readarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Readarr.develop.0.3.25.2515.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Readarr.develop.0.3.25.2515.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Readarr.develop.0.3.26.2526.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Readarr.develop.0.3.26.2526.windows-core-x64-installer.exe"
   softwareName  = 'Readarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
