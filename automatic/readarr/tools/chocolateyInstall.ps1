e585b0d8b34dd8e6e297d95374fa5786306b726e
Maurice Kevenaar <maurice@kevenaar.name>
2024-06-09 10:19:52 +0000

    AU: 1 updated - readarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/b06f36a1e0021cd042417310a9f052c7107d8956

--- automatic/readarr/tools/chocolateyInstall.ps1
+++ automatic/readarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'readarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Readarr.develop.0.3.27.2538.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Readarr.develop.0.3.27.2538.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Readarr.develop.0.3.28.2554.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Readarr.develop.0.3.28.2554.windows-core-x64-installer.exe"
   softwareName  = 'Readarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
