52e47e5401c4ca2e30249b83ea036e787714291c
Maurice Kevenaar <maurice@kevenaar.name>
2024-06-25 12:21:14 +0000

    AU: 3 updated - grafana grafana.install vscode-ansible [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/aa7055074960fcd893eb6411745120cb028f0670

--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # clean up old extension
 Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
 
-Install-VsCodeExtension -extensionId 'redhat.ansible@24.8.0'
+Install-VsCodeExtension -extensionId 'redhat.ansible@24.9.250742'
