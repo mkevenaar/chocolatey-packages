bf6299339f74454c1f01b70c3fd457d003dac90f
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-07-09 18:16:20 -0500

    (slack) remove 32 bit installer as no longer offered

--- automatic/slack/tools/chocolateyInstall.ps1
+++ automatic/slack/tools/chocolateyInstall.ps1
@@ -1,9 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.slack-edge.com/desktop-releases/windows/ia32/4.38.127/slack-standalone-4.38.127.0.msi'
-$checksum32     = '0bedbb08fbd508b63291fd39db91d370eabbc66cfb53e01a9aa9fbae6b11f0c5'
-$checksumType32 = 'sha256'
 $url64          = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.38.127/slack-standalone-4.38.127.0.msi'
 $checksum64     = '2623eb314f049dfaa6f99d0158c5a4c05c81ca8203f350082b86549152b151cf'
 $checksumType64 = 'sha256'
@@ -12,9 +9,6 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = $checksumType32
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= $checksumType64
