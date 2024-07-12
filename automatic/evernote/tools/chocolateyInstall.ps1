184a76677eabc1b800573e26467813e842be283a
Maurice Kevenaar <maurice@kevenaar.name>
2024-07-12 06:20:46 +0000

    AU: 3 updated - evernote jackett vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/7dc3cd914d9a3b9d68e25373f8b818419cdcc968

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = '9CF8B8FC938643B2894D5496BF03A06FE1FCBF3B0A1CAB8E17EF3A1ED645BDF2'
+$checksum     = '9E016913A66200DB35544683053F6FBD86B6E2DB3C2286D00AADD3C5A1D8F963'
 $checksumType = 'sha256'
 
 $packageArgs = @{
