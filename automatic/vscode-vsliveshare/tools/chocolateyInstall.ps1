d317aa3842e2558db39bce489641b40387350f15
Maurice Kevenaar <maurice@kevenaar.name>
2024-07-15 20:21:06 +0000

    AU: 4 updated - sonarr vscode-vsliveshare zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/cfd7b70454525ad7225387fa5fea67706210f72a

--- automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
+++ automatic/vscode-vsliveshare/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.5932'
+Install-VsCodeExtension -extensionId 'ms-vsliveshare.vsliveshare@1.0.5936'
