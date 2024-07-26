2cae50656a57d65bdff0fdc256398449f7fc0e95
Maurice Kevenaar <maurice@kevenaar.name>
2024-07-26 16:20:49 +0000

    AU: 3 updated - firefox-dev grafana grafana.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/f786207f6d94f59c235abc2e9127829b5e289c55

--- automatic/grafana/tools/chocolateyinstall.ps1
+++ automatic/grafana/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  file           = "$toolsdir\grafana-10.4.6.windows-amd64.zip"
+  file           = "$toolsdir\grafana-11.1.3.windows-amd64.zip"
   validExitCodes = @(0)
 }
 
