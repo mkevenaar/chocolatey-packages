158d9926ceb3744d826ffcde266f4cd9c23ecb8a
Maurice Kevenaar <maurice@kevenaar.name>
2024-08-14 18:23:51 +0000

    AU: 4 updated - firefox-dev grafana grafana.install plexmediaserver [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/acf1776b8e541403774d169e0bb910b0a9ad7631

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://downloads.plex.tv/plex-media-server-new/1.40.4.8679-424562606/windows/PlexMediaServer-1.40.4.8679-424562606-x86.exe'
-$checksum32     = '8ed300fa9320a273d1a28d614dbe8a003a74579364afb37c7fff21f51eb16809'
+$url32          = 'https://downloads.plex.tv/plex-media-server-new/1.40.5.8854-f36c552fd/windows/PlexMediaServer-1.40.5.8854-f36c552fd-x86.exe'
+$checksum32     = '5e262673c519f6990873fdc8cd8776c4e0ab2f3beaa6be4e786bf4d7d801f099'
 $checksumType32 = 'sha256'
-$url64          = 'https://downloads.plex.tv/plex-media-server-new/1.40.4.8679-424562606/windows/PlexMediaServer-1.40.4.8679-424562606-x86_64.exe'
-$checksum64     = 'c2f54fbe16275106990cf7a792d2808aabae70915c0d938d3130fd298902f39e'
+$url64          = 'https://downloads.plex.tv/plex-media-server-new/1.40.5.8854-f36c552fd/windows/PlexMediaServer-1.40.5.8854-f36c552fd-x86_64.exe'
+$checksum64     = '208a454a7613fc7d1e1aa9563aae0d044eaf9660c7bfd827883d4f9fa7806f71'
 $checksumType64 = 'sha256'
 
 Start-CheckandStop "Plex Media Server"
