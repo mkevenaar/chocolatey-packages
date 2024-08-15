4b285b77aa62d2cd75a05c5fc4daaab76eb0fb2b
Maurice Kevenaar <maurice@kevenaar.name>
2024-08-15 08:22:30 +0000

    AU: 1 updated - evernote [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/d612df637790712ca13d84ed48baf99048f0e8b3

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = '716033907E34B8A7241C89B22C379C46EFA00A9D20300A2FE299F2255753E2FF'
+$checksum     = '871368AB87E5DAEFA014CB5DF6BB7B95FBB5BA7FC505102B6B23F6D90198F701'
 $checksumType = 'sha256'
 
 $packageArgs = @{
