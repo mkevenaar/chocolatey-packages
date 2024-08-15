d2a54d58613a963f91c5f62a7630210414b81a1c
Maurice Kevenaar <maurice@kevenaar.name>
2024-08-15 22:20:11 +0000

    AU: 1 updated - trillian [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/7ae587efdd61085ba36b2ded133ae21bc6524e66

--- automatic/trillian/tools/chocolateyInstall.ps1
+++ automatic/trillian/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://trillian.im/get/windows/6.5/trillian-v6.5.0.42.msi'
-$checksum     = 'acb90f1cd6e42256b6174e45601bffca5e550ffd660f0e988e4b16fdc8737c5b'
+$url          = 'https://trillian.im/get/windows/6.5/trillian-v6.5.0.43.msi'
+$checksum     = 'b5512bff8391e9a533e286cf63778eea7e54bea93c0f0742d5ef5fe1df8040fe'
 $checksumType = 'sha256'
 
 $packageArgs = @{
