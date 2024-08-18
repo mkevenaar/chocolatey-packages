8457068ab0819e29908f3acfff51a6b010b8a0cd
Maurice Kevenaar <maurice@kevenaar.name>
2024-08-18 14:20:22 +0000

    AU: 2 updated - firefox-nightly prowlarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/e1496f7317307724918259eae071ff489015c800

--- automatic/firefox-nightly/tools/chocolateyInstall.ps1
+++ automatic/firefox-nightly/tools/chocolateyInstall.ps1
@@ -34,7 +34,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://archive.mozilla.org/pub/firefox/nightly/2024/08/2024-08-17-21-46-36-mozilla-central/firefox-131.0a1.${locale}.win32.installer.exe"
+    Url = "https://archive.mozilla.org/pub/firefox/nightly/2024/08/2024-08-18-09-40-39-mozilla-central/firefox-131.0a1.${locale}.win32.installer.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2024/08/2024-08-17-21-46-36-mozilla-central/firefox-131.0a1.${locale}.win64.installer.exe"
+    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2024/08/2024-08-18-09-40-39-mozilla-central/firefox-131.0a1.${locale}.win64.installer.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
