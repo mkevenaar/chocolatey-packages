34b8c5ed85a02041a4946b3d94dc527998549fa6
Maurice Kevenaar <maurice@kevenaar.name>
2024-08-27 18:23:38 +0000

    AU: 3 updated - grafana grafana.install microsoft-windows-terminal [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/f3c68a147002b8190f074f89b964569ab5a23c2f

--- automatic/grafana/tools/chocolateyinstall.ps1
+++ automatic/grafana/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  file           = "$toolsdir\grafana-11.1.5.windows-amd64.zip"
+  file           = "$toolsdir\grafana-11.2.0.windows-amd64.zip"
   validExitCodes = @(0)
 }
 
