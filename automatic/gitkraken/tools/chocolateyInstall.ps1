98de4315d1190329c26684f132ebd3a4d21db74e
Cory Knox <me@coryknox.dev>
2024-09-05 14:20:36 -0700

    (gitkraken) Update download URLs
    GitKraken is no longer providing a 32 bit version. They have also moved
    the URL that the files are available from. This updates these URLs and
    removes the 32 bit install.

--- automatic/gitkraken/tools/chocolateyInstall.ps1
+++ automatic/gitkraken/tools/chocolateyInstall.ps1
@@ -1,9 +1,6 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://release.gitkraken.com/win32/GitKrakenSetup.exe'
-$checksum32     = '5328d2cac7e2ba4b8436df01f11d8a011fc009ea099dbacf5b3b2c062bd12d08'
-$checksumType32 = 'sha256'
 $url64          = 'https://release.gitkraken.com/win64/GitKrakenSetup.exe'
 $checksum64     = 'ec3a637cf9f678fefc1a73b636b947ba1ded020728d5db90399bbc6d51db216e'
 $checksumType64 = 'sha256'
@@ -12,9 +9,6 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe' #only one of these: exe, msi, msu
-  url            = $url32
-  checksum       = $checksum32
-  checksumType   = $checksumType32
   url64bit       = $url64
   checksum64     = $checksum64
   checksumType64 = $checksumType64
--- automatic/gitkraken/update.ps1
+++ automatic/gitkraken/update.ps1
@@ -5,9 +5,6 @@ $releases = 'https://www.gitkraken.com/download'
 function global:au_SearchReplace {
   @{
     'tools\chocolateyInstall.ps1' = @{
-      "(^[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-      "(^[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
       "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
       "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
       "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -21,14 +18,12 @@ function global:au_GetLatest {
   $re = 'Latest release: (.+)</span>'
   $version = ([regex]::Match($download_page.content, $re)).Captures.Groups[1].value
 
-  $url32 = 'https://release.gitkraken.com/win32/GitKrakenSetup.exe'
-  $url64 = 'https://release.gitkraken.com/win64/GitKrakenSetup.exe'
+  $url64 = 'https://release.axocdn.com/windows/GitKrakenSetup.exe'
 
   return @{
-    URL32   = $url32
     URL64   = $url64
     Version = $version
   }
 }
 
-update
+update 64
