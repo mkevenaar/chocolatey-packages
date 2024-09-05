0b837a59867459cad85e2d0e2968e7868b4a773f
Maurice Kevenaar <maurice@kevenaar.name>
2024-09-05 09:06:39 +0200

    (bacula) remove 32bit as Bacula dropped 32bit support

--- automatic/bacula/tools/chocolateyInstall.ps1
+++ automatic/bacula/tools/chocolateyInstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://www.bacula.org/download/11249/'
-$checksum32     = 'dbc5b2706ccd401ef677cbfc87afafe6b38e128d7aa30f70f0620e2fb79f9f9e'
-$checksumType32 = 'sha256'
-$url64          = 'https://www.bacula.org/download/11251/'
-$checksum64     = 'a8773a83c4d47251ad5ca51f49d090a99923bb462257e79df2ccc56ba78d8fa6'
+$url64          = 'https://www.bacula.org/download/11489/?tmstv=1723110204'
+$checksum64     = 'd863afadc85eb9c29e4f03c123d18cf77561724e818f3a628f8f184171091f83'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = $checksumType32
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= $checksumType64
--- automatic/bacula/update.ps1
+++ automatic/bacula/update.ps1
@@ -5,9 +5,6 @@ $releases = 'https://www.bacula.org/binary-download-center/'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
             "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -21,16 +18,12 @@ function global:au_GetLatest {
     $re = "Windows (.+\d) 64 bit"
     $url64 = $download_page.Links | Where-Object outerhtml -match $re | Select-Object -First 1 -expand href
 
-    $re = "Windows (.+\d) 32 bit"
-    $url32 = $download_page.Links | Where-Object outerhtml -match $re | Select-Object -First 1 -expand href
-
     $version = ([regex]::Match(($download_page.Links | Where-Object outerhtml -match $re | Select-Object -First 1 -expand outerhtml),$re)).Groups[1].value
 
     return @{
-        URL32 = $url32
         URL64 = $url64
         Version = $version
     }
 }
 
-update
+update -ChecksumFor 64
