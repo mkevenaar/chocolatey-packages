fe314b6837fc52b928c9caeeb2ae05819033ceda
Maurice Kevenaar <maurice@kevenaar.name>
2024-09-11 10:20:47 +0000

    AU: 1 updated - thonny [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/ae64ea6360b4e5849224bf6e3dd30110ae023ba1

--- automatic/thonny/tools/chocolateyinstall.ps1
+++ automatic/thonny/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\thonny-4.1.4.exe"
+  file          = "$toolsDir\thonny-4.1.6.exe"
   softwareName  = 'Thonny*'
   silentArgs    = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /FORCECLOSEAPPLICATIONS"
   validExitCodes= @(0,3010)
