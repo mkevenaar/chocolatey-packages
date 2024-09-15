08116d60c8cd5fe484992ec5db17fa975ec0ab6a
Maurice Kevenaar <maurice@kevenaar.name>
2024-09-15 21:40:20 +0200

    (vscode-ansible) fix package

--- automatic/vscode-ansible/tools/chocolateyInstall.ps1
+++ automatic/vscode-ansible/tools/chocolateyInstall.ps1
@@ -1,6 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-# clean up old extension
-Uninstall-VsCodeExtension -extensionId 'vscoss.vscode-ansible'
-
 Install-VsCodeExtension -extensionId 'redhat.ansible@24.9.5152897'
