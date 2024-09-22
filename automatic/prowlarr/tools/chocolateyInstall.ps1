9521166ad8c4414e3ec563ad28c99a9d06da2910
Maurice Kevenaar <maurice@kevenaar.name>
2024-09-22 06:20:25 +0000

    AU: 4 updated - jackett lidarr prowlarr vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/9f059b43242d385b7477cef1b01e36421ac7fae4

--- automatic/prowlarr/tools/chocolateyInstall.ps1
+++ automatic/prowlarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'prowlarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Prowlarr.develop.1.24.1.4740.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Prowlarr.develop.1.24.1.4740.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Prowlarr.develop.1.24.2.4749.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Prowlarr.develop.1.24.2.4749.windows-core-x64-installer.exe"
   softwareName  = 'Prowlarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
