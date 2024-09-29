4e5adc996befe598ffbf2a5cb5229aaa081d9018
Maurice Kevenaar <maurice@kevenaar.name>
2024-09-29 06:20:40 +0000

    AU: 6 updated - jackett lidarr prowlarr radarr readarr vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/d43529bf30258a8c59ba408a932d7789d4234d59

--- automatic/lidarr/tools/chocolateyInstall.ps1
+++ automatic/lidarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename    = "lidarr"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Lidarr.develop.2.6.1.4370.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Lidarr.develop.2.6.1.4370.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Lidarr.develop.2.6.2.4379.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Lidarr.develop.2.6.2.4379.windows-core-x64-installer.exe"
   softwareName  = 'Lidarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
