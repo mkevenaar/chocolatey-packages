436cb799f2e0ce41d69c78b1c53bded667506b6b
Maurice Kevenaar <maurice@kevenaar.name>
2024-10-01 20:20:11 +0000

    AU: 2 updated - grafana grafana.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/b230c48acde1ce0646829a67aa805198c3d82d06

--- automatic/grafana/tools/chocolateyinstall.ps1
+++ automatic/grafana/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  file           = "$toolsdir\grafana-11.0.6.windows-amd64.zip"
+  file           = "$toolsdir\grafana-11.1.7.windows-amd64.zip"
   validExitCodes = @(0)
 }
 
