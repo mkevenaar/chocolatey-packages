c5685ed6db1e1f33a8f8f7d7024617669464485b
James Ruskin <james@chocolatey.io>
2024-10-17 13:04:21 +0100

    (intel-dsa) Migrates Package from BCurran3
    This brings the package code across from BC's repository, and updates it to fit this ownership and repository style.
    
    The update script could probably be trimmed back a little to just contain one of the two methods - parsing the release table is a bit brash when we could just download the binary. Hopefully it's acceptable - and/or it can be trimmed to the repository preference.

---
+++ automatic/intel-dsa/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop'
+$packageName = 'intel-dsa'
+$url         = 'https://dsadata.intel.com/installer/'
+$checksum    = 'A64BAA1195DBFD4A462D1D2854F40CE293E08C999F91235D4134CCA75021A0E0'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  silentArgs     = '-s -norestart'
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'Intel® Driver & Support Assistant'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
