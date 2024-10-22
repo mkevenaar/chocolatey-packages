a430e54aea4e415551b85bd3f62a22e08c56eee7
Maurice Kevenaar <maurice@kevenaar.name>
2024-10-22 12:21:35 +0000

    AU: 3 updated - grafana grafana.install jetbrainstoolbox [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/9d692e8139eca63fd88ad7c916fbfc594da35331

--- automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.0.34528.exe'
-$checksum     = 'e433f78d99e98c2833e8dcdf8326149ce5ceacba6ab45cea0f07fa0fd4977e28'
+$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.1.34629.exe'
+$checksum     = 'd8c7606d80fc432b4c70516e0039e7dac2711b7af464a53e2833c9b38e533ac8'
 $checksumType = 'sha256'
 
 $packageArgs = @{
