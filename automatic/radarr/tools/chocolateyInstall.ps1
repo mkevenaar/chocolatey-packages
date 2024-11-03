86a21853ea3beaaab9c505edaf1019a92b12db9e
Maurice Kevenaar <maurice@kevenaar.name>
2024-11-03 10:20:33 +0000

    AU: 4 updated - lidarr prowlarr radarr readarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/827b5f8bc6b9e4cdeb23005f5716697f5c54f58b

--- automatic/radarr/tools/chocolateyInstall.ps1
+++ automatic/radarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'radarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Radarr.master.5.14.0.9383.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Radarr.master.5.14.0.9383.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Radarr.develop.5.15.0.9412.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Radarr.develop.5.15.0.9412.windows-core-x64-installer.exe"
   softwareName  = 'Radarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
