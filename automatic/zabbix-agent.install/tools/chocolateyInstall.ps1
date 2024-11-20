dd209cbb1e60ead7ef6857a32ed63daa96165fa5
Maurice Kevenaar <maurice@kevenaar.name>
2024-11-20 04:21:37 +0000

    AU: 4 updated - vlc-nightly zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/975b4e7857472f044eab7db5e08f91a809eb05ac

--- automatic/zabbix-agent.install/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent.install/tools/chocolateyInstall.ps1
@@ -35,8 +35,8 @@ if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'MSI'
-  file          = "$toolsDir\zabbix_agent-5.0.45-windows-i386-openssl.msi"
-  file64        = "$toolsDir\zabbix_agent-5.0.45-windows-amd64-openssl.msi"
+  file          = "$toolsDir\zabbix_agent-6.0.36-windows-i386-openssl.msi"
+  file64        = "$toolsDir\zabbix_agent-6.0.36-windows-amd64-openssl.msi"
   softwareName  = "Zabbix Agent *"
 
   SilentArgs     = $SilentArgs
--- automatic/zabbix-agent/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$version = '5.0.45'
+$version = '6.0.36'
 $title = 'Zabbix Agent'
 
 $configDir = Join-Path $env:PROGRAMDATA 'zabbix'
@@ -19,8 +19,8 @@ $service = Get-WmiObject -Class Win32_Service -Filter "Name=`'$title`'"
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   unzipLocation = $tempDir
-  file          = "$toolsDir\zabbix_agent-5.0.45-windows-i386-openssl.zip"
-  file64        = "$toolsDir\zabbix_agent-5.0.45-windows-amd64-openssl.zip"
+  file          = "$toolsDir\zabbix_agent-6.0.36-windows-i386-openssl.zip"
+  file64        = "$toolsDir\zabbix_agent-6.0.36-windows-amd64-openssl.zip"
 }
 
 try {
