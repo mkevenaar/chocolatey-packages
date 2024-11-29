f2cc1f2170e82c6322a39af7691cb6fd596ccb9e
Maurice Kevenaar <maurice@kevenaar.name>
2024-11-29 14:07:40 +0100

    (samsung-magician) fix package

--- automatic/samsung-magician/tools/chocolateyInstall.ps1
+++ automatic/samsung-magician/tools/chocolateyInstall.ps1
@@ -3,43 +3,38 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-$url                       = 'https://download.semiconductor.samsung.com/resources/software-resources/Samsung_Magician_installer_Official_8.0.0.900_Windows.zip'
-$checksum                  = 'c786d822e1442d30bf63480a3c3a31c779e087753a060f4359f9f1351d44f311'
+$url                       = 'https://download.semiconductor.samsung.com/resources/software-resources/Samsung_Magician_Installer_Official_8.1.0.800.exe'
+$checksum                  = '5F3F8D53B3DD8FA7655366A7C2B4427B8799FA52BC242176C1F67ACFCDB7B2C5'
 $checksumType              = 'sha256'
-[version] $softwareVersion = '8.0.0.900'
 
-$installedVersion = Get-InstalledVersion
-if ($installedVersion -gt $softwareVersion) {
-  Write-Output "Current installed version (v$installedVersion) must be uninstalled first..."
-  Uninstall-CurrentVersion
-}
-
+# Download the installer using Chocolatey's internal handling
 $packageArgs = @{
-  PackageName    = $env:ChocolateyPackageName
+  packageName    = $env:ChocolateyPackageName
   url            = $url
   checksum       = $checksum
   checksumType   = $checksumType
-  Destination    = $toolsDir
-}
-
-Install-ChocolateyZipPackage @packageArgs
-
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  file           = Get-Item $toolsDir\*.exe
   softwareName   = 'Samsung Magician*'
-  silentArgs     = ""
-  validExitCodes = @(0,3010)
 }
 
-# silent install requires AutoHotKey
+# Use AutoHotkey portable from the Choco directory
+$ahkEXE = Get-ChildItem -Path "$env:ChocolateyInstall\lib\autohotkey.portable\tools" -Filter 'AutoHotKey.exe' -Recurse -ErrorAction SilentlyContinue
+if (-not $ahkEXE) {
+  Write-Error "AutoHotKey executable not found in $env:ChocolateyInstall\lib\autohotkey.portable\tools"
+  return
+}
+
+# Start AutoHotKey script for the silent install
 $ahkFile = Join-Path $toolsDir 'chocolateyInstall.ahk'
-$ahkEXE = Get-ChildItem "$env:ChocolateyInstall\lib\autohotkey.portable" -Recurse -filter autohotkey.exe
 $ahkProc = Start-Process -FilePath $ahkEXE.FullName -ArgumentList $ahkFile -PassThru
 Write-Debug "AutoHotKey start time:`t$($ahkProc.StartTime.ToShortTimeString())"
 Write-Debug "Process ID:`t$($ahkProc.Id)"
 
-Install-ChocolateyInstallPackage @packageArgs
+Write-Output (("-") * 80)
+Write-Warning "Please wait a few minutes for Samsung Magician to install."
+Write-Warning "If you run into errors, please run the install again and refrain from using the computer during the install."
+Write-Output (("-") * 80)
+
+# Install the package
+Install-ChocolateyPackage @packageArgs
 
 if (Get-Process -id $ahkProc.Id -ErrorAction SilentlyContinue) {Stop-Process -id $ahkProc.Id}
