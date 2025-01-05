0c3b9d214f8de6b727da89cce54684ab13e968b7
Maurice Kevenaar <maurice@kevenaar.name>
2025-01-05 20:56:03 +0100

    (intel-mas) fix package

--- automatic/intel-mas/update.ps1
+++ automatic/intel-mas/update.ps1
@@ -2,31 +2,49 @@ Import-Module Chocolatey-AU
 
 $releases = 'https://downloadcenter.intel.com/download/30161'
 
-function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
+$headers = @{
+  "Accept"          = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8"
+  "Accept-Language" = "en-GB,en;q=0.5"
+  "Accept-Encoding" = "gzip, deflate, br, zstd"
+}
 
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
+$options =
+@{
+  Headers = $headers
+}
+
+
+function global:au_SearchReplace {
+  @{
+    'tools\chocolateyInstall.ps1'   = @{
+      "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $url = (($download_page.ParsedHtml.getElementsByTagName('button') | Where-Object innerHtml -Match ".zip").attributes | Where-Object name -eq "data-href").nodeValue
-    $url = [uri]::EscapeUriString($url) -replace "&#174;", "%C2%AE"
+  $download_page = Invoke-WebRequest -Uri $releases -Headers $headers
 
-    $version = $download_page.ParsedHtml.getElementsByName('DownloadVersion') | Select-Object -First 1 -ExpandProperty content
+  $url = (($download_page.ParsedHtml.getElementsByTagName('button') | Where-Object innerHtml -Match ".zip").attributes | Where-Object name -eq "data-href").nodeValue
+  $url = [uri]::EscapeUriString($url) -replace "&#174;", "%C2%AE"
 
-    $releaseNotes = $download_page.Links | Where-Object href -Match ".pdf" | Where-Object href -Match "Release" | Select-Object -First 1 -ExpandProperty href
+  $version = $download_page.ParsedHtml.getElementsByName('DownloadVersion') | Select-Object -First 1 -ExpandProperty content
 
-    $Latest = @{ URL32 = $url; Version = $version; ReleaseNotes = $releaseNotes }
-    return $Latest
+  $releaseNotes = $download_page.Links | Where-Object href -Match ".pdf" | Where-Object href -Match "Release" | Select-Object -First 1 -ExpandProperty href
+
+  $Latest = @{
+    URL32        = $url
+    Version      = $version
+    ReleaseNotes = $releaseNotes
+    Options      = $options
+  }
+  return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 32 -NoCheckUrl
