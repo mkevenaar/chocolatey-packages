26f249a532718482b5436d0772f270f111042174
mingm <ming_maneerat@hotmail.com>
2025-01-15 21:07:04 +0700

    Add uninstall script
    The script will be remove the mysql service from windows host.

---
+++ automatic/mysql/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$pp = Get-PackageParameters
+
+$packageName = 'mysql'
+$binRoot = if ($pp.installLocation) { $pp.installLocation } else { Get-ToolsLocation }
+$installDir = Join-Path $binRoot "$packageName"
+$installDirBin = "$($installDir)\current\bin"
+Write-Host "Adding `'$installDirBin`' to the path and the current shell path"
+Install-ChocolateyPath "$installDirBin"
+
+$serviceName = if ($pp.serviceName) { $pp.serviceName } else { "MySQL" }
+
+# turn off the service
+Start-ChocolateyProcessAsAdmin "cmd /c NET STOP $serviceName"
+
+# remove the service itself
+write-host "Removing the mysql service"
+Start-ChocolateyProcessAsAdmin "cmd /c '$($installDirBin)\mysqld' --remove $serviceName"
+
+# notify user
+write-host "You are safe to remove the package from $installDir"
