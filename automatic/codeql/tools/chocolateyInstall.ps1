c40c3de77710eac69caab781ec66d8ab922894d5
Maurice Kevenaar <maurice@kevenaar.name>
2025-01-22 16:34:55 +0000

    AU: 2 updated - codeql firefox-dev [skip ci]

--- automatic/codeql/tools/chocolateyInstall.ps1
+++ automatic/codeql/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $package = Split-Path $toolsDir
 $codeql_home = Join-Path $package 'codeql-win64/codeql'
 $codeql_bat = Join-Path $codeql_home 'codeql.exe'
 
-$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.20.1/codeql-win64.zip'
-$checksum = '0466e572c513149af84b7eb27e31349f1eb049b9a01e8105f3aba1fed616d792'
+$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.20.2/codeql-win64.zip'
+$checksum = 'eaa098d6c30cbf9c2b74815019765fd0bdadc04c5e47406153dc54834181f6fe'
 $checksumType = 'sha256'
 
 $packageArgs = @{
