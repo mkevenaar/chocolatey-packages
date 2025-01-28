ffb2825cce39abec7e512ebd8197e40aedc49c2c
Maurice Kevenaar <maurice@kevenaar.name>
2025-01-28 12:34:40 +0000

    AU: 4 updated - evernote zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci]

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = 'DD119DDE31DB9FE4C0A078285198288515C8C0A12D9790356D6A491AD9A28450'
+$checksum     = '5FF6B93C3EADC4D202FA29E0AC16F66A1DC3E2918851873C885477FD2F5D6BDE'
 $checksumType = 'sha256'
 
 $packageArgs = @{
