8787ea305fbab3333abc9e10de12131042e94ea6
Maurice Kevenaar <maurice@kevenaar.name>
2025-02-09 16:34:22 +0000

    AU: 3 updated - prowlarr radarr vscode-yaml [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/e8e290fed0d41151f33dc7fcbd4a01f90d2e4d18

--- automatic/radarr/tools/chocolateyInstall.ps1
+++ automatic/radarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'radarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Radarr.master.5.18.4.9674.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Radarr.master.5.18.4.9674.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Radarr.develop.5.19.0.9697.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Radarr.develop.5.19.0.9697.windows-core-x64-installer.exe"
   softwareName  = 'Radarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
