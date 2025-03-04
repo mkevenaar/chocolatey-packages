bdf42d0c7da234120a050212f5ec8837256c18b7
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-04 00:34:24 +0000

    AU: 1 updated - firefox-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/d428593b51041d881315f6d08f32f8b8e95ed099

--- automatic/firefox-nightly/tools/chocolateyInstall.ps1
+++ automatic/firefox-nightly/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'firefox-nightly'
 $softwareName = 'Nightly'
 
-#$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '137.0a1')
+#$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '138.0a1')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://archive.mozilla.org/pub/firefox/nightly/2025/03/2025-03-03-09-00-51-mozilla-central/firefox-137.0a1.${locale}.win32.installer.exe"
+    Url = "https://archive.mozilla.org/pub/firefox/nightly/2025/03/2025-03-03-16-29-10-mozilla-central/firefox-138.0a1.${locale}.win32.installer.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2025/03/2025-03-03-09-00-51-mozilla-central/firefox-137.0a1.${locale}.win64.installer.exe"
+    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2025/03/2025-03-03-16-29-10-mozilla-central/firefox-138.0a1.${locale}.win64.installer.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
