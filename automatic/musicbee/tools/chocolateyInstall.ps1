409484e0c9526444fa9e75af18a936e4a2b94cc0
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-11 20:56:04 +0100

    (musicbee) embed package [AU musicbee]

--- automatic/musicbee/tools/chocolateyInstall.ps1
+++ automatic/musicbee/tools/chocolateyInstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://files1.majorgeeks.com/cbc0ce49d4264009a3e23d38a40fb732630685b8/multimedia/MusicBeeSetup_3_5.zip'
-$checksum     = 'be27f1149654924dddd106254493137280f3e8098ec1029435c02fdda092bdb5'
-$checksumType = 'sha256'
-
 
 $downloadArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = $url
-  checksum      = $checksum
-  checksumType  = $checksumType
+  file          = "xxx"
 }
 
-Install-ChocolateyZipPackage @downloadArgs
+Get-ChocolateyUnzip @downloadArgs
 
 $fileLocation = (Get-ChildItem $toolsDir -Filter *.exe | Select-Object -First 1).FullName
 
--- automatic/musicbee/update.ps1
+++ automatic/musicbee/update.ps1
@@ -2,15 +2,7 @@ Import-Module Chocolatey-AU
 
 $releases = 'https://www.majorgeeks.com/files/details/musicbee.html'
 
-function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
-    }
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
@@ -18,11 +10,28 @@ function global:au_GetLatest {
     $url = $Matches[0]
 
     $version = $download_page.Content -Match 'MusicBee ([\d.]+)'
-    $versiondata = Get-Version($Matches[1])
-    $version = $versiondata.toString()
-
-    $Latest = @{ URL32 = $url; Version = $version }
-    return $Latest
+    $versionData = Get-Version($Matches[1])
+    $version = $versionData.toString()
+  return @{
+        URL32 = $url
+        Version = $version
+        FileType = 'zip'
+    }
+}
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+    }
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+    }
+  }
 }
 
-update -ChecksumFor 32
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor None
+}
