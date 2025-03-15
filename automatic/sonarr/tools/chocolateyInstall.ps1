770b26d7d9aefb2b1968cd4929b78d623b28b5d9
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-15 18:23:24 +0000

    AU: 1 updated - sonarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/36d5ec44ac618a69d4140ffb8f114a6c5f9967b0

--- automatic/sonarr/tools/chocolateyInstall.ps1
+++ automatic/sonarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename    = 'sonarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Sonarr.develop.4.0.13.2934.win-x86-installer.exe"
-  file64        = "$toolsDir\Sonarr.develop.4.0.13.2934.win-x64-installer.exe"
+  file          = "$toolsDir\Sonarr.develop.4.0.14.2938.win-x86-installer.exe"
+  file64        = "$toolsDir\Sonarr.develop.4.0.14.2938.win-x64-installer.exe"
   softwareName  = 'Sonarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
