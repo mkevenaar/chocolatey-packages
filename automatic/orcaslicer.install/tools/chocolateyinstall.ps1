63f8d465bf02710dbb74e98c96ce562e140afd8f
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-20 16:22:10 +0000

    AU: 1 updated - orcaslicer.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/1ce8365fda0df88ccbf4b382c2a8fd5e986162cf

--- automatic/orcaslicer.install/tools/chocolateyinstall.ps1
+++ automatic/orcaslicer.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\OrcaSlicer_Windows_Installer_V2.3.0-rc.exe"
+  file          = "$toolsDir\OrcaSlicer_Windows_Installer_V2.3.0.exe"
   fileType      = 'exe'
   softwareName  = 'OrcaSlicer*'
   silentArgs    = "/S"
