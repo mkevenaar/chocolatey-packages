0f85af041e38342480c57d4ab87e93d196781c81
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-27 16:20:58 +0000

    AU: 1 updated - bacula [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/f0933795894149e9725de13d7b3f882912341e4d

--- automatic/bacula/tools/chocolateyInstall.ps1
+++ automatic/bacula/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://www.bacula.org/download/11489/?tmstv=1723110204'
-$checksum64     = 'd863afadc85eb9c29e4f03c123d18cf77561724e818f3a628f8f184171091f83'
+$url64          = 'https://www.bacula.org/download/11565/?tmstv=1743091640'
+$checksum64     = '681952b31d3654aacef16e9cbb5615e69c8abcde67336ec1af30ce6a943f04e7'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
