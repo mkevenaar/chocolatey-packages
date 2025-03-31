99e074185a3f72286bbbd0f6548f62520c97b1cb
Maurice Kevenaar <maurice@kevenaar.name>
2025-03-31 11:00:46 +0200

    (signal) remove unused line

--- automatic/signal/tools/chocolateyInstall.ps1
+++ automatic/signal/tools/chocolateyInstall.ps1
@@ -9,7 +9,6 @@ Set-AutoUpdate -Enable
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = 'https://updates.signal.org/desktop/signal-desktop-win-7.48.0.exe'
   checksum       = '498d90cc8060bf16fd8ddeaca31b18b6b1c944f22ade522f7a91e13764434b4d'
