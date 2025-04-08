e460d7f73aaca492d3efed0cad4659fdbe7d1753
Maurice Kevenaar <maurice@kevenaar.name>
2025-04-08 14:22:41 +0000

    AU: 2 updated - evernote firefox-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/3784e18d54e693e9ddcb6adce058d0a1ae17d437

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = 'FC1E837610D6559C4767DC6E3B6FE9A05FE120AF7B17154AB71787338F7CD3D7'
+$checksum     = '81603D10A77CF2C688AF5105CDECD8D550D0453A2FC56280C6A73B0D2CFBD740'
 $checksumType = 'sha256'
 
 $packageArgs = @{
