98bd1e1ab4676d5bf6bb8914db04bf2265eb9cf4
Maurice Kevenaar <maurice@kevenaar.name>
2025-04-21 06:21:26 +0000

    AU: 5 updated - helio-workstation helio-workstation.install helio-workstation.portable jackett vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/b8f291e17a86ecc65da97cba16e2d3d4f92b4deb

--- automatic/helio-workstation.portable/tools/chocolateyinstall.ps1
+++ automatic/helio-workstation.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  file          = "$toolsDir\helio-3.14-x32.zip"
-  file64        = "$toolsDir\helio-3.14-x64.zip"
+  file          = "$toolsDir\helio-3.15-x32.zip"
+  file64        = "$toolsDir\helio-3.15-x64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
