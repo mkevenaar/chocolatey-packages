d1151c2504658c454d30b219a935b6c06b9dc81e
Maurice Kevenaar <maurice@kevenaar.name>
2025-04-22 14:25:48 +0000

    AU: 5 updated - codeql firefox-nightly zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/0acf5e18f351e5cd05298443f85a15ace5012a27

--- automatic/zabbix-agent2/tools/chocolateyinstall.ps1
+++ automatic/zabbix-agent2/tools/chocolateyinstall.ps1
@@ -39,8 +39,8 @@ if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'MSI'
-  file          = "$toolsDir\zabbix_agent2-6.0.40-windows-i386-openssl.msi"
-  file64        = "$toolsDir\zabbix_agent2-6.0.40-windows-amd64-openssl.msi"
+  file          = "$toolsDir\zabbix_agent2-7.0.12-windows-i386-openssl.msi"
+  file64        = "$toolsDir\zabbix_agent2-7.0.12-windows-amd64-openssl.msi"
   softwareName  = "Zabbix Agent *"
 
   SilentArgs     = $SilentArgs
