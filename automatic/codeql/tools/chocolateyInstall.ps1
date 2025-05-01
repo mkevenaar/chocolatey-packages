abdfd123d82cedccd751bdcba2e8891e2abff768
Maurice Kevenaar <maurice@kevenaar.name>
2025-05-01 14:21:32 +0000

    AU: 1 updated - codeql [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/dc8aa4d87d97e0ec6f7c02a3e5c189c6864ae063

--- automatic/codeql/tools/chocolateyInstall.ps1
+++ automatic/codeql/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $package = Split-Path $toolsDir
 $codeql_home = Join-Path $package 'codeql-win64/codeql'
 $codeql_bat = Join-Path $codeql_home 'codeql.exe'
 
-$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.21.1/codeql-win64.zip'
-$checksum = '79b10e3d71604c72a31226a628f128f437750b56bcf4a4c561d42d96b7ee004e'
+$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.21.2/codeql-win64.zip'
+$checksum = '6b0eec8475cc8a85bde18f8077c846bade5022fc4468b9932418b5e6b3760677'
 $checksumType = 'sha256'
 
 $packageArgs = @{
