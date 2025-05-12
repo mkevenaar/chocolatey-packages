5403f62e3a80848afa478a8712c45e7d4d824776
Maurice Kevenaar <maurice@kevenaar.name>
2025-05-12 08:23:29 +0000

    AU: 1 updated - driverbooster [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/a8b16437913877a518c7cb6c7d4e25af65ac055f

--- automatic/driverbooster/tools/chocolateyInstall.ps1
+++ automatic/driverbooster/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://cdn.iobit.com/dl/driver_booster_setup.exe'
-$checksum     = '325E0E824E9AC0B679AC96C649A1ECA6ECCC08299A523CE7AD19D96FE8D6A4DD'
+$checksum     = 'AA2DCD17822BBF94344D13ACB5B6A6C1B997ED4A16518AB8F7626DC576DD82DF'
 
 $checksumType = 'sha256'
 
