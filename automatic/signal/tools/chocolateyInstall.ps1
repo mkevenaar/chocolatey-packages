96156d61a3be4d84f8b86d852c07f0d7865d43f6
Maurice Kevenaar <maurice@kevenaar.name>
2025-05-13 22:24:57 +0000

    AU: 1 updated - signal [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/a37b1cbd85369fc89b5cc0fcbaaaf06730e12202

--- automatic/signal/tools/chocolateyInstall.ps1
+++ automatic/signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://updates.signal.org/desktop/signal-desktop-win-7.53.0.exe'
-  checksum       = 'fbe374dce87ea9e49489aade8d209e16b9abfd928e70b96bc690b549c15dd0cd'
+  url            = 'https://updates.signal.org/desktop/signal-desktop-win-7.54.0.exe'
+  checksum       = '3ea684ff4731ef94669523663d2f6299beae1b12e092bc94348657e3eea82fc5'
   checksumType   = 'sha256'
   softwareName   = 'Signal *'
   silentArgs     = '/S'
