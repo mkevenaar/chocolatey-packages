d7a85efab818f88c8fcffb9d3f0b3484cf0abdbd
Maurice Kevenaar <maurice@kevenaar.name>
2025-05-24 12:20:54 +0000

    AU: 3 updated - helio-workstation helio-workstation.install helio-workstation.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/bb56f1acfb8250897e7616aec38841dcca95b557

--- automatic/helio-workstation.install/tools/chocolateyinstall.ps1
+++ automatic/helio-workstation.install/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\helio-3.15.exe"
+  file          = "$toolsDir\helio-3.16.exe"
   softwareName  = 'Helio Workstation*'
   silentArgs    = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /FORCECLOSEAPPLICATIONS /NOICONS"
   validExitCodes= @(0,3010)
