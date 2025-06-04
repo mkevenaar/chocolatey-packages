943164711a9bc106fa529ed1f0e05f8eb6902279
Maurice Kevenaar <maurice@kevenaar.name>
2025-06-04 12:21:48 +0000

    AU: 2 updated - firefox-nightly prowlarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/292543f39c350670eb9114dfaeeba28983f8eb53

--- automatic/prowlarr/tools/chocolateyInstall.ps1
+++ automatic/prowlarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'prowlarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Prowlarr.master.1.36.3.5071.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Prowlarr.master.1.36.3.5071.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Prowlarr.master.1.37.0.5076.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Prowlarr.master.1.37.0.5076.windows-core-x64-installer.exe"
   softwareName  = 'Prowlarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
