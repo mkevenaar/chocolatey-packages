c681573ce538779f75d26cb60b34b1fd91969ef4
Maurice Kevenaar <maurice@kevenaar.name>
2025-06-09 18:21:07 +0000

    AU: 2 updated - evernote lidarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/b2c8c9e0ecadc32a337ef94faa0b58d039720c25

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = '453F27BB2EC63826B724B0CB916A3E4C291C94F0B6D4D6F69B5A4A1ED6A48126'
+$checksum     = 'F93ECB32D5A5DC84A85ABB41D7BFE67CD30629CADF1478D123B1085E9F0A5EB8'
 $checksumType = 'sha256'
 
 $packageArgs = @{
