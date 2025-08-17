c6456a5039fbe8587bb03757bf2292bc4884a59b
Maurice Kevenaar <maurice@kevenaar.name>
2025-08-17 20:21:59 +0000

    AU: 3 updated - lidarr prowlarr radarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/d0a9c8cebd369710609ad0ba63c611a3b8682144

--- automatic/prowlarr/tools/chocolateyInstall.ps1
+++ automatic/prowlarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'prowlarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Prowlarr.develop.2.0.3.5130.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Prowlarr.develop.2.0.3.5130.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Prowlarr.develop.2.0.4.5138.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Prowlarr.develop.2.0.4.5138.windows-core-x64-installer.exe"
   softwareName  = 'Prowlarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
