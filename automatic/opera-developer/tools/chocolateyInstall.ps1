43f0bf4d3b67416eeb0cea4ed34abc852808b713
Maurice Kevenaar <maurice@kevenaar.name>
2025-08-19 12:22:01 +0000

    AU: 5 updated - firefox-nightly opera-developer zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/2894df096baa269241daec38c57a39407a720316

--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/122.0.5621.0/win/Opera_Developer_122.0.5621.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/122.0.5621.0/win/Opera_Developer_122.0.5621.0_Setup_x64.exe'
-  checksum       = 'f87dbcf2f4a1590d72cea88ff8a3e44cb8b2837c122da15e108620579b1c695e'
-  checksum64     = 'ff352ebd602fdb93d96fb979ffd3d6d3f661ce35adc0c93a87ed8f9541301bac'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/122.0.5629.0/win/Opera_Developer_122.0.5629.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/122.0.5629.0/win/Opera_Developer_122.0.5629.0_Setup_x64.exe'
+  checksum       = '8a28c167e0d3bd2819404851c301f6c14b27094e9c37d7e95a55d7b71077b35b'
+  checksum64     = 'fdb1a3b2bb6302b31ee1446fa99ed963813f9975690146fadda7c4411134511b'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '122.0.5621.0'
+$version = '122.0.5629.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
