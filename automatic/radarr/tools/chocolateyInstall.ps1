86bd95e264a3537f324b404894c94ffc6d1bd99e
Maurice Kevenaar <maurice@kevenaar.name>
2025-09-07 06:21:12 +0000

    AU: 4 updated - jackett prowlarr radarr vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/2d1d8899d24d17c28689bb9b27f13371cb0a7eaf

--- automatic/radarr/tools/chocolateyInstall.ps1
+++ automatic/radarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename = 'radarr'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Radarr.master.5.27.5.10198.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Radarr.master.5.27.5.10198.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Radarr.develop.5.28.0.10205.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Radarr.develop.5.28.0.10205.windows-core-x64-installer.exe"
   softwareName  = 'Radarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
