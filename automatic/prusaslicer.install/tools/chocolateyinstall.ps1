ee2adc46f73738c6e4bbeced7a0a0490b0d4a1b8
Maurice Kevenaar <maurice@kevenaar.name>
2025-09-21 20:32:16 +0200

    (maint) updated prusaslicer [skip-ci]

--- automatic/prusaslicer.install/tools/chocolateyinstall.ps1
+++ automatic/prusaslicer.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\prusa3d_win_2_9_2_x64.exe"
+  file          = "$toolsDir\prusa3d_win_2_9_3_x64.exe"
   fileType      = 'exe'
   softwareName  = 'PrusaSlicer*'
   silentArgs    = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /FORCECLOSEAPPLICATIONS /NOICONS"
