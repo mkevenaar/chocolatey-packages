bb139be06f81b6c6e1a4bb67d68763cfd79abe49
Maurice Kevenaar <maurice@kevenaar.name>
2025-09-21 20:21:41 +0000

    AU: 3 updated - lidarr prowlarr radarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/1c94074aa4b9454a6d53777d67f4967ac64bd505

--- automatic/lidarr/tools/chocolateyInstall.ps1
+++ automatic/lidarr/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $servicename    = "lidarr"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\Lidarr.develop.2.14.2.4785.windows-core-x86-installer.exe"
-  file64        = "$toolsDir\Lidarr.develop.2.14.2.4785.windows-core-x64-installer.exe"
+  file          = "$toolsDir\Lidarr.develop.2.14.3.4791.windows-core-x86-installer.exe"
+  file64        = "$toolsDir\Lidarr.develop.2.14.3.4791.windows-core-x64-installer.exe"
   softwareName  = 'Lidarr*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
