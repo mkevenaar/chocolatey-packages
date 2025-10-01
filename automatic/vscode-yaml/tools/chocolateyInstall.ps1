51b44424f276e13d445195b2447b97f439d4d9bf
Maurice Kevenaar <maurice@kevenaar.name>
2025-10-01 14:40:59 +0000

    AU: 4 updated - vscode-yaml zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/8c138468979ead505609e69713b12c84eebc32a7

--- automatic/vscode-yaml/tools/chocolateyInstall.ps1
+++ automatic/vscode-yaml/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.19.0'
+Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.20.2025100113'
--- automatic/zabbix-agent/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$version = '7.0.19'
+$version = '7.4.3'
 $title = 'Zabbix Agent'
 
 $configDir = Join-Path $env:PROGRAMDATA 'zabbix'
@@ -19,8 +19,8 @@ $service = Get-WmiObject -Class Win32_Service -Filter "Name=`'$title`'"
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   unzipLocation = $tempDir
-  file          = "$toolsDir\zabbix_agent-7.0.19-windows-i386-openssl.zip"
-  file64        = "$toolsDir\zabbix_agent-7.0.19-windows-amd64-openssl.zip"
+  file          = "$toolsDir\zabbix_agent-7.4.3-windows-i386-openssl.zip"
+  file64        = "$toolsDir\zabbix_agent-7.4.3-windows-amd64-openssl.zip"
 }
 
 try {
