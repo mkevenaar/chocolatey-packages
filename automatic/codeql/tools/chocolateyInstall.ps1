ba7eacc8854774b36dfdb6b3d05cc821df746547
Maurice Kevenaar <maurice@kevenaar.name>
2025-10-17 14:40:36 +0000

    AU: 2 updated - codeql viber [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/fe0f2dc999efd9e4e69bae1c7b650aecb0d37f80

--- automatic/codeql/tools/chocolateyInstall.ps1
+++ automatic/codeql/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $package = Split-Path $toolsDir
 $codeql_home = Join-Path $package 'codeql-win64/codeql'
 $codeql_bat = Join-Path $codeql_home 'codeql.exe'
 
-$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.23.2/codeql-win64.zip'
-$checksum = '7e53592f7984350edbe6da88246f57e9f74da89d92e5c6e0ad7997a55c929851'
+$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.23.3/codeql-win64.zip'
+$checksum = '4fd0a039438ddc66f7f178cee9008686fe689f9e06e2cb7adc4bc73622220378'
 $checksumType = 'sha256'
 
 $packageArgs = @{
--- automatic/viber/tools/chocolateyInstall.ps1
+++ automatic/viber/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://download.cdn.viber.com/desktop/windows/ViberSetup.exe'
-$checksum     = 'E358CD4FC46528592EEB21D3C826BBC7EF57BB3DCF535A8468A5215E6467EC72'
+$checksum     = 'F07D6EB54504FA0B5767B876992C16E3FF835E79FF08067BF2EB8AA8955140D5'
 $checksumType = 'sha256'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
