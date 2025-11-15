e21afa5c3049dbcf455584cc973a88838e77adfe
Maurice Kevenaar <maurice@kevenaar.name>
2025-11-15 08:38:54 +0000

    AU: 3 updated - mariadb mariadb.install mariadb.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/d4066a81363489c5a52a5e7087c2cbdf4d7054df

--- automatic/mariadb.portable/tools/chocolateyInstall.ps1
+++ automatic/mariadb.portable/tools/chocolateyInstall.ps1
@@ -13,7 +13,7 @@ if (![System.IO.Directory]::Exists($installDir)) { [System.IO.Directory]::Create
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file64        = "$toolsdir\mariadb-11.8.4-winx64.zip"
+  file64        = "$toolsdir\mariadb-11.8.5-winx64.zip"
 }
 
 Write-Verbose "Downloading and installing program..."
