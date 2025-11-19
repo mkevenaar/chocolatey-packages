7d9a31384b726d48ca5dd1a72803ca193c6ef919
Maurice Kevenaar <maurice@kevenaar.name>
2025-11-19 12:22:50 +0000

    AU: 2 updated - jetbrainstoolbox vscode-yaml [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/db1a65f82d45aa6b0baf9d863b7c3de5c201bd86

--- automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
+++ automatic/jetbrainstoolbox/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-3.0.1.59888.exe'
-$checksum     = 'd5a1cd8cdd4059ef8a4cb70dffbff11f125c5d9d9e0400927c315ab5b3e4f500'
+$url          = 'https://download.jetbrains.com/toolbox/jetbrains-toolbox-3.1.0.62320.exe'
+$checksum     = '8ed7f9d199c151d845ce1898c8e21024cce11e29c1ba54adbf34531daa29adc4'
 $checksumType = 'sha256'
 
 $packageArgs = @{
