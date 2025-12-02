9c1d62dd0166a0ddbb5a076856cabd1d30f10a69
Maurice Kevenaar <maurice@kevenaar.name>
2025-12-02 08:38:24 +0000

    AU: 1 updated - vscode-yaml [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/c7f51e558022ef3dcfb432186e0404371d748635

--- automatic/vscode-yaml/tools/chocolateyInstall.ps1
+++ automatic/vscode-yaml/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.20.2025112808'
+Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.20.2025120208'
