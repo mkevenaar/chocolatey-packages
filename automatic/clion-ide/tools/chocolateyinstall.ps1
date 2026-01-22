5c112f5f1161d9e1ea13d3fa6c02bad8bd1b246e
Maurice Kevenaar <maurice@kevenaar.name>
2026-01-22 16:38:22 +0000

    AU: 2 updated - clion-ide firefox-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/0024d932d25e3621caa5cbfd07e55a4a0e185218

--- automatic/clion-ide/tools/chocolateyinstall.ps1
+++ automatic/clion-ide/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/cpp/CLion-2025.3.1.1.exe'
-$checksum     = 'd3d07f227ed708811359cae52fa43904aed5f9023442e1e36789719418747b86'
+$url          = 'https://download.jetbrains.com/cpp/CLion-2025.3.2.exe'
+$checksum     = '39f07019496d9250053e143c9d5423b54e53c7dc636f71e3768e51852ea4fddd'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
