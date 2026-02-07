047ddbfe475b9de4cab86ef2546608c9e16bc1f2
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-07 10:37:40 +0000

    AU: 3 updated - texstudio texstudio.install texstudio.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/82b8b611ebabaae5cbf00dfeb1cf3bccf989b7ba

--- automatic/texstudio.install/tools/chocolateyinstall.ps1
+++ automatic/texstudio.install/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  file           = "$toolsdir\texstudio-4.9.1-win-qt6-signed.exe"
+  file           = "$toolsdir\texstudio-4.9.2-win-qt6-signed.exe"
   softwareName   = 'TeXStudio *'
   silentArgs     = '/S'
 
