f0132b0cbcafe76d7d3faf118dc0be911f2e6fcb
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-11 14:38:22 +0000

    AU: 3 updated - zabbix-agent zabbix-agent.install zabbix-agent2 [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/7e76fceeaab783baab1eef64206d33632f33d79f

--- automatic/zabbix-agent/tools/chocolateyInstall.ps1
+++ automatic/zabbix-agent/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-$version = '6.0.44'
+$version = '7.0.23'
 $title = 'Zabbix Agent'
 
 $configDir = Join-Path $env:PROGRAMDATA 'zabbix'
@@ -19,8 +19,8 @@ $service = Get-WmiObject -Class Win32_Service -Filter "Name=`'$title`'"
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   unzipLocation = $tempDir
-  file          = "$toolsDir\zabbix_agent-6.0.44-windows-i386-openssl.zip"
-  file64        = "$toolsDir\zabbix_agent-6.0.44-windows-amd64-openssl.zip"
+  file          = "$toolsDir\zabbix_agent-7.0.23-windows-i386-openssl.zip"
+  file64        = "$toolsDir\zabbix_agent-7.0.23-windows-amd64-openssl.zip"
 }
 
 try {
--- automatic/zabbix-agent2/tools/chocolateyinstall.ps1
+++ automatic/zabbix-agent2/tools/chocolateyinstall.ps1
@@ -39,8 +39,8 @@ if ($pp.ALLOWDENYKEY) { $SilentArgs += " ALLOWDENYKEY=`"$($pp.ALLOWDENYKEY)`"" }
 $PackageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'MSI'
-  file          = "$toolsDir\zabbix_agent2-6.0.44-windows-i386-openssl.msi"
-  file64        = "$toolsDir\zabbix_agent2-6.0.44-windows-amd64-openssl.msi"
+  file          = "$toolsDir\zabbix_agent2-7.0.23-windows-i386-openssl.msi"
+  file64        = "$toolsDir\zabbix_agent2-7.0.23-windows-amd64-openssl.msi"
   softwareName  = "Zabbix Agent *"
 
   SilentArgs     = $SilentArgs
