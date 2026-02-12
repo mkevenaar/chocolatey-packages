aa88918940ab84411eb9bbe1018baab2ad01b889
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-12 22:37:58 +0000

    AU: 3 updated - mariadb mariadb.install mariadb.portable [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/8d7dc6c8552d0c9c4544a09afc1183a7474538c3

--- automatic/mariadb.install/tools/chocolateyInstall.ps1
+++ automatic/mariadb.install/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  file64         = "$toolsdir\mariadb-11.8.6-winx64.msi"
+  file64         = "$toolsdir\mariadb-12.3.1-winx64.msi"
   softwareName   = 'MariaDB *'
   silentArgs     = "SERVICENAME=MySQL /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0,3010)
--- automatic/mariadb.portable/tools/chocolateyInstall.ps1
+++ automatic/mariadb.portable/tools/chocolateyInstall.ps1
@@ -13,7 +13,7 @@ if (![System.IO.Directory]::Exists($installDir)) { [System.IO.Directory]::Create
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file64        = "$toolsdir\mariadb-11.8.6-winx64.zip"
+  file64        = "$toolsdir\mariadb-12.3.1-winx64.zip"
 }
 
 Write-Verbose "Downloading and installing program..."
