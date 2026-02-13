83242e230b8b008773b08841a2095161c696eaaa
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-13 00:38:20 +0000

    AU: 2 updated - grafana grafana.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/2f4c766371c381c90e58b4d91e15930ef6d00c5b

--- automatic/grafana.install/tools/chocolateyinstall.ps1
+++ automatic/grafana.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://dl.grafana.com/oss/release/grafana-12.2.5.windows-amd64.msi'
-$checksum64 = 'd2e3a0f68fb0193a76551228fd369e843703b6850d37d1d5203728d216c6f563'
+$url64 = 'https://dl.grafana.com/oss/release/grafana-12.3.3.windows-amd64.msi'
+$checksum64 = 'd05f542d833a95db4464552b5e64357c7fe71cd98262a4b463ec1d9bb7ab8ec2'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
--- automatic/grafana/tools/chocolateyinstall.ps1
+++ automatic/grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dl.grafana.com/oss/release/grafana-12.2.5.windows-amd64.zip'
-$checksum32 = '3b34dfc983e056299e5a740abca3732c925055d474388d686fba8fa3dda6886c'
+$url32 = 'https://dl.grafana.com/oss/release/grafana-12.3.3.windows-amd64.zip'
+$checksum32 = '50aa8c3afb908672b63296d3c508bc39052e148a8e9bab1b74103c797e4bdc6c'
 $checksumType32 = 'sha256'
 
 $packageArgs = @{
