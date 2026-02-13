21daf8f5d5cc791dd063923473cb89661b1dca90
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-13 18:37:58 +0000

    AU: 1 updated - firefox-dev [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/dbbc1c60136c92536e6353a3d4f54d38968bb836

--- automatic/firefox/tools/chocolateyInstall.ps1
+++ automatic/firefox/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'firefox-dev'
 $softwareName = 'Firefox Developer Edition'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '148.0b14')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '148.0b15')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/devedition/releases/148.0b14/win32/${locale}/Firefox%20Setup%20148.0b14.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/devedition/releases/148.0b15/win32/${locale}/Firefox%20Setup%20148.0b15.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/devedition/releases/148.0b14/win64/${locale}/Firefox%20Setup%20148.0b14.exe"
+    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/devedition/releases/148.0b15/win64/${locale}/Firefox%20Setup%20148.0b15.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
