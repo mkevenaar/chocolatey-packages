75b7337ea958d683d096bc2cde40ba8fe92ed348
Maurice Kevenaar <maurice@kevenaar.name>
2026-02-25 10:39:50 +0000

    AU: 2 updated - firefox-nightly vscode-yaml [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/4bdd43fb5ca4e4e7e50c6c8ec4805981b2f8f8f7

--- automatic/firefox-nightly/tools/chocolateyInstall.ps1
+++ automatic/firefox-nightly/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'firefox-nightly'
 $softwareName = 'Nightly'
 
-#$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '149.0a1')
+#$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '150.0a1')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://archive.mozilla.org/pub/firefox/nightly/2026/02/2026-02-23-09-16-13-mozilla-central/firefox-149.0a1.${locale}.win32.installer.exe"
+    Url = "https://archive.mozilla.org/pub/firefox/nightly/2026/02/2026-02-24-09-50-58-mozilla-central/firefox-150.0a1.${locale}.win32.installer.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2026/02/2026-02-23-09-16-13-mozilla-central/firefox-149.0a1.${locale}.win64.installer.exe"
+    $packageArgs.Url64 = "https://archive.mozilla.org/pub/firefox/nightly/2026/02/2026-02-24-09-50-58-mozilla-central/firefox-150.0a1.${locale}.win64.installer.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
--- automatic/vscode-yaml/tools/chocolateyInstall.ps1
+++ automatic/vscode-yaml/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.21.2026021808'
+Install-VsCodeExtension -extensionId 'redhat.vscode-yaml@1.21.2026022508'
