96e6347c800d307d88d58495faeb79bacc7835e2
Maurice Kevenaar <maurice@kevenaar.name>
2026-03-04 22:37:39 +0000

    AU: 1 updated - slack [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/ea1cb0c832035661912e86312b4eb3f0e1d1d083

--- automatic/slack/tools/chocolateyInstall.ps1
+++ automatic/slack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.48.95/slack-standalone-4.48.95.0.msi'
-$checksum64     = '1deafc3f5707bdad80433cf98460cdef4ca802514f14f8d5897b7ca40ba64bcb'
+$url64          = 'https://downloads.slack-edge.com/desktop-releases/windows/x64/4.48.99/slack-standalone-4.48.99.0.msi'
+$checksum64     = 'bb2b996212625773fe424481bbffd1e20e24b00ffd40c8c4bfc66777ecd02a52'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
