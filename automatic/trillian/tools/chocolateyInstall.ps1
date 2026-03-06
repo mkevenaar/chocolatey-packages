f0f9042cb38f86cb7d83b803bb7aa6c50632bec9
Maurice Kevenaar <maurice@kevenaar.name>
2026-03-06 16:37:49 +0000

    AU: 2 updated - firefox-dev trillian [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/95eb57097efdbfb95ab1c2ab741e336779c8914e

--- automatic/trillian/tools/chocolateyInstall.ps1
+++ automatic/trillian/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://trillian.im/get/windows/6.6/trillian-v6.6.0.13.msi'
-$checksum     = 'a4fa7cdd5ce83278a67f77a44c8b68a410bc9303afba3b2793c1b2005997ee12'
+$url          = 'https://trillian.im/get/windows/6.6/trillian-v6.6.0.14.msi'
+$checksum     = 'df118389c625b8887001318168cf4c7df04944d8fe6760b51a150ffbb230c260'
 $checksumType = 'sha256'
 
 $packageArgs = @{
