0ddcec2a18ec5afcaafc3cd724fc5f1c4ca6f3ba
Maurice Kevenaar <maurice@kevenaar.name>
2026-03-27 10:39:31 +0000

    AU: 5 updated - codeql texstudio texstudio.install texstudio.portable vscode-yaml [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/45ea284742200678fa150fd373d8213fd2218d81

--- automatic/codeql/tools/chocolateyInstall.ps1
+++ automatic/codeql/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $package = Split-Path $toolsDir
 $codeql_home = Join-Path $package 'codeql-win64/codeql'
 $codeql_bat = Join-Path $codeql_home 'codeql.exe'
 
-$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.25.0/codeql-win64.zip'
-$checksum = '2724fd9a6278d07970323c525b37002b08b474a8761fa9a8c57f82d9282b9726'
+$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.25.1/codeql-win64.zip'
+$checksum = 'ec3a183ac0ec54d451905a4407235f122a1365e49b7a470a1ae6da66714bc1cf'
 $checksumType = 'sha256'
 
 $packageArgs = @{
