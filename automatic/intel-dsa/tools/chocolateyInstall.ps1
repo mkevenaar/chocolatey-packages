a3bd1dbc5ccf2d2b68454ac1ab3bc2dc47704489
Maurice Kevenaar <maurice@kevenaar.name>
2026-04-12 16:41:38 +0200

    (intel-dsa) small update

--- automatic/intel-dsa/tools/chocolateyInstall.ps1
+++ automatic/intel-dsa/tools/chocolateyInstall.ps1
@@ -1,10 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName = 'intel-dsa'
 $url         = 'https://dsadata.intel.com/installer/'
 $checksum    = '5B4957C11B7370CBC2A0EE0F5AFDC3DFCA3BC624A9165EB0BC0A8FE3C7AEEE5E'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url            = $url
   silentArgs     = '-s -norestart'
