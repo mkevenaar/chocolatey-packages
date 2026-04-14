0f944edbf988990d9aa9f6dbed77f0e322b28a64
Maurice Kevenaar <maurice@kevenaar.name>
2026-04-14 08:23:16 +0000

    AU: 2 updated - iobit-uninstaller opera-developer [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/905f3c69385fc12be77df0cce7e31f51098f2ae0

--- automatic/iobit-uninstaller/tools/chocolateyInstall.ps1
+++ automatic/iobit-uninstaller/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://cdn.iobit.com/dl/iobituninstaller.exe'
-$checksum     = '77757ED3C4D9226F9FD7B9D9A5DCB1DD7DE34F3B9833CCE1D8CB76DBA34451E6'
+$checksum     = '5400C987D02C3F4B159CB5B11B432971379901FD79A90FDDD1E70FA0D98E7DE5'
 $checksumType = 'sha256'
 
 . $toolsDir\helpers.ps1
--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/131.0.5863.0/win/Opera_Developer_131.0.5863.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/131.0.5863.0/win/Opera_Developer_131.0.5863.0_Setup_x64.exe'
-  checksum       = '3e3c713f85aba212d21f47dbad117a1e0be2be0a1376cf7d5af331f16b5630ee'
-  checksum64     = 'ce31d9ba80cbe02437e4c543699d9c346acdc5313bd4874eee7b629160c4f07c'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/131.0.5868.0/win/Opera_Developer_131.0.5868.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/131.0.5868.0/win/Opera_Developer_131.0.5868.0_Setup_x64.exe'
+  checksum       = 'a9310931f0f93af003304b9ba00e1861e48b70b3082d066119cd9bee059a8e0b'
+  checksum64     = 'e97b75e5c65ee0905e1715ee38e99cf58f3dc3d0d970d06d17fe3e48d351e714'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '131.0.5863.0'
+$version = '131.0.5868.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
