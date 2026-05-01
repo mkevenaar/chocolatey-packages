68d00673b7516e33abf67976acffbb2ee37dce88
Maurice Kevenaar <maurice@kevenaar.name>
2026-05-01 12:30:21 +0000

    AU: 2 updated - codeql winrar [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/4b14d6345c8213432f777c516dcc61bd3d5fb9c8

--- automatic/codeql/tools/chocolateyInstall.ps1
+++ automatic/codeql/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $package = Split-Path $toolsDir
 $codeql_home = Join-Path $package 'codeql-win64/codeql'
 $codeql_bat = Join-Path $codeql_home 'codeql.exe'
 
-$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.25.2/codeql-win64.zip'
-$checksum = '4f582d7c1dbb1291cf81b32c6c42f68a78df2d512b5bc7e8dbfe27d02c9dd4a3'
+$url = 'https://github.com/github/codeql-cli-binaries/releases/download/v2.25.3/codeql-win64.zip'
+$checksum = '268950c5ca1444bb8439ecdb20e60be9080ba0d786c5a690871251a4b7677647'
 $checksumType = 'sha256'
 
 $packageArgs = @{
--- automatic/winrar/tools/chocolateyInstall.ps1
+++ automatic/winrar/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $LCID = (Get-UICulture).LCID
-$url_version = '721'
+$url_version = '722'
 
 $checksumType64 = 'sha256'
 
