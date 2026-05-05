5e3bc8c5c3e273b0d45cf9b10b36c8b0a345604f
Maurice Kevenaar <maurice@kevenaar.name>
2026-05-05 18:36:02 +0000

    AU: 1 updated - exiftool [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/8aed687af92899fbdb3334db62737583ba89e881

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  file          = "$toolsDir\exiftool-13.57_32.zip"
-  file64        = "$toolsDir\exiftool-13.57_64.zip"
+  file          = "$toolsDir\exiftool-13.58_32.zip"
+  file64        = "$toolsDir\exiftool-13.58_64.zip"
 }
 
 Get-ChocolateyUnzip  @packageArgs
