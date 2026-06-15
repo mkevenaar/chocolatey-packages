a6bccc40830bf9f2801585a3e727c7cf334a3a13
Maurice Kevenaar <maurice@kevenaar.name>
2026-06-15 18:22:01 +0000

    AU: 1 updated - jetbrains-rider [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/e80ed496d4ba79915f40bf092939a85e38c03388

--- automatic/jetbrains-rider/tools/chocolateyInstall.ps1
+++ automatic/jetbrains-rider/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2026.1.2.exe'
-$checksum     = '84f08e7758256b84503939de9b5ddf6336bd7286485c7cc257e7c434b299ed12'
+$url          = 'https://download.jetbrains.com/rider/JetBrains.Rider-2026.1.3.exe'
+$checksum     = '41ed77cd74466cdcc906f3a6c5792f6d414cc01c48b58181fa82f7205d0bb70d'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
