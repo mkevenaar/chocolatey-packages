e3831f05cc5c5e5f9e474f443096e1e456b13749
Maurice Kevenaar <maurice@kevenaar.name>
2026-06-16 06:21:00 +0000

    AU: 3 updated - driverbooster jackett vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/85ded0f7993641e33df16da91b8ecad3beecd072

--- automatic/driverbooster/tools/chocolateyInstall.ps1
+++ automatic/driverbooster/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://cdn.iobit.com/dl/driver_booster_setup.exe'
-$checksum     = 'A18ADCF0BC47E21209B37DB2D1C1AF69719E92333045786D8B2CFF4D17A97656'
+$checksum     = '934869C27901333F11569D68C414BD43C9580779BC57E0BC351803267E6C4693'
 
 $checksumType = 'sha256'
 
