41de898a028146cd6f0d858a34bbab2320783d5e
Maurice Kevenaar <maurice@kevenaar.name>
2026-06-19 10:21:22 +0000

    AU: 2 updated - anyrail6 vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/968b88ab3dc96416591d6511f0b07b3bbce7deef

--- automatic/anyrail6/tools/chocolateyinstall.ps1
+++ automatic/anyrail6/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.anyrail.com/downloads/AnyRail/7.20/AnyRail7.20.0.msi'
-$checksum     = '5c1c558797410adc3585debb6fdd566aefb2e4cf230f6882ef8930ca952aa750'
+$url          = 'https://www.anyrail.com/downloads/AnyRail/7.20/AnyRail7.20.1.msi'
+$checksum     = 'd869a1135d1e5fb4219dcd9dbd081b5e0ba4b3b81cea54ea5af86a087e1fa8a0'
 $checksumType = 'sha256'
 
 $packageArgs = @{
