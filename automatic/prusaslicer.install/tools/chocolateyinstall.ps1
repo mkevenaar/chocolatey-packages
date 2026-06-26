32eb62b69f988574e6dc42914cf39bfbca495d3b
Maurice Kevenaar <maurice@kevenaar.name>
2026-06-26 06:21:19 +0000

    AU: 3 updated - jackett prusaslicer.install vlc-nightly [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/205dfa1b8b167f803c3e81c943098597da900ae0

--- automatic/prusaslicer.install/tools/chocolateyinstall.ps1
+++ automatic/prusaslicer.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\PrusaSlicer-2.9.5-setup_x64.exe"
+  file          = "$toolsDir\PrusaSlicer-2.9.6-setup_x64.exe"
   fileType      = 'exe'
   softwareName  = 'PrusaSlicer*'
   silentArgs    = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /FORCECLOSEAPPLICATIONS /NOICONS"
