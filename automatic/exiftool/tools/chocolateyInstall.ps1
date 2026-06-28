a6f14716455e5f5511fabbf0009eb83b2c3ae068
Maurice Kevenaar <maurice@kevenaar.name>
2026-06-28 13:11:48 +0200

    (exiftool) Ignore bundled executables
    Fixes #289

--- automatic/exiftool/tools/chocolateyInstall.ps1
+++ automatic/exiftool/tools/chocolateyInstall.ps1
@@ -13,4 +13,12 @@ Get-ChocolateyUnzip  @packageArgs
 
 Get-ChildItem -Path $toolsDir\*\exiftool*.exe | Rename-Item -NewName exiftool.exe
 
+Get-ChildItem -Path $toolsDir -Recurse -Filter 'exiftool_files' |
+  Where-Object { $_.PSIsContainer } |
+  ForEach-Object {
+    Get-ChildItem -Path $_.FullName -Recurse -Filter '*.exe' |
+      Where-Object { -not $_.PSIsContainer } |
+      ForEach-Object { Set-Content "$($_.FullName).ignore" }
+  }
+
 Get-ChildItem $toolsDir\*.zip | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
