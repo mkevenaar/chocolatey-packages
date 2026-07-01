f6ec47e19086106f76dc10ab8240e28d7ebafbc9
Maurice Kevenaar <maurice@kevenaar.name>
2026-07-01 16:39:09 +0000

    AU: 3 updated - firefox-dev grafana grafana.install [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/b22d54871009c2f970f2315c4c3af7e3fb7a8dd8

--- automatic/grafana/tools/chocolateyinstall.ps1
+++ automatic/grafana/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dl.grafana.com/oss/release/grafana-13.0.3.windows-amd64.zip'
-$checksum32 = '70b65d68d8e1d4f2ac42c84ccf277ee67e7bbaf6554c297266d804077ac73678'
+$url32 = 'https://dl.grafana.com/oss/release/grafana-13.1.0.windows-amd64.zip'
+$checksum32 = '2c5c0733fc87129334333987799e26807a5eef1572c40941909d948392cf29f4'
 $checksumType32 = 'sha256'
 
 $packageArgs = @{
