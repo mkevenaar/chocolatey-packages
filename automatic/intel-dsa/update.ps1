12742bf863557684ff1486358d503d442fbda611
Maurice Kevenaar <maurice@kevenaar.name>
2026-08-10 20:24:04 +0200

    (intel-dsa) Fix AU installer version detection

--- automatic/intel-dsa/update.ps1
+++ automatic/intel-dsa/update.ps1
@@ -1,6 +1,5 @@
 ﻿Import-Module Chocolatey-AU
-[string]$ReleasesUrl = ([xml](Get-Content $PSScriptRoot\intel-dsa.nuspec)).package.metadata.releasenotes
-[string]$DownloadURL = 'https://dsadata.intel.com/installer/'
+[string]$DownloadURL = 'https://dsadata.intel.com/installer'
 
 function global:au_SearchReplace {
   @{
@@ -18,34 +17,26 @@ function global:au_GetLatest {
     URL32 = $DownloadURL
   }
 
-  $result = Invoke-WebRequest -Uri $ReleasesUrl -UseBasicParsing -Headers @{
-    "Accept"          = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8"
-    "Accept-Language" = "en-GB,en;q=0.5"
-    "Accept-Encoding" = "gzip, deflate, br, zstd"
-  }
-
-  if ($result.Content -match '(?im)(?<TableData><table class="icstable".+?</table>)') {
-    $LatestVersion = $Matches.TableData -split "</tr>" | Select-Object -Skip 1 | ForEach-Object {
-      if ($_ -match '>(?<Version>[\d\.]+)</td>') {
-        $Matches.Version
-      }
-      else {
-        Write-Verbose "Couldn't find a version in '$_'"
-      }
-    } | Sort-Object { [version]$_ } -Descending | Select-Object -First 1
-  }
-  else {
-    Write-Verbose "Couldn't find the ICSTable on '$($ReleasesUrl)', falling back to testing the file..."
-  }
-
   $TempFile = Join-Path $env:TEMP "$(New-Guid).exe"
+  $PreviousProgressPreference = $ProgressPreference
+
   try {
-    Invoke-WebRequest -Uri $DownloadURL -OutFile $TempFile
-    $LatestVersion = (Get-ItemProperty $TempFile).VersionInfo.ProductVersion
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $TempFile
+
+    $LatestVersion = (Get-ItemProperty -Path $TempFile).VersionInfo.ProductVersion
+    if (-not $LatestVersion) {
+      throw "Failed to determine the Intel DSA version from '$DownloadURL'."
+    }
+
     $ReturnHt.Checksum32 = Get-FileHash -Path $TempFile -Algorithm SHA256 | ForEach-Object Hash
   }
   finally {
-    Remove-Item $TempFile
+    $ProgressPreference = $PreviousProgressPreference
+
+    if (Test-Path -LiteralPath $TempFile) {
+      Remove-Item -LiteralPath $TempFile -Force
+    }
   }
 
   $ReturnHt.Version = $LatestVersion
