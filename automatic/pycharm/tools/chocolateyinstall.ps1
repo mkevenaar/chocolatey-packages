cb4eea692aa6eb0e253c5ae4c4fda59f4b741c13
Maurice Kevenaar <maurice@kevenaar.name>
2026-08-12 12:24:36 +0000

    AU: 1 updated - pycharm [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/fad69cf7139ccdf89b6c95af0e30247e4cdd926c

--- automatic/pycharm/tools/chocolateyinstall.ps1
+++ automatic/pycharm/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.jetbrains.com/python/pycharm-2026.2.0.1.exe' 
-$checksum     = '8714bd82cda11977bb566097d70451c471f73cb0682ef12fdf12ec1f3387f3f9'
+$url = 'https://download.jetbrains.com/python/pycharm-2026.2.1.exe' 
+$checksum     = '163d006b77e64df2014a5c26c0e5fdf98503f9212b8281dd424ecc4d7776c49f'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
