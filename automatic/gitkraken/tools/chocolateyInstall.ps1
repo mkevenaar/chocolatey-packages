f312008e150de31df42f0ab25174644feb64e486
Maurice Kevenaar <maurice@kevenaar.name>
2026-09-02 18:22:16 +0000

    AU: 3 updated - firefox-dev gitkraken veeam-one-caching-service [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/9eb61cca60692c2ab64ac762ed2112f1bf5a50d4

--- automatic/gitkraken/tools/chocolateyInstall.ps1
+++ automatic/gitkraken/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://release.gitkraken.dev/gkd/production/normal/windows/x64/12.4.0/3HTGa9F2NGmS98S3dRE7M87vdQC/GitKrakenSetup.exe'
-$checksum64     = 'b1efd60f6ea477e0f324d6cfe27f8aa17c30c74ba9697faebd1699f18653bb22'
+$url64          = 'https://release.gitkraken.dev/gkd/production/normal/windows/x64/12.4.1/3IjalabmP6Q1ZPr7nowgJXSDwkQ/GitKrakenSetup.exe'
+$checksum64     = 'bdd2a93d91b105e3e6250f375b1664a4ecc8f2e81eb38745c046a9e8f0ac5458'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
