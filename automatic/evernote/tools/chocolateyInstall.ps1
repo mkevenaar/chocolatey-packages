895fc73210f47af75105fe962510b055b42b2033
Maurice Kevenaar <maurice@kevenaar.name>
2026-09-15 08:21:55 +0000

    AU: 2 updated - evernote opera-developer [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/e93d48d30498a8b5c48649f480a7530c94ad57e6

--- automatic/evernote/tools/chocolateyInstall.ps1
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -14,7 +14,7 @@ elseif ($osInfo.ProductType -ne 1) {
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://win.desktop.evernote.com/builds/Evernote-latest.exe'
-$checksum     = 'C3644C8EA44828F96B5690BF8D85625445CD2A4BC5C3C98A8D00F2865D8FC6FF'
+$checksum     = 'C3856DFFB4ED5442229BC2F7E7C1AC1B7C5283E11C9270CF3386FEBB5F67DA02'
 $checksumType = 'sha256'
 
 $packageArgs = @{
--- automatic/opera-developer/tools/chocolateyInstall.ps1
+++ automatic/opera-developer/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera-developer/137.0.6015.0/win/Opera_Developer_137.0.6015.0_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera-developer/137.0.6015.0/win/Opera_Developer_137.0.6015.0_Setup_x64.exe'
-  checksum       = 'dce1682cbfa77a5d2961e5ef7a7a1095dea6a2998a81b3481f8237ef58f62d1d'
-  checksum64     = 'f97732c443b69c41bed38c654ba6d3e3164e997b14cf6ea013299bc95ba13ed1'
+  url            = 'https://get.geo.opera.com/pub/opera-developer/137.0.6022.0/win/Opera_Developer_137.0.6022.0_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera-developer/137.0.6022.0/win/Opera_Developer_137.0.6022.0_Setup_x64.exe'
+  checksum       = 'dfd56fbd30dd4e34636b5ddb682be7a1b3f9eb117096c21494388ef5e1777cde'
+  checksum64     = '96b8fd78acd081940b81012f490145426aeee84a46cb823392a5a6c861a2ef11'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '137.0.6015.0'
+$version = '137.0.6022.0'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
