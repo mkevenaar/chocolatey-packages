3b74c7be7f6a6f8e571ed548aa5ece865f9dcc3e
Maurice Kevenaar <maurice@kevenaar.name>
2026-09-16 18:22:37 +0000

    AU: 5 updated - firefox-dev goland prowlarr radarr sonarr [skip ci] https://gist.github.com/mkevenaar/675e3f672bda54270f8d7f5415f9374d/0a6c9210adeff2d35ef47302a9ce6d3673123cd5

--- automatic/goland/tools/chocolateyInstall.ps1
+++ automatic/goland/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.jetbrains.com/go/goland-2026.2.2.1.exe'
-$checksum     = '2760e4c6ebbdc16123d4d4f9bbafd780b4d99624be8813171526618b935cc1d2'
+$url          = 'https://download.jetbrains.com/go/goland-2026.2.3.exe'
+$checksum     = '78d0f7fb368c7233470c057e12712fec7df315f5cecdbe1796cc9474541673e0'
 $checksumType = 'sha256'
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
