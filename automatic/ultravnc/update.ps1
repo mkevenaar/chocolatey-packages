ca0e88eb5108e6fcfca1ef309ca921531cd57245
Maurice Kevenaar <maurice@kevenaar.name>
2026-09-20 20:43:49 +0200

    (ultravnc) Fix installer URL discovery

--- automatic/ultravnc/update.ps1
+++ automatic/ultravnc/update.ps1
@@ -1,9 +1,9 @@
-Import-Module Chocolatey-AU
+﻿Import-Module Chocolatey-AU
 
-$releases = 'http://www.uvnc.com/downloads/ultravnc.html'
+$releases = 'https://uvnc.com/downloads/ultravnc.html'
 
 $headers = @{
-  Referer = 'https://www.uvnc.com/';
+  Referer = 'https://uvnc.com/'
 }
 
 $options =
@@ -11,82 +11,52 @@ $options =
   Headers = $headers
 }
 
-function name4url($url) {
-  if ($FileNameBase) { return $FileNameBase }
-  $res = $url -split '/' | Select-Object -Last 1
-  $res -replace '\.[^.]+$'
-}
-
-# function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-function global:au_BeforeUpdate {
-  $ext = $Latest.FileType
-  $toolsPath = Resolve-Path tools
-  $NoSuffix = $true
-
-  Write-Host 'Purging' $ext
-  Remove-Item -Force "$toolsPath\*.$ext" -ea ignore
-
-  $Algorithm = 'sha256'
-
-  try {
-    $client = New-Object System.Net.WebClient
-
-    $Latest.Options.Headers.GetEnumerator() | ForEach-Object { $client.Headers.Add($_.Key, $_.Value) | Out-Null }
-
-    $base_name = name4url $Latest.Url32
-    $file_name = "{0}{2}.{1}" -f $base_name, $ext, $(if ($NoSuffix) { '' } else {'_x32'})
-    $file_path = "$toolsPath\$file_name"
-
-    Write-Host "Downloading to $file_name -" $Latest.Url32
-    $client.DownloadFile($Latest.URL32, $file_path)
-    $Latest.Checksum32 = Get-FileHash $file_path -Algorithm $Algorithm | ForEach-Object Hash
-    $Latest.ChecksumType32 = $Algorithm
-    $Latest.FileName32 = $file_name
-
-    $Latest.Options.Headers.GetEnumerator() | ForEach-Object { $client.Headers.Add($_.Key, $_.Value) | Out-Null }
-
-    $base_name = name4url $Latest.Url64
-    $file_name = "{0}{2}.{1}" -f $base_name, $ext, $(if ($NoSuffix) { '' } else {'_x64'})
-    $file_path = "$toolsPath\$file_name"
-
-    Write-Host "Downloading to $file_name -" $Latest.Url64
-    $client.DownloadFile($Latest.URL64, $file_path)
-    $Latest.Checksum64 = Get-FileHash $file_path -Algorithm $Algorithm | ForEach-Object Hash
-    $Latest.ChecksumType64 = $Algorithm
-    $Latest.FileName64 = $file_name
-
-  } catch {
-    throw $_
-  } finally {
-    $client.Dispose()
-  }
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $version_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $versionPage = Invoke-WebRequest -Uri $releases -UseBasicParsing -ErrorAction Stop
+  $versionMatch = [regex]::Match($versionPage.Content, '(?i)Latest release version:\s*UltraVNC\s+(\d+(?:\.\d+){2,3})\s*<')
+  if (!$versionMatch.Success) { throw 'Unable to determine the latest stable UltraVNC version' }
+  $version = $versionMatch.Groups[1].Value
+  $versionShort = $version -replace '\.', ''
 
-    $re = 'version: UltraVNC (.+\d)'
-    $version = ([regex]::Match($version_page.content,$re)).Captures.Groups[1].value.Trim()
-    $version_short = $version -replace '\.',''
+  $releaseLinks = @($versionPage.Links | Where-Object {
+    $_.href -match '/downloads/ultravnc/[^/]+\.html$' -and
+    [Net.WebUtility]::HtmlDecode(($_.outerHTML -replace '<[^>]+>', '')).Trim() -eq "UltraVNC $version"
+  } | Select-Object -ExpandProperty href -Unique)
+  if ($releaseLinks.Count -ne 1) { throw "Expected one release page for UltraVNC $version; found $($releaseLinks.Count)" }
+  $releaseUrl = [uri]::new([uri]$releases, $releaseLinks[0])
+  $releasePage = Invoke-WebRequest -Uri $releaseUrl -UseBasicParsing -ErrorAction Stop
 
-    # $url32 = "https://www.uvnc.eu/download/" + $version_short + "/UltraVNC_" + ($version -replace '(\d).(\d).(\d).?(\d)?','$1_$2_$3$4') + "_X86_Setup.exe"
-    # $url64 = $url32 -Replace "X86","X64"
-    ## https://uvnc.eu/download/1640/UltraVNC_1640_x64_Setup.exe
-    $url32 = "https://uvnc.eu/download/" + ($version -replace '(\d).(\d).(\d).?(\d)?','$1$2$3') + "0/UltraVNC_" + $version_short + "_x86_Setup.exe"
-    $url64 = $url32 -Replace "x86","x64"
+  $urls = @{}
+  foreach ($architecture in @('x86', 'x64')) {
+    $downloadLinks = @($releasePage.Links | Where-Object {
+      $_.href -match "/summary/[^/]+/\d+-ultravnc-${versionShort}-${architecture}-setup\.html$"
+    } | Select-Object -ExpandProperty href -Unique)
+    if ($downloadLinks.Count -ne 1) { throw "Expected one $architecture installer for UltraVNC $version; found $($downloadLinks.Count)" }
 
-    $version = ($version -replace '(\d).(\d).(\d).?(\d)?','$1.$2$3$4')
-
-    while ($version.length -lt 6) {
-      $version += '0'
+    # jDownloads returns a JavaScript redirect to the actual installer directory.
+    $downloadUrl = [uri]::new($releaseUrl, ($downloadLinks[0] -replace '/summary/', '/send/'))
+    $downloadPage = Invoke-WebRequest -Uri $downloadUrl -UseBasicParsing -ErrorAction Stop
+    $redirect = [regex]::Match($downloadPage.Content, '(?i)document\.location\.href\s*=\s*["''](?<Url>https://(?:www\.)?uvnc\.eu/download/[^"''<>]+)["'']')
+    if (!$redirect.Success) { throw "Unable to resolve the UltraVNC $version $architecture installer URL" }
+    $url = [Net.WebUtility]::HtmlDecode($redirect.Groups['Url'].Value)
+    if ([IO.Path]::GetFileName(([uri]$url).AbsolutePath) -ine "UltraVNC_${versionShort}_${architecture}_Setup.exe") {
+      throw "Unexpected installer URL for UltraVNC $version ${architecture}: $url"
     }
+    $urls[$architecture] = $url
+  }
 
-    return @{
-      URL32 = $url32
-      URL64 = $url64
-      Version = $version
-      FileType = 'exe'
-      Options = $options
+  # Retain the package's historical version format (for example, 1.2.3.4 -> 1.2340).
+  $major, $minor = $version -split '\.', 2
+  $packageVersion = '{0}.{1}' -f $major, ($minor.Replace('.', '').PadRight(4, '0'))
+
+  return @{
+    URL32 = $urls.x86
+    URL64 = $urls.x64
+    Version = $packageVersion
+    FileType = 'exe'
+    Options = $options
   }
 }
 
